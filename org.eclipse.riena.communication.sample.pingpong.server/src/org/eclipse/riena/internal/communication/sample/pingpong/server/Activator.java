00dadf4f2ef039fb549eaf8bd68f5f91c41d8f66
Christian Campo <christian.campo@compeople.de>
2008-01-17 14:41:29 +0000

    initial checkin

---
+++ org.eclipse.riena.communication.sample.pingpong.server/src/org/eclipse/riena/internal/communication/sample/pingpong/server/Activator.java
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ * Copyright (c) 2007 compeople AG and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    compeople AG - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.riena.internal.communication.sample.pingpong.server;
+
+import java.util.Hashtable;
+
+import org.eclipse.riena.communication.core.publisher.RSDPublisherProperties;
+import org.eclipse.riena.communication.sample.pingpong.common.IPingPong;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+/**
+ * The Ping Pong sample shows how to publish an OSGi Services.
+ * 
+ * This sample Activator registers the IPingPong OSGi Service. This service
+ * becomes published by the Riena Communication Hessian Publisher
+ * 
+ * @author Alexander Ziegler
+ * 
+ */
+public class Activator implements BundleActivator {
+	private ServiceRegistration pingPongReg;
+
+	public void start(BundleContext context) throws Exception {
+		// create hessian service
+		PingPong pingPong = new PingPong();
+		Hashtable<String, String> properties = new Hashtable<String, String>(3);
+
+		properties.put(RSDPublisherProperties.PROP_IS_REMOTE, "true");
+		properties.put(RSDPublisherProperties.PROP_REMOTE_PROTOCOL, "hessian");
+		properties.put(RSDPublisherProperties.PROP_REMOTE_PATH, "/PingPongWS");
+
+		pingPongReg = context.registerService(IPingPong.ID, pingPong,
+				properties);
+
+		// create xfire service
+		pingPong = new PingPong();
+		properties = new Hashtable<String, String>(3);
+
+		properties.put(RSDPublisherProperties.PROP_IS_REMOTE, "true");
+		properties.put(RSDPublisherProperties.PROP_REMOTE_PROTOCOL, "xfire");
+		properties.put(RSDPublisherProperties.PROP_REMOTE_PATH,
+				"/PingPongXFireWS");
+
+		pingPongReg = context.registerService(IPingPong.ID, pingPong,
+				properties);
+
+	}
+
+	public void stop(BundleContext context) throws Exception {
+		pingPongReg.unregister();
+		pingPongReg = null;
+	}
+
+}
---
+++ org.eclipse.riena.communication.sample.pingpong.server/src/org/eclipse/riena/internal/communication/sample/pingpong/server/PingPong.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright (c) 2007 compeople AG and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    compeople AG - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.riena.internal.communication.sample.pingpong.server;
+
+import org.eclipse.riena.communication.sample.pingpong.common.IPingPong;
+import org.eclipse.riena.communication.sample.pingpong.common.Ping;
+import org.eclipse.riena.communication.sample.pingpong.common.Pong;
+
+/**
+ * Simple PingPong sample implementation
+ * 
+ * @author Alexander Ziegler
+ * 
+ */
+public class PingPong implements IPingPong {
+
+	/**
+	 * Writes the given ping into the console and answers a pong.
+	 * 
+	 * @see org.eclipse.riena.communication.sample.pingpong.common.IPingPong#ping(org.eclipse.riena.communication.sample.pingpong.common.Ping)
+	 */
+	public Pong ping(Ping ping) {
+
+		System.out.println("PingPong::Server:: " + ping);
+
+		Pong pong = new Pong();
+		pong.setText("Thx. I got the ping!");
+
+		return pong;
+	}
+
+}
