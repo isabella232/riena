15b5b44a7f04944c324ebc656dcafde080a28af3
Christian Campo <christian.campo@compeople.de>
2008-01-17 14:32:29 +0000

    changes to make hessian-3.1.3 work again disabling the check for Serializable in the SerializerFactory of hessian

--- org.eclipse.riena.communication.publisher.hessian/src/org/eclipse/riena/communication/publisher/hessian/HessianRienaDispatcherServlet.java
+++ org.eclipse.riena.communication.publisher.hessian/src/org/eclipse/riena/communication/publisher/hessian/HessianRienaDispatcherServlet.java
@@ -41,109 +41,111 @@ import com.caucho.hessian.server.HessianSkeleton;
 @SuppressWarnings("serial")
 public class HessianRienaDispatcherServlet extends GenericServlet {
 
-    private SerializerFactory serializerFactory = new SerializerFactory();
+	private SerializerFactory serializerFactory = null;
 
-    @Override
-    public void init(ServletConfig config) throws ServletException {
-        super.init(config);
-        log("HessianRienaDispatcherServlet:: DEBUG initialized");
-    }
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		serializerFactory = new SerializerFactory();
+		serializerFactory.setAllowNonSerializable(true);
+		log("HessianRienaDispatcherServlet:: DEBUG initialized");
+	}
 
-    @Override
-    public void log(String message) {
-        System.out.println(message);
-    }
+	@Override
+	public void log(String message) {
+		System.out.println(message);
+	}
 
-    public void log(String message, Exception e) {
-        System.out.println(message + "(" + e.getMessage() + ")");
-    }
+	public void log(String message, Exception e) {
+		System.out.println(message + "(" + e.getMessage() + ")");
+	}
 
-    @Override
-    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
 
-        HttpServletRequest httpReq = (HttpServletRequest) req;
-        HttpServletResponse httpRes = (HttpServletResponse) res;
+		HttpServletRequest httpReq = (HttpServletRequest) req;
+		HttpServletResponse httpRes = (HttpServletResponse) res;
 
-        // set the message context
-        MessageContextAccessor.setMessageContext(new MessageContext(httpReq, httpRes));
+		// set the message context
+		MessageContextAccessor.setMessageContext(new MessageContext(httpReq, httpRes));
 
-        HessianRemoteServicePublisher publisher = getPublisher();
-        if (publisher == null) {
-            if (httpReq.getMethod().equals("GET")) {
-                if (httpReq.getRemoteHost().equals("127.0.0.1")) {
-                    PrintWriter pw = new PrintWriter(res.getOutputStream());
-                    pw.write(new String("no webservices available"));
-                    pw.flush();
-                    return;
-                } else {
-                    httpRes.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED, "Hessian requires POST");
-                    return;
-                }
-            } else {
-                httpRes.sendError(HttpServletResponse.SC_NOT_FOUND, "webservice not found");
-                return;
-            }
-        }
-        String requestURI = httpReq.getRequestURI();
-        String contextPath = httpReq.getContextPath();
-        if (contextPath.length()>1) {
-        	requestURI = requestURI.substring(contextPath.length());
-        }
-        RemoteServiceDescription rsd = publisher.findService(requestURI);
-        log("call " + rsd);
-        if (httpReq.getMethod().equals("GET")) {
-            if (httpReq.getRemoteHost().equals("127.0.0.1")) {
-                PrintWriter pw = new PrintWriter(res.getOutputStream());
-                if (rsd == null) {
-                    pw.write("call received from browser, no remote service registered with this URL");
-                } else {
-                    pw.write(new String("calls ") + rsd);
-                }
-                pw.flush();
-                return;
-            } else {
-                httpRes.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED, "Hessian requires POST");
-                return;
-            }
-        }
-        if (rsd==null) {
-        	httpRes.sendError(HttpServletResponse.SC_NOT_FOUND,"unknown url :"+httpReq.getRequestURI());
-        	return;
-        }
-        Object instance = rsd.getService();
-        HessianSkeleton sk = new HessianSkeleton(instance, rsd.getServiceInterfaceClass());
-        Hessian2Input inp = new Hessian2Input(httpReq.getInputStream());
-        AbstractHessianOutput out;
-        inp.setSerializerFactory(serializerFactory);
+		HessianRemoteServicePublisher publisher = getPublisher();
+		if (publisher == null) {
+			if (httpReq.getMethod().equals("GET")) {
+				if (httpReq.getRemoteHost().equals("127.0.0.1")) {
+					PrintWriter pw = new PrintWriter(res.getOutputStream());
+					pw.write(new String("no webservices available"));
+					pw.flush();
+					return;
+				} else {
+					httpRes.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED, "Hessian requires POST");
+					return;
+				}
+			} else {
+				httpRes.sendError(HttpServletResponse.SC_NOT_FOUND, "webservice not found");
+				return;
+			}
+		}
+		String requestURI = httpReq.getRequestURI();
+		String contextPath = httpReq.getContextPath();
+		if (contextPath.length() > 1) {
+			requestURI = requestURI.substring(contextPath.length());
+		}
+		RemoteServiceDescription rsd = publisher.findService(requestURI);
+		log("call " + rsd);
+		if (httpReq.getMethod().equals("GET")) {
+			if (httpReq.getRemoteHost().equals("127.0.0.1")) {
+				PrintWriter pw = new PrintWriter(res.getOutputStream());
+				if (rsd == null) {
+					pw.write("call received from browser, no remote service registered with this URL");
+				} else {
+					pw.write(new String("calls ") + rsd);
+				}
+				pw.flush();
+				return;
+			} else {
+				httpRes.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED, "Hessian requires POST");
+				return;
+			}
+		}
+		if (rsd == null) {
+			httpRes.sendError(HttpServletResponse.SC_NOT_FOUND, "unknown url :" + httpReq.getRequestURI());
+			return;
+		}
+		Object instance = rsd.getService();
+		HessianSkeleton sk = new HessianSkeleton(instance, rsd.getServiceInterfaceClass());
+		Hessian2Input inp = new Hessian2Input(httpReq.getInputStream());
+		AbstractHessianOutput out;
+		inp.setSerializerFactory(serializerFactory);
 
-        int code = inp.read();
-        if (code != 'c') {
-            throw new IOException("expected 'c' in hessian input at " + code);
-        }
-        int major = inp.read();
-        // int minor = inp.read();
-        inp.read(); // TODO obsolete?? (replaced line above)
-        if (major >= 2) {
-            out = new Hessian2Output(httpRes.getOutputStream());
-        } else {
-            out = new HessianOutput(httpRes.getOutputStream());
-        }
+		int code = inp.read();
+		if (code != 'c') {
+			throw new IOException("expected 'c' in hessian input at " + code);
+		}
+		int major = inp.read();
+		// int minor = inp.read();
+		inp.read(); // TODO obsolete?? (replaced line above)
+		if (major >= 2) {
+			out = new Hessian2Output(httpRes.getOutputStream());
+		} else {
+			out = new HessianOutput(httpRes.getOutputStream());
+		}
 
-        out.setSerializerFactory(serializerFactory);
+		out.setSerializerFactory(serializerFactory);
 
-        try {
-            sk.invoke(inp, out);
-        } catch (Throwable t) {
-            throw new ServletException(t);
-        }
-    }
+		try {
+			sk.invoke(inp, out);
+		} catch (Throwable t) {
+			throw new ServletException(t);
+		}
+	}
 
-    /**
-     * 
-     * @return the publisher
-     */
-    protected HessianRemoteServicePublisher getPublisher() {
-        return Activator.getPublisher();
-    }
+	/**
+	 * 
+	 * @return the publisher
+	 */
+	protected HessianRemoteServicePublisher getPublisher() {
+		return Activator.getPublisher();
+	}
 
 }
