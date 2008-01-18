09e33a6aefd911b809bebed8ec7db7d24de1b534
Christian Campo <christian.campo@compeople.de>
2008-01-18 14:49:34 +0000

    *** empty log message ***

--- org.eclipse.riena.tests/src/org/eclipse/riena/security/services/itest/MyCallbackHandler.java
+++ org.eclipse.riena.tests/src/org/eclipse/riena/security/services/itest/MyCallbackHandler.java
@@ -28,33 +28,34 @@ public class MyCallbackHandler implements CallbackHandler {
 
 	public void handle(Callback[] callbacks) {
 		for (int i = 0; i < callbacks.length; i++) {
-			if (callbacks[i] instanceof TextOutputCallback) {
-				TextOutputCallback toc = (TextOutputCallback) callbacks[i];
-				String typeAsString;
-				// detect textoutput message type and translate it to string
-				switch (toc.getMessageType()) {
-				case TextOutputCallback.INFORMATION:
-					typeAsString = "information";
-					break;
-				case TextOutputCallback.ERROR:
-					typeAsString = "error";
-					break;
-				case TextOutputCallback.WARNING:
-					typeAsString = "warning";
-					break;
-				default:
-					typeAsString = "unknown";
-					break;
-				}
-				System.out.println(typeAsString + " " + toc.getMessage());
+			if (callbacks[i] instanceof NameCallback) {
+				NameCallback nc = (NameCallback) callbacks[i];
+				nc.setName(name);
 			} else {
-				if (callbacks[i] instanceof NameCallback) {
-					NameCallback nc = (NameCallback) callbacks[i];
-					nc.setName(name);
+				if (callbacks[i] instanceof PasswordCallback) {
+					PasswordCallback pc = (PasswordCallback) callbacks[i];
+					pc.setPassword(password.toCharArray());
 				} else {
-					if (callbacks[i] instanceof PasswordCallback) {
-						PasswordCallback pc = (PasswordCallback) callbacks[i];
-						pc.setPassword(password.toCharArray());
+					if (callbacks[i] instanceof TextOutputCallback) {
+						TextOutputCallback toc = (TextOutputCallback) callbacks[i];
+						String typeAsString;
+						// detect text output message type and translate it
+						// to string
+						switch (toc.getMessageType()) {
+						case TextOutputCallback.INFORMATION:
+							typeAsString = "information";
+							break;
+						case TextOutputCallback.ERROR:
+							typeAsString = "error";
+							break;
+						case TextOutputCallback.WARNING:
+							typeAsString = "warning";
+							break;
+						default:
+							typeAsString = "unknown";
+							break;
+						}
+						System.out.println(typeAsString + " " + toc.getMessage());
 					}
 				}
 			}
