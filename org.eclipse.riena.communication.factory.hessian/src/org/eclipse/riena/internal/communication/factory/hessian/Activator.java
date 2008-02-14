87bfaff44bfe89ada6bfa6a2e35d6c2f4ff7e747
Christian Campo <christian.campo@compeople.de>
2008-02-14 16:34:52 +0000

    add logging to hessian

--- org.eclipse.riena.communication.factory.hessian/src/org/eclipse/riena/internal/communication/factory/hessian/Activator.java
+++ org.eclipse.riena.communication.factory.hessian/src/org/eclipse/riena/internal/communication/factory/hessian/Activator.java
@@ -12,55 +12,67 @@ package org.eclipse.riena.internal.communication.factory.hessian;
 
 import java.util.Hashtable;
 
+import org.eclipse.equinox.log.Logger;
 import org.eclipse.riena.communication.core.factory.IRemoteServiceFactory;
+import org.eclipse.riena.core.logging.LogUtil;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
-
+import org.osgi.service.log.LogService;
 
 public class Activator implements BundleActivator {
 
-    private ServiceRegistration regFactory;
-    private RemoteServiceFactoryHessian factory;
+	private ServiceRegistration regFactory;
+	private RemoteServiceFactoryHessian factory;
+	private LogUtil logUtil;
+	private Logger LOGGER = null;
 
-    private static BundleContext CONTEXT;
+	private static BundleContext CONTEXT;
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
-     */
-    public void start(BundleContext context) throws Exception {
-        CONTEXT = context;
-        System.out.println("start hessian support on client");
-        factory = new RemoteServiceFactoryHessian();
-        Hashtable<String, Object> properties = new Hashtable<String, Object>(1);
-        properties.put(IRemoteServiceFactory.PROP_PROTOCOL, factory.getProtocol());
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		LOGGER = getLogger(this.getClass().getName());
+		CONTEXT = context;
+		LOGGER.log(LogService.LOG_INFO, "start hessian support on client");
+		factory = new RemoteServiceFactoryHessian();
+		Hashtable<String, Object> properties = new Hashtable<String, Object>(1);
+		properties.put(IRemoteServiceFactory.PROP_PROTOCOL, factory.getProtocol());
 
-        regFactory = context.registerService(IRemoteServiceFactory.ID, factory, properties);
-    }
+		regFactory = context.registerService(IRemoteServiceFactory.ID, factory, properties);
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
-     */
-    public void stop(BundleContext context) throws Exception {
-        regFactory.unregister();
-        regFactory = null;
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		regFactory.unregister();
+		regFactory = null;
 
-        factory.dispose();
-        CONTEXT = null;
-        System.out.println("stop hessian support on client");
-    }
+		factory.dispose();
+		CONTEXT = null;
+		LOGGER.log(LogService.LOG_INFO, "stop hessian support on client");
+	}
 
-    /**
-     * Returns the shared instance
-     * 
-     * @return the shared instance
-     */
-    public static BundleContext getContext() {
-        return CONTEXT;
-    }
+	/**
+	 * Returns the shared instance
+	 * 
+	 * @return the shared instance
+	 */
+	public static BundleContext getContext() {
+		return CONTEXT;
+	}
+
+	public Logger getLogger(String name) {
+		if (logUtil == null) {
+			logUtil = new LogUtil(CONTEXT);
+		}
+		return logUtil.getLogger(name);
+	}
 
 }
