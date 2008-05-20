a0137559854a830570b2e1b7f604bccf797612d7
Stefan Liebig <stefan.liebig@compeople.de>
2008-05-20 12:27:24 +0000

    is now a RienaPlugin

--- org.eclipse.riena.tests/src/org/eclipse/riena/internal/tests/Activator.java
+++ org.eclipse.riena.tests/src/org/eclipse/riena/internal/tests/Activator.java
@@ -10,23 +10,16 @@
  *******************************************************************************/
 package org.eclipse.riena.internal.tests;
 
-import org.eclipse.core.runtime.Plugin;
-import org.eclipse.equinox.log.Logger;
-import org.eclipse.riena.core.logging.LogUtil;
+import org.eclipse.riena.core.RienaPlugin;
 import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle
  */
-public class Activator extends Plugin {
+public class Activator extends RienaPlugin {
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "org.eclipse.riena.tests";
-
-	// The shared instance
-	private static Activator plugin;
-	private static BundleContext CONTEXT;
-	private LogUtil logUtil;
 
 	/**
 	 * The constructor
@@ -37,42 +30,21 @@ public class Activator extends Plugin {
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
+	 * @see
+	 * org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
 	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
-		plugin = this;
-		CONTEXT = context;
 	}
 
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
+	 * @see
+	 * org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
 	 */
 	public void stop(BundleContext context) throws Exception {
 		super.stop(context);
-		plugin = null;
-		CONTEXT = null;
 	}
 
-	/**
-	 * Returns the shared instance
-	 * 
-	 * @return the shared instance
-	 */
-	public static Activator getDefault() {
-		return plugin;
-	}
-
-	public static BundleContext getContext() {
-		return CONTEXT;
-	}
-
-	public synchronized Logger getLogger(String name) {
-		if (logUtil == null) {
-			logUtil = new LogUtil(CONTEXT);
-		}
-		return logUtil.getLogger(name);
-	}
 }
