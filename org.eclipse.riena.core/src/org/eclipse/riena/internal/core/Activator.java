3da7c891c21f2d9f92cd88a738fe692445d4cd8e
Stefan Liebig <stefan.liebig@compeople.de>
2008-05-20 12:23:08 +0000

    (re)moved getDefault()

--- org.eclipse.riena.core/src/org/eclipse/riena/internal/core/Activator.java
+++ org.eclipse.riena.core/src/org/eclipse/riena/internal/core/Activator.java
@@ -30,21 +30,18 @@ public class Activator extends RienaPlugin {
 	// The plug-in ID
 	public static final String PLUGIN_ID = "org.eclipse.riena.core";
 
-	// The shared instance
-	private static Activator plugin;
-
 	private ServiceRegistration configSymbolReplace;
 	private ServiceRegistration configurationPlugin;
 
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 * @see
+	 * org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext
+	 * )
 	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
-
-		plugin = this;
 		Logger LOGGER = getLogger(Activator.class.getName());
 
 		Bundle[] bundles = context.getBundles();
@@ -59,8 +56,8 @@ public class Activator extends RienaPlugin {
 				// TODO STARTING == LAZY, so start that also, STARTING is
 				// disabled, bundles with forceStart should not be LAZY
 				if (bundle.getState() == Bundle.RESOLVED/*
-														 * || bundle.getState() ==
-														 * Bundle.STARTING
+														 * || bundle.getState()
+														 * == Bundle.STARTING
 														 */) {
 					bundle.start();
 					LOGGER.log(LogService.LOG_INFO, bundle.getSymbolicName() + " forced autostart successfully");
@@ -93,24 +90,13 @@ public class Activator extends RienaPlugin {
 	}
 
 	/*
-	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 * @see
+	 * org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
 	 */
 	public void stop(BundleContext context) throws Exception {
 		((RienaStartupStatusSetter) RienaStartupStatus.getInstance()).setStarted(false);
 		configSymbolReplace.unregister();
 		configurationPlugin.unregister();
-		plugin = null;
-
 		super.stop(context);
 	}
-
-	/**
-	 * Returns the shared instance
-	 * 
-	 * @return the shared instance
-	 */
-	public static Activator getDefault() {
-		return plugin;
-	}
-
 }
