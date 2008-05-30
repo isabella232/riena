6256657bbcb21b8035f0fb1449ec8fc37270e2d1
Elias Volanakis <elias@eclipsesource.com>
2008-05-30 22:31:45 +0000

    Javadocs

--- org.eclipse.riena.ui.ridgets/src/org/eclipse/riena/ui/ridgets/ISortableByColumn.java
+++ org.eclipse.riena.ui.ridgets/src/org/eclipse/riena/ui/ridgets/ISortableByColumn.java
@@ -19,23 +19,32 @@ public interface ISortableByColumn {
 	/**
 	 * Property holding the sortability state of the individual columns:
 	 * sortable or not sortable.
+	 * <p>
+	 * Implementors firing a property change must supply the index value of the
+	 * column whose sortability was changed. The index is 0-based. The old value
+	 * is null.
 	 */
 	String PROPERTY_COLUMN_SORTABILITY = "columnSortability"; //$NON-NLS-1$
 
 	/**
 	 * Property holding the column by which the table is currently sorted.
+	 * <p>
+	 * Implementors firing a property change must supply the old and new index
+	 * value of the sorted column. The index is 0-based.
 	 */
 	String PROPERTY_SORTED_COLUMN = "sortedColumn"; //$NON-NLS-1$
 
 	/**
 	 * Property indicating whether the order in the sorted column is ascending
 	 * or descending.
+	 * <p>
+	 * Implementors firing a property change must supply the old and new value.
 	 */
 	String PROPERTY_SORT_ASCENDING = "sortAscending"; //$NON-NLS-1$
 
 	/**
 	 * @return <code>true</code> if and only if the currently sorted column is
-	 *         in ascending order. Otherwise <code>false</code>
+	 * 	in ascending order. Otherwise <code>false</code>
 	 */
 	public boolean isSortedAscending();
 
@@ -45,14 +54,14 @@ public interface ISortableByColumn {
 	 * Fires a {@link #PROPERTY_SORT_ASCENDING} event.
 	 * 
 	 * @param ascending
-	 *            <code>true</code> for ascending order, <code>false</code>
-	 *            for descending order.
+	 * 		<code>true</code> for ascending order, <code>false</code> for descending
+	 * 		order.
 	 */
 	public void setSortedAscending(boolean ascending);
 
 	/**
-	 * @return the index of the currently sorted column or <code>-1</code> if
-	 *         no column is currently sorted.
+	 * @return the index of the currently sorted column or <code>-1</code> if no
+	 * 	column is currently sorted.
 	 */
 	public int getSortedColumn();
 
@@ -64,10 +73,10 @@ public interface ISortableByColumn {
 	 * Fires a {@link #PROPERTY_SORTED_COLUMN} event.
 	 * 
 	 * @param columnIndex
-	 *            a columnIndex in the allowed range (0 &le; columnIndex &lt;
-	 *            numColumns)
-	 * @throws IllegalArgumentException
-	 *             if <code>columnIndex</code> is out of range
+	 * 		a columnIndex in the allowed range (0 &le; columnIndex &lt;
+	 * 		numColumns)
+	 * @throws RuntimeException
+	 * 		if <code>columnIndex</code> is out of range
 	 */
 	public void setSortedColumn(int columnIndex);
 
@@ -79,10 +88,10 @@ public interface ISortableByColumn {
 	 * implementors of the {@link Comparable} interface only)
 	 * 
 	 * @param columnIndex
-	 *            a columnIndex in the allowed range (0 &le; columnIndex &lt;
-	 *            numColumns)
-	 * @throws IllegalArgumentException
-	 *             if <code>columnIndex</code> is out of range
+	 * 		a columnIndex in the allowed range (0 &le; columnIndex &lt;
+	 * 		numColumns)
+	 * @throws RuntimeException
+	 * 		if <code>columnIndex</code> is out of range
 	 */
 	public boolean isColumnSortable(int columnIndex);
 
@@ -92,8 +101,8 @@ public interface ISortableByColumn {
 	 * Implementations may differ in what exactly is assumed sortable (e.g
 	 * implementors of the {@link Comparable} interface only). For example a
 	 * sortable column may allows the user to change the sort order from the UI.
-	 * Turning <em>off</em> the sortability of the currently sorted column
-	 * does not change it's sort order.
+	 * Turning <em>off</em> the sortability of the currently sorted column does
+	 * not change it's sort order.
 	 * <p>
 	 * Note that {@link #isColumnSortable(int)} is not required to return the
 	 * value set using this method as implementations are free to choose the way
@@ -102,13 +111,12 @@ public interface ISortableByColumn {
 	 * Fires a {@link #PROPERTY_COLUMN_SORTABILITY} event.
 	 * 
 	 * @param columnIndex
-	 *            a columnIndex in the allowed range (0 &le; columnIndex &lt;
-	 *            numColumns)
+	 * 		a columnIndex in the allowed range (0 &le; columnIndex &lt;
+	 * 		numColumns)
 	 * @param sortable
-	 *            <code>true</code> if the column is sortable,
-	 *            <code>false</code> otherwise
-	 * @throws IllegalArgumentException
-	 *             if <code>columnIndex</code> is out of range
+	 * 		<code>true</code> if the column is sortable, <code>false</code> otherwise
+	 * @throws RuntimeException
+	 * 		if <code>columnIndex</code> is out of range
 	 */
 	public void setColumnSortable(int columnIndex, boolean sortable);
 }
