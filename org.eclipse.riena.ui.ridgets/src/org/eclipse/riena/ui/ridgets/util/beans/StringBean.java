5d51513ff4f09310dbd68d4072d2ff902fb1ec03
Elias Volanakis <elias@eclipsesource.com>
2008-06-13 23:16:30 +0000

    Added key for value property.

--- org.eclipse.riena.ui.ridgets/src/org/eclipse/riena/ui/ridgets/util/beans/StringBean.java
+++ org.eclipse.riena.ui.ridgets/src/org/eclipse/riena/ui/ridgets/util/beans/StringBean.java
@@ -1,6 +1,6 @@
 /****************************************************************
  *                                                              *
- * Copyright (c) 2004-2005 compeople AG                              *
+ * Copyright (c) 2004-2008 compeople AG                         *
  * All rights reserved. The use of this program and the         *
  * accompanying materials are subject to license terms.         *
  *                                                              *
@@ -9,43 +9,51 @@ package org.eclipse.riena.ui.ridgets.util.beans;
 
 /**
  * String bean provides a boolean value for simple adapter UI-Binding
- *
+ * 
  * @author Alexander Ziegler
  */
 public class StringBean extends AbstractBean {
+
+	/**
+	 * Key for the value property (PROP_VALUE = "value").
+	 */
+	private static final String PROP_VALUE = "value"; //$NON-NLS-1$
 
 	private String value;
 
 	/**
 	 * Creates an String bean with a default 0
+	 * 
 	 * @param value
 	 */
 	public StringBean() {
-		value = "";
+		value = ""; //$NON-NLS-1$
 	}
 
 	/**
 	 * Creates a String bean with the given value
+	 * 
 	 * @param value
 	 */
-	public StringBean( String value ) {
+	public StringBean(String value) {
 		this.value = value;
 	}
 
 	/**
 	 * Sets the value of this bean
-	 *
-	 * @param value to set
+	 * 
+	 * @param value
+	 * 		to set
 	 */
-	public void setValue( String value ) {
+	public void setValue(String value) {
 		Object old = this.value;
 		this.value = value;
-		firePropertyChanged( "value", old, this.value );
+		firePropertyChanged(PROP_VALUE, old, this.value);
 	}
 
 	/**
 	 * Returns the value of this bean
-	 *
+	 * 
 	 * @return value
 	 */
 	public String getValue() {
