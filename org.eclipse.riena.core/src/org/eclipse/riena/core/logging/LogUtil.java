d0975435f5f1c8e14f02586191c431792d501357
Erich Achilles <erich.achilles@compeople.de>
2008-07-03 11:27:41 +0000

    changes regarding log4j config

--- org.eclipse.riena.core/src/org/eclipse/riena/core/logging/LogUtil.java
+++ org.eclipse.riena.core/src/org/eclipse/riena/core/logging/LogUtil.java
@@ -10,15 +10,16 @@
  *******************************************************************************/
 package org.eclipse.riena.core.logging;
 
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.eclipse.riena.core.injector.Inject;
-
+import org.apache.log4j.xml.DOMConfigurator;
 import org.eclipse.equinox.log.ExtendedLogReaderService;
 import org.eclipse.equinox.log.ExtendedLogService;
 import org.eclipse.equinox.log.LogFilter;
 import org.eclipse.equinox.log.Logger;
+import org.eclipse.riena.core.injector.Inject;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.service.log.LogListener;
@@ -33,6 +34,8 @@ public class LogUtil {
 	private List<LogListener> logListeners = new ArrayList<LogListener>();
 	private static boolean initialized = false;
 	private BundleContext context;
+	private static final String DEFAULT_CONFIGURATION = "/log4j.default.xml";
+	private String config = null;
 
 	public LogUtil(BundleContext context) {
 		this.context = context;
@@ -44,9 +47,29 @@ public class LogUtil {
 	 * {@link org.osgi.service.log.LogService}
 	 * 
 	 * @param name
+	 *            logger name
 	 * @return
 	 */
 	public Logger getLogger(String name) {
+		init();
+		return logService == null ? new ConsoleLogger(name) : logService.getLogger(name);
+	}
+
+	/**
+	 * Get the logger for the specified category where log4j is configured by a
+	 * given config xml.<br>
+	 * <b>Note:</b> Use the log levels defined in
+	 * {@link org.osgi.service.log.LogService}
+	 * 
+	 * @param config
+	 *            name of the config file, specified relative ( example:
+	 *            "/log4j_example.xml")
+	 * @param name
+	 *            logger name
+	 * @return
+	 */
+	public Logger getLogger(String config, String name) {
+		this.config = config;
 		init();
 		return logService == null ? new ConsoleLogger(name) : logService.getLogger(name);
 	}
@@ -97,15 +120,45 @@ public class LogUtil {
 	 */
 	private void init() {
 		synchronized (LogUtil.class) {
-			if (initialized)
-				return;
-			// TODO remove SysoLogListener if we have Log4jLogListener working
-			logListeners.add(new SysoLogListener());
-			logListeners.add(new Log4jLogListener());
+			if (!initialized) {
 
-			Inject.service(ExtendedLogService.class.getName()).useRanking().into(this).andStart(context);
-			Inject.service(ExtendedLogReaderService.class.getName()).useRanking().into(this).andStart(context);
-			initialized = true;
+				logListeners.add(new SysoLogListener());
+				logListeners.add(new Log4jLogListener());
+
+				Inject.service(ExtendedLogService.class.getName()).useRanking().into(this).andStart(context);
+				Inject.service(ExtendedLogReaderService.class.getName()).useRanking().into(this).andStart(context);
+				initialized = true;
+			}
+
+			// TODO: do we need a default ???
+
+			if (config == null) {
+				config = DEFAULT_CONFIGURATION;
+			}
+
+			// fetch the URL of given log4j configuration file via context
+			// the context is the context of the bundle from which the log was
+			// initiated
+			URL url = context.getBundle().getResource(config);
+
+			if (url != null) {
+				// workaround to fix class loader problems with log4j
+				// implementation. see "eclipse rich client platform, eclipse
+				// series, page 340.
+				Thread thread = Thread.currentThread();
+				ClassLoader loader = thread.getContextClassLoader();
+				thread.setContextClassLoader(this.getClass().getClassLoader());
+				try {
+					// configure the log4j with given config
+					DOMConfigurator.configure(url);
+				} finally {
+					thread.setContextClassLoader(loader);
+				}
+			} else {
+				// TODO: handle this ...
+
+			}
+
 		}
 	}
 
