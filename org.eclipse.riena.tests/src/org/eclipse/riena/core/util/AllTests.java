ec53cfc901e1ca7fc6b01f442806c0020c329f9a
Stefan Liebig <stefan.liebig@compeople.de>
2008-07-08 14:08:45 +0000

    new AllTests for core.util package

---
+++ org.eclipse.riena.tests/src/org/eclipse/riena/core/util/AllTests.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2007 compeople AG and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    compeople AG - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.riena.core.util;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Tests cases within package: <code>org.eclipse.riena.core.util</code>
+ */
+public class AllTests extends TestCase {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite(AllTests.class.getName());
+		// TODO fails!
+		// suite.addTestSuite(ContainerModelTest.class);
+		suite.addTestSuite(IterTest.class);
+		suite.addTestSuite(ReflectionUtilsTest.class);
+		suite.addTestSuite(StringUtilsTest.class);
+		return suite;
+	}
+
+}
