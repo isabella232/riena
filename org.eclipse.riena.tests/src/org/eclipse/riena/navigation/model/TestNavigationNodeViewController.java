02b8ddce8305e4375b95edb38d6ad0daa0011750
Erich Achilles <erich.achilles@compeople.de>
2008-07-14 08:46:22 +0000

    workareapresentationfactory and extensionpoint added for riena

---
+++ org.eclipse.riena.tests/src/org/eclipse/riena/navigation/model/TestNavigationNodeViewController.java
@@ -0,0 +1,31 @@
+/****************************************************************
+ *                                                              *
+ * Copyright (c) 2004-2008 compeople AG                         *
+ * All rights reserved. The use of this program and the         *
+ * accompanying materials are subject to license terms.         *
+ *                                                              *
+ ****************************************************************/
+package org.eclipse.riena.navigation.model;
+
+import org.eclipse.riena.navigation.INavigationNode;
+import org.eclipse.riena.navigation.ui.controllers.NavigationNodeViewController;
+
+/**
+ *
+ */
+public class TestNavigationNodeViewController extends NavigationNodeViewController {
+
+	public TestNavigationNodeViewController() {
+		super();
+
+	}
+
+	/**
+	 * @param navigationNode
+	 */
+	public TestNavigationNodeViewController(INavigationNode navigationNode) {
+		super(navigationNode);
+		// TODO Auto-generated constructor stub
+	}
+
+}
