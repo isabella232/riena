9c04d51ab06d4458bf5b99791fae1b0b7eb11c26
Thorsten Schenkel <thorsten.schenkel@compeople.de>
2008-07-23 08:38:53 +0000

    the height of the gap after the last module item of a module group was not correct

--- org.eclipse.riena.navigation.ui.swt/src/org/eclipse/riena/navigation/ui/swt/lnf/renderer/ModuleGroupRenderer.java
+++ org.eclipse.riena.navigation.ui.swt/src/org/eclipse/riena/navigation/ui/swt/lnf/renderer/ModuleGroupRenderer.java
@@ -11,6 +11,7 @@
 package org.eclipse.riena.navigation.ui.swt.lnf.renderer;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.eclipse.riena.navigation.ui.swt.component.ModuleItem;
@@ -58,11 +59,12 @@ public class ModuleGroupRenderer extends AbstractLnfRenderer {
 
 		// modules
 		Rectangle innerBorder = borderRenderer.computeInnerBounds(borderRenderer.getBounds());
-		int x = innerBorder.x + MODULE_GROUP_PADDING;
-		int y = innerBorder.y + MODULE_GROUP_PADDING;
-		int w = innerBorder.width - MODULE_GROUP_PADDING * 2;
+		int x = innerBorder.x + getModuleGroupPadding();
+		int y = innerBorder.y + getModuleGroupPadding();
+		int w = innerBorder.width - getModuleGroupPadding() * 2;
 		List<ModuleItem> modules = getItems();
-		for (ModuleItem module : modules) {
+		for (Iterator<ModuleItem> iterator = modules.iterator(); iterator.hasNext();) {
+			ModuleItem module = iterator.next();
 
 			// title bar
 			titlebarRenderer.setActive(module.isActivated());
@@ -89,7 +91,11 @@ public class ModuleGroupRenderer extends AbstractLnfRenderer {
 				titlebarBounds.height += module.getOpenHeight();
 			}
 
-			y += MODULE_MODULE_GAP;
+			if (iterator.hasNext()) {
+				y += getModuleModuleGap();
+			} else {
+				y += getModuleGroupPadding();
+			}
 
 			computeTextBounds(gc, module);
 
@@ -109,12 +115,12 @@ public class ModuleGroupRenderer extends AbstractLnfRenderer {
 	/**
 	 * Computes the size of the module group.
 	 * 
-	 * @param gc -
-	 *            <code>GC</code> of the component <code>Control</code>
-	 * @param wHint -
-	 *            the width hint
-	 * @param hHint -
-	 *            the height hint
+	 * @param gc
+	 *            - <code>GC</code> of the component <code>Control</code>
+	 * @param wHint
+	 *            - the width hint
+	 * @param hHint
+	 *            - the height hint
 	 * @return a Point representing the size of the module group
 	 */
 	public Point computeSize(GC gc, int wHint, int hHint) {
@@ -124,19 +130,24 @@ public class ModuleGroupRenderer extends AbstractLnfRenderer {
 
 		int w = wHint;
 		if (w == SWT.DEFAULT) {
-			w = borderRenderer.computeOuterWidth(MODULE_WIDTH);
+			w = borderRenderer.computeOuterWidth(getItemWidth());
 		}
 
 		List<ModuleItem> modules = getItems();
-		int h = MODULE_GROUP_PADDING;
-		for (ModuleItem module : modules) {
+		int h = getModuleGroupPadding();
+		for (Iterator<ModuleItem> iterator = modules.iterator(); iterator.hasNext();) {
+			ModuleItem module = iterator.next();
 			titlebarRenderer.setIcon(module.getIcon());
 			Point titlebarSize = titlebarRenderer.computeSize(gc, wHint, 0);
 			h += titlebarSize.y;
 			if (module.getModuleNode().isActivated()) {
 				h += module.getOpenHeight();
 			}
-			h += MODULE_MODULE_GAP;
+			if (iterator.hasNext()) {
+				h += getModuleModuleGap();
+			} else {
+				h += getModuleGroupPadding();
+			}
 		}
 		h = borderRenderer.computeOuterHeight(h);
 
@@ -144,16 +155,12 @@ public class ModuleGroupRenderer extends AbstractLnfRenderer {
 
 	}
 
-	public int getItemWidth() {
-		return MODULE_WIDTH;
-	}
-
 	/**
 	 * Returns the bounds of the close "button" of the title bar for the given
 	 * module item.
 	 * 
-	 * @param item -
-	 *            module item
+	 * @param item
+	 *            - module item
 	 * @return bounds of close "button".
 	 */
 	public Rectangle computeCloseButtonBounds(GC gc, ModuleItem item) {
@@ -173,8 +180,8 @@ public class ModuleGroupRenderer extends AbstractLnfRenderer {
 	/**
 	 * Returns the bounds of the text of the titlebar for the given module item.
 	 * 
-	 * @param item -
-	 *            module item
+	 * @param item
+	 *            - module item
 	 * @return bounds of text.
 	 */
 	public Rectangle computeTextBounds(GC gc, ModuleItem item) {
@@ -254,6 +261,33 @@ public class ModuleGroupRenderer extends AbstractLnfRenderer {
 	 */
 	public void setActivated(boolean activated) {
 		this.activated = activated;
+	}
+
+	/**
+	 * Returns the gap between to modules.
+	 * 
+	 * @return gap
+	 */
+	protected int getModuleModuleGap() {
+		return MODULE_MODULE_GAP;
+	}
+
+	/**
+	 * Returns the padding (top,left,bottom,right) of a module group.
+	 * 
+	 * @return padding
+	 */
+	protected int getModuleGroupPadding() {
+		return MODULE_GROUP_PADDING;
+	}
+
+	/**
+	 * Returns the width of a module (module item).
+	 * 
+	 * @return width
+	 */
+	public int getItemWidth() {
+		return MODULE_WIDTH;
 	}
 
 }
