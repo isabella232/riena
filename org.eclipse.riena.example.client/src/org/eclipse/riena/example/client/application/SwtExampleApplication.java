2b15bfc2348a1ee556fec25c232774fb15977bf9
Heiko Barth <heiko.barth@compeople.de>
2008-07-25 09:12:20 +0000

    - added second UIProcess SubModule for demo of context switching

--- org.eclipse.riena.example.client/src/org/eclipse/riena/example/client/application/SwtExampleApplication.java
+++ org.eclipse.riena.example.client/src/org/eclipse/riena/example/client/application/SwtExampleApplication.java
@@ -53,6 +53,7 @@ public class SwtExampleApplication extends SwtApplication {
 	 * Creates a new instance of <code>SwtExampleApplication</code> and set the
 	 * look and feel, if a class for the look and feel is given.
 	 */
+	@SuppressWarnings("unchecked")
 	public SwtExampleApplication() {
 
 		super();
@@ -93,78 +94,78 @@ public class SwtExampleApplication extends SwtApplication {
 
 		SwtPresentationManager presentation = SwtPresentationManagerAccessor.getManager();
 
-		final IApplicationModel applicationModel = new ApplicationModel("Riena Navigation Example");
+		final IApplicationModel applicationModel = new ApplicationModel("Riena Navigation Example"); //$NON-NLS-1$
 		applicationModel.setIcon(createIconPath(IExampleIcons.ICON_APPLICATION));
 		// sub application 1
-		subApplication = new SubApplication("Navigation");
+		subApplication = new SubApplication("Navigation"); //$NON-NLS-1$
 		subApplication.setIcon(createIconPath(IExampleIcons.ICON_APPLICATION));
-		presentation.present(subApplication, "subapplication.1");
+		presentation.present(subApplication, "subapplication.1"); //$NON-NLS-1$
 		applicationModel.addChild(subApplication);
 		subApplication.setSelected(true);
 
-		moduleGroup = new ModuleGroupNode("Group 1.1");
+		moduleGroup = new ModuleGroupNode("Group 1.1"); //$NON-NLS-1$
 		subApplication.addChild(moduleGroup);
-		module = new ModuleNode("Module 1.1.1");
+		module = new ModuleNode("Module 1.1.1"); //$NON-NLS-1$
 		module.setIcon(createIconPath(IExampleIcons.ICON_APPLICATION));
 		moduleGroup.addChild(module);
-		subModule = new SubModuleNode("SubModule 1.1.1.1");
+		subModule = new SubModuleNode("SubModule 1.1.1.1"); //$NON-NLS-1$
 		subModule.setIcon(createIconPath(IExampleIcons.ICON_FILE));
-		presentation.present(subModule, "customerDetailView");
+		presentation.present(subModule, "customerDetailView"); //$NON-NLS-1$
 		module.addChild(subModule);
 
-		SubModuleNode subModule2 = new SubModuleNode("SubModule 1.1.1.1");
-		presentation.present(subModule2, "customerDetailView");
+		SubModuleNode subModule2 = new SubModuleNode("SubModule 1.1.1.1"); //$NON-NLS-1$
+		presentation.present(subModule2, "customerDetailView"); //$NON-NLS-1$
 		subModule.addChild(subModule2);
 
-		subModule = new SubModuleNode("SubModule 1.1.1.2");
-		presentation.present(subModule, "customerDetailView");
+		subModule = new SubModuleNode("SubModule 1.1.1.2"); //$NON-NLS-1$
+		presentation.present(subModule, "customerDetailView"); //$NON-NLS-1$
 		module.addChild(subModule);
-		module = new ModuleNode("Module 1.1.2 (closeable)");
+		module = new ModuleNode("Module 1.1.2 (closeable)"); //$NON-NLS-1$
 		module.setIcon(createIconPath(IExampleIcons.ICON_HOMEFOLDER));
 		moduleGroup.addChild(module);
-		subModule = new SubModuleNode("SubModule 1.1.2.1");
-		presentation.present(subModule, "customerDetailView");
+		subModule = new SubModuleNode("SubModule 1.1.2.1"); //$NON-NLS-1$
+		presentation.present(subModule, "customerDetailView"); //$NON-NLS-1$
 		module.addChild(subModule);
 		/* NEW */
-		subModule = new SubModuleNode("Navigation");
+		subModule = new SubModuleNode("Navigation"); //$NON-NLS-1$
 		presentation.registerView(NavigationSubModuleView.ID, false);
 		presentation.present(subModule, NavigationSubModuleView.ID);
 		module.addChild(subModule);
 
-		moduleGroup = new ModuleGroupNode("Group 1.2");
+		moduleGroup = new ModuleGroupNode("Group 1.2"); //$NON-NLS-1$
 		moduleGroup.setPresentWithSingleModule(false);
 		subApplication.addChild(moduleGroup);
-		module = new ModuleNode("Module 1.2.1 (not closeable)");
+		module = new ModuleNode("Module 1.2.1 (not closeable)"); //$NON-NLS-1$
 		module.setCloseable(false);
 		module.setIcon(createIconPath(IExampleIcons.ICON_RED_LED));
 		moduleGroup.addChild(module);
-		subModule = new SubModuleNode("SubModule 1.2.1.1");
-		presentation.present(subModule, "customerDetailView");
+		subModule = new SubModuleNode("SubModule 1.2.1.1"); //$NON-NLS-1$
+		presentation.present(subModule, "customerDetailView"); //$NON-NLS-1$
 		module.addChild(subModule);
-		subModule = new SubModuleNode("SubModule 1.2.1.2");
-		presentation.present(subModule, "customerDetailView");
+		subModule = new SubModuleNode("SubModule 1.2.1.2"); //$NON-NLS-1$
+		presentation.present(subModule, "customerDetailView"); //$NON-NLS-1$
 		module.addChild(subModule);
 
 		// Playground
-		subApplication = new SubApplication("Playground");
+		subApplication = new SubApplication("Playground"); //$NON-NLS-1$
 		subApplication.setIcon(createIconPath(IExampleIcons.ICON_SAMPLE));
-		presentation.present(subApplication, "subapplication.2");
+		presentation.present(subApplication, "subapplication.2"); //$NON-NLS-1$
 		applicationModel.addChild(subApplication);
 
-		moduleGroup = new ModuleGroupNode("Group 2.2");
+		moduleGroup = new ModuleGroupNode("Group 2.2"); //$NON-NLS-1$
 		subApplication.addChild(moduleGroup);
 
 		// shared view demo
 		presentation.registerView(SharedViewDemoView.ID, true);
-		IModuleNode sharedViewModule = new ModuleNode("Shared View Demo");
+		IModuleNode sharedViewModule = new ModuleNode("Shared View Demo"); //$NON-NLS-1$
 		module.setIcon(createIconPath(IExampleIcons.ICON_SAMPLE));
 		moduleGroup.addChild(sharedViewModule);
 
-		ISubModuleNode sharedViewSm1 = new SubModuleNode("Node 1");
+		ISubModuleNode sharedViewSm1 = new SubModuleNode("Node 1"); //$NON-NLS-1$
 		presentation.present(sharedViewSm1, SharedViewDemoView.ID);
 		sharedViewModule.addChild(sharedViewSm1);
 
-		ISubModuleNode sharedViewSm2 = new SubModuleNode("Node 2");
+		ISubModuleNode sharedViewSm2 = new SubModuleNode("Node 2"); //$NON-NLS-1$
 		presentation.present(sharedViewSm2, SharedViewDemoView.ID);
 		sharedViewModule.addChild(sharedViewSm2);
 
@@ -172,61 +173,66 @@ public class SwtExampleApplication extends SwtApplication {
 		IModuleNode uiProcessModule = new ModuleNode("UIProcess"); //$NON-NLS-1$
 		moduleGroup.addChild(uiProcessModule);
 
-		ISubModuleNode uiPSubModule = new SubModuleNode("Demo"); //$NON-NLS-1$
+		ISubModuleNode uiPSubModule = new SubModuleNode("Demo1"); //$NON-NLS-1$
+		presentation.registerView(UiProcessDemoView.ID, false);
+		presentation.present(uiPSubModule, UiProcessDemoView.ID);
+		uiProcessModule.addChild(uiPSubModule);
+
+		uiPSubModule = new SubModuleNode("Demo2"); //$NON-NLS-1$
 		presentation.registerView(UiProcessDemoView.ID, false);
 		presentation.present(uiPSubModule, UiProcessDemoView.ID);
 		uiProcessModule.addChild(uiPSubModule);
 
 		// playground
-		IModuleNode playgroundModule = new ModuleNode("Playground");
+		IModuleNode playgroundModule = new ModuleNode("Playground"); //$NON-NLS-1$
 		moduleGroup.addChild(playgroundModule);
 
-		ISubModuleNode buttonsSubModule = new SubModuleNode("Buttons");
+		ISubModuleNode buttonsSubModule = new SubModuleNode("Buttons"); //$NON-NLS-1$
 		presentation.registerView(RidgetsSubModuleView.ID, false);
 		presentation.present(buttonsSubModule, RidgetsSubModuleView.ID);
 		playgroundModule.addChild(buttonsSubModule);
 
-		ISubModuleNode comboSubModule = new SubModuleNode("Combo");
+		ISubModuleNode comboSubModule = new SubModuleNode("Combo"); //$NON-NLS-1$
 		presentation.registerView(ComboView.ID, false);
 		presentation.present(comboSubModule, ComboView.ID);
 		playgroundModule.addChild(comboSubModule);
 
-		ISubModuleNode listSubModule = new SubModuleNode("List");
+		ISubModuleNode listSubModule = new SubModuleNode("List"); //$NON-NLS-1$
 		presentation.registerView(ListView.ID, false);
 		presentation.present(listSubModule, ListView.ID);
 		playgroundModule.addChild(listSubModule);
 
-		ISubModuleNode textSubModule = new SubModuleNode("Text");
+		ISubModuleNode textSubModule = new SubModuleNode("Text"); //$NON-NLS-1$
 		presentation.registerView(TextView.ID, false);
 		presentation.present(textSubModule, TextView.ID);
 		playgroundModule.addChild(textSubModule);
 
-		ISubModuleNode markerSubModule = new SubModuleNode("Marker");
+		ISubModuleNode markerSubModule = new SubModuleNode("Marker"); //$NON-NLS-1$
 		presentation.registerView(MarkerView.ID, false);
 		presentation.present(markerSubModule, MarkerView.ID);
 		playgroundModule.addChild(markerSubModule);
 
-		ISubModuleNode focusableSubModule = new SubModuleNode("Focusable");
+		ISubModuleNode focusableSubModule = new SubModuleNode("Focusable"); //$NON-NLS-1$
 		presentation.registerView(FocusableView.ID, false);
 		presentation.present(focusableSubModule, FocusableView.ID);
 		playgroundModule.addChild(focusableSubModule);
 
-		ISubModuleNode validationSubModule = new SubModuleNode("Validation");
+		ISubModuleNode validationSubModule = new SubModuleNode("Validation"); //$NON-NLS-1$
 		presentation.registerView(ValidationView.ID, false);
 		presentation.present(validationSubModule, ValidationView.ID);
 		playgroundModule.addChild(validationSubModule);
 
-		ISubModuleNode systemPropertiesSubModule = new SubModuleNode("System Properties");
+		ISubModuleNode systemPropertiesSubModule = new SubModuleNode("System Properties"); //$NON-NLS-1$
 		presentation.registerView(SystemPropertiesView.ID, false);
 		presentation.present(systemPropertiesSubModule, SystemPropertiesView.ID);
 		playgroundModule.addChild(systemPropertiesSubModule);
 
-		ISubModuleNode treeSubModule = new SubModuleNode("Tree");
+		ISubModuleNode treeSubModule = new SubModuleNode("Tree"); //$NON-NLS-1$
 		presentation.registerView(TreeView.ID, false);
 		presentation.present(treeSubModule, TreeView.ID);
 		playgroundModule.addChild(treeSubModule);
 
-		ISubModuleNode treeTableSubModule = new SubModuleNode("Tree Table");
+		ISubModuleNode treeTableSubModule = new SubModuleNode("Tree Table"); //$NON-NLS-1$
 		presentation.registerView(TreeTableView.ID, false);
 		presentation.present(treeTableSubModule, TreeTableView.ID);
 		playgroundModule.addChild(treeTableSubModule);
