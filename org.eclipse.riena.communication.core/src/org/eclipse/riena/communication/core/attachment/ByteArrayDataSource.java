6ad548a07781c5044dd35721bf160a05a1bcc5df
Stefan Liebig <stefan.liebig@compeople.de>
2008-07-30 08:42:27 +0000

    unit test no longer fails

--- org.eclipse.riena.communication.core/src/org/eclipse/riena/communication/core/attachment/ByteArrayDataSource.java
+++ org.eclipse.riena.communication.core/src/org/eclipse/riena/communication/core/attachment/ByteArrayDataSource.java
@@ -58,6 +58,8 @@ public class ByteArrayDataSource implements IDataSource {
 	 */
 	public InputStream getInputStream() throws IOException {
 		// return new ByteArrayInputStream( internalBuffer );
+		if (input == null)
+			return null;
 		input.reset();
 		return input;
 	}
