f17111be87196aff7e6a8c39da1d5ebeff58f128
Stefan Liebig <stefan.liebig@compeople.de>
2008-08-06 12:14:35 +0000

    uses VariableManagerUtil instead of ConfigurationPlugin

--- org.eclipse.riena.communication.core/src/org/eclipse/riena/communication/core/factory/RemoteServiceFactory.java
+++ org.eclipse.riena.communication.core/src/org/eclipse/riena/communication/core/factory/RemoteServiceFactory.java
@@ -13,23 +13,22 @@ package org.eclipse.riena.communication.core.factory;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.util.Hashtable;
 
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.equinox.log.Logger;
 import org.eclipse.riena.communication.core.IRemoteServiceReference;
 import org.eclipse.riena.communication.core.IRemoteServiceRegistration;
 import org.eclipse.riena.communication.core.IRemoteServiceRegistry;
 import org.eclipse.riena.communication.core.RemoteServiceDescription;
-import org.eclipse.riena.communication.core.publisher.RSDPublisherProperties;
 import org.eclipse.riena.core.RienaStartupStatus;
 import org.eclipse.riena.core.injector.Inject;
+import org.eclipse.riena.core.util.VariableManagerUtil;
 import org.eclipse.riena.internal.communication.core.Activator;
 import org.eclipse.riena.internal.communication.core.factory.CallHooksProxy;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.framework.ServiceReference;
 import org.osgi.framework.ServiceRegistration;
-import org.osgi.service.cm.ConfigurationPlugin;
 import org.osgi.service.cm.ManagedService;
 import org.osgi.service.log.LogService;
 
@@ -43,7 +42,7 @@ import org.osgi.service.log.LogService;
  * in one step call {@link #createAndRegisterProxy(..)}
  * <p>
  * This RemoteServiceFactory do not create IRemoteServiceReference itself. This
- * RemoteServiceFactory delegates the behaviour to a protocol specifically
+ * RemoteServiceFactory delegates the behavior to a protocol specifically
  * {@link IRemoteServiceFactory} OSGi Service. This RemoteServiceFactory finds a
  * corresponding IRemoteServiceFactory by the filter
  * "riena.remote.protocol=[aProtocol]" (e.g. aProtcol replaced with 'hessian').
@@ -55,7 +54,7 @@ import org.osgi.service.log.LogService;
  * The Riena communication bundle content includes generic class loading and
  * object instantiation or delegates this behavior to other Riena communication
  * bundles. Riena supports Eclipse-BuddyPolicy concept. For further information
- * about Riena class loading and instanciation please read /readme.txt.
+ * about Riena class loading and instantiation please read /readme.txt.
  * 
  * @author Alexander Ziegler
  * @author Christian Campo
@@ -63,10 +62,10 @@ import org.osgi.service.log.LogService;
  */
 public class RemoteServiceFactory {
 
-	private static final String HOST_ID = RemoteServiceFactory.class.getName();
 	private BundleContext context;
-	private Logger LOGGER = Activator.getDefault().getLogger(RemoteServiceFactory.class.getName());
 	private IRemoteServiceRegistry registry;
+	private static final String HOST_ID = RemoteServiceFactory.class.getName();
+	private final static Logger LOGGER = Activator.getDefault().getLogger(RemoteServiceFactory.class.getName());
 
 	/**
 	 * Creates a RemoteServiceFactory instance with the default bundle context.
@@ -159,8 +158,8 @@ public class RemoteServiceFactory {
 	}
 
 	/**
-	 * Creates and registers a protocol specifically remote service reference
-	 * and registers the reference into the {@link IRemoteServiceRegistry}. A
+	 * Creates and registers a protocol specific remote service reference and
+	 * registers the reference into the {@link IRemoteServiceRegistry}. A
 	 * registered reference becomes automatically registered as "remote" OSGi
 	 * Service within the local OSGi container. Answers the registration object
 	 * for the reference. If no protocol specific {@link IRemoteServiceFactory}
@@ -180,10 +179,9 @@ public class RemoteServiceFactory {
 			rsRef = createLazyProxy(rsDesc);
 		}
 		if (rsRef == null) {
-			LOGGER
-					.log(LogService.LOG_ERROR,
-							"could not create serviceInstance (neither serviceInstance nor lazy serviceInstance) for "
-									+ rsDesc);
+			LOGGER.log(LogService.LOG_ERROR,
+					"could not create serviceInstance (neither serviceInstance nor lazy serviceInstance) for " //$NON-NLS-1$
+							+ rsDesc);
 			return null;
 		}
 		rsRef.setHostId(hostId);
@@ -193,14 +191,14 @@ public class RemoteServiceFactory {
 			return reg;
 		}
 		return null;
-
 	}
 
 	/**
-	 * Creates a protocol specifically serviceInstance reference
+	 * Creates a protocol specific serviceInstance reference
 	 * (IRemoteServcieRefernce) with given end point parameters. Answers the
-	 * IRemoteServiceReference. If no protocol specific {@link
-	 * IRemoteServiceFactory} OSGI Service available answers <code>null</code>.
+	 * IRemoteServiceReference. If no protocol specific
+	 * {@link IRemoteServiceFactory} OSGI Service available answers
+	 * <code>null</code>.
 	 * 
 	 * @param serviceClass
 	 * @param url
@@ -224,7 +222,7 @@ public class RemoteServiceFactory {
 	}
 
 	/**
-	 * Creates a protocol specifically IRemoteServcieRefernce for the given end
+	 * Creates a protocol specific IRemoteServcieRefernce for the given end
 	 * point description. Answers the IRemoteServiceReference.. If no end point
 	 * specific {@link IRemoteServiceFactory} OSGI Service available answers
 	 * <code>null</code>.
@@ -236,24 +234,13 @@ public class RemoteServiceFactory {
 		if (!RienaStartupStatus.getInstance().isStarted()) {
 			LOGGER.log(LogService.LOG_WARNING, "riena.core is not started. This will probably not work."); //$NON-NLS-1$
 		}
-		// consult ConfigurationPlugins for URL
+
 		try {
-			ServiceReference[] pluginRefs = Activator.getDefault().getContext().getServiceReferences(
-					ConfigurationPlugin.class.getName(), null);
-			Hashtable<String, String> props = new Hashtable<String, String>();
-			props.put(RSDPublisherProperties.PROP_URL, rsd.getURL());
-			if (pluginRefs != null) {
-				for (ServiceReference pluginRef : pluginRefs) {
-					ConfigurationPlugin plugin = (ConfigurationPlugin) Activator.getDefault().getContext().getService(
-							pluginRef);
-					if (plugin != null) {
-						plugin.modifyConfiguration(null, props);
-					}
-				}
-			}
-			rsd.setURL(props.get(RSDPublisherProperties.PROP_URL));
-		} catch (InvalidSyntaxException e1) {
-			e1.printStackTrace();
+			// Substitute any ${refs} within the url
+			rsd.setURL(VariableManagerUtil.substitute(rsd.getURL()));
+		} catch (CoreException e) {
+			LOGGER.log(LogService.LOG_ERROR, "Could not substitute url '" + rsd.getURL() + "'.", e); //$NON-NLS-1$ //$NON-NLS-2$
+			return null;
 		}
 		ServiceReference[] references;
 		if (rsd.getProtocol() == null) {
@@ -324,8 +311,8 @@ public class RemoteServiceFactory {
 					Activator.getDefault().getContext());
 			return ref;
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			LOGGER.log(LogService.LOG_ERROR, "Could not load service interface class '" //$NON-NLS-1$
+					+ rsd.getServiceInterfaceClassName() + "'.", e); //$NON-NLS-1$
 			return null;
 		}
 	}
@@ -390,8 +377,8 @@ public class RemoteServiceFactory {
 		public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 			if (delegateHandler == null) {
 				throw new RuntimeException(
-						"LazyProxy: missing IRemoteServiceFactory to create proxy for " + "protocol=" + rsd.getProtocol() + " url=" //$NON-NLS-1$ //$NON-NLS-2$
-								+ rsd.getURL() + " interface=" + rsd.getServiceInterfaceClassName());
+						"LazyProxy: missing IRemoteServiceFactory to create proxy for " + "protocol=" + rsd.getProtocol() + " url=" //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+								+ rsd.getURL() + " interface=" + rsd.getServiceInterfaceClassName()); //$NON-NLS-1$
 			}
 
 			return delegateHandler.invoke(proxy, method, args);
