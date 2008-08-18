838dbd49613ab31fcd931cbb3c0a2c7c17dc8226
Carsten Drossel <carsten.drossel@compeople.de>
2008-08-18 15:01:49 +0000

    fixed test

--- org.eclipse.riena.tests/src/org/eclipse/riena/navigation/ui/swt/views/SubModuleViewTest.java
+++ org.eclipse.riena.tests/src/org/eclipse/riena/navigation/ui/swt/views/SubModuleViewTest.java
@@ -11,13 +11,17 @@
 package org.eclipse.riena.navigation.ui.swt.views;
 
 import org.eclipse.riena.navigation.IApplicationModel;
+import org.eclipse.riena.navigation.IModuleNode;
 import org.eclipse.riena.navigation.ISubModuleNode;
 import org.eclipse.riena.navigation.model.ApplicationModel;
+import org.eclipse.riena.navigation.model.ModuleNode;
 import org.eclipse.riena.navigation.model.NavigationNodeId;
 import org.eclipse.riena.navigation.model.SubModuleNode;
+import org.eclipse.riena.navigation.ui.controllers.ModuleController;
 import org.eclipse.riena.navigation.ui.controllers.SubModuleController;
 import org.eclipse.riena.tests.RienaTestCase;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Shell;
 
 /**
  * Tests for the SubModuleNodeView.
@@ -33,7 +37,9 @@ public class SubModuleViewTest extends RienaTestCase {
 		addPluginXml(SubModuleViewTest.class, "SubModuleViewTest.xml");
 
 		subModuleNodeView = new TestView();
-		node = new SubModuleNode(new NavigationNodeId("testId")) {
+		node = new SubModuleNode(new NavigationNodeId("testId"), "TestSubModuleLabel");
+
+		IModuleNode parent = new ModuleNode(null, "TestModuleLabel") {
 			@Override
 			public <T> T getTypecastedAdapter(Class<T> clazz) {
 				if (clazz.equals(IApplicationModel.class)) {
@@ -42,12 +48,13 @@ public class SubModuleViewTest extends RienaTestCase {
 				return null;
 			}
 		};
-		node.setLabel("Test");
+		parent.setPresentation(new ModuleController(parent));
+		parent.addChild(node);
 	}
 
 	public void testCreateController() throws Exception {
 
-		subModuleNodeView.createPartControl(null);
+		subModuleNodeView.createPartControl(new Shell());
 
 		assertTrue(subModuleNodeView.getController() instanceof SubModuleController);
 		assertEquals(node, subModuleNodeView.getController().getNavigationNode());
