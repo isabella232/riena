b977db05b4dca7bca0791b2119707ddbd3dc4362
Stefan Liebig <stefan.liebig@compeople.de>
2008-08-25 11:32:16 +0000

    Without this comments the application fails switching sub-applications!!!

--- org.eclipse.riena.navigation.ui/src/org/eclipse/riena/navigation/ui/controllers/SubApplicationController.java
+++ org.eclipse.riena.navigation.ui/src/org/eclipse/riena/navigation/ui/controllers/SubApplicationController.java
@@ -292,9 +292,11 @@ public class SubApplicationController extends NavigationNodeController<ISubAppli
 	 * @return the statuslineRidget
 	 */
 	public IStatuslineRidget getStatuslineRidget() {
-		if (statuslineRidget == null) {
-			statuslineRidget = (IStatuslineRidget) getRidget("statuslineRidget"); //$NON-NLS-1$
-		}
+		// TODO Without this comments the application fails switching
+		// sub-applications!!!
+		// if (statuslineRidget == null) {
+		//			statuslineRidget = (IStatuslineRidget) getRidget("statuslineRidget"); //$NON-NLS-1$
+		// }
 		return statuslineRidget;
 	}
 
