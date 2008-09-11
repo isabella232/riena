d6bb2862893c9e42c3da03cbb6eebc3da50e0dde
Thorsten Schenkel <thorsten.schenkel@compeople.de>
2008-09-11 11:23:54 +0000

    to of not existing method removed

--- org.eclipse.riena.tests/src/org/eclipse/riena/ui/swt/lnf/renderer/EmbeddedTitlebarRendererTest.java
+++ org.eclipse.riena.tests/src/org/eclipse/riena/ui/swt/lnf/renderer/EmbeddedTitlebarRendererTest.java
@@ -10,15 +10,8 @@
  *******************************************************************************/
 package org.eclipse.riena.ui.swt.lnf.renderer;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import junit.framework.TestCase;
 
-import org.eclipse.riena.core.marker.IMarker;
-import org.eclipse.riena.core.util.ReflectionUtils;
-import org.eclipse.riena.ui.core.marker.ErrorMarker;
-import org.eclipse.riena.ui.core.marker.UIProcessFinishedMarker;
 import org.eclipse.riena.ui.swt.lnf.ILnfKeyConstants;
 import org.eclipse.riena.ui.swt.lnf.LnfManager;
 import org.eclipse.swt.graphics.Font;
@@ -54,36 +47,6 @@ public class EmbeddedTitlebarRendererTest extends TestCase {
 		font.dispose();
 		gc.dispose();
 		shell.dispose();
-
-	}
-
-	/**
-	 * Tests the method {@code isProcessMarkerVisible()}.
-	 */
-	public void testIsProcessMarkerVisible() {
-
-		EmbeddedTitlebarRenderer renderer = new EmbeddedTitlebarRenderer();
-		renderer.setMarkers(null);
-		boolean ret = ReflectionUtils.invokeHidden(renderer, "isProcessMarkerVisible", new Object[] {});
-		assertFalse(ret);
-
-		List<IMarker> markers = new ArrayList<IMarker>(2);
-		ErrorMarker errorMarker = new ErrorMarker();
-		markers.add(errorMarker);
-		renderer.setMarkers(markers);
-		ret = ReflectionUtils.invokeHidden(renderer, "isProcessMarkerVisible", new Object[] {});
-		assertFalse(ret);
-
-		UIProcessFinishedMarker finishedMarker = new UIProcessFinishedMarker();
-		markers.add(finishedMarker);
-		renderer.setMarkers(markers);
-		finishedMarker.setOn(false);
-		ret = ReflectionUtils.invokeHidden(renderer, "isProcessMarkerVisible", new Object[] {});
-		assertFalse(ret);
-
-		finishedMarker.setOn(true);
-		ret = ReflectionUtils.invokeHidden(renderer, "isProcessMarkerVisible", new Object[] {});
-		assertTrue(ret);
 
 	}
 
