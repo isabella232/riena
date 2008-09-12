ecebb33014cde27c62fec3106a595789983a84a1
Christian Campo <christian.campo@compeople.de>
2008-09-12 07:47:38 +0000

    removed code that was not active (in comments) or no longer called

--- org.eclipse.riena.communication.publisher/src/org/eclipse/riena/internal/communication/publisher/Activator.java
+++ org.eclipse.riena.communication.publisher/src/org/eclipse/riena/internal/communication/publisher/Activator.java
@@ -12,26 +12,16 @@ package org.eclipse.riena.internal.communication.publisher;
 
 import org.eclipse.riena.communication.core.publisher.IServicePublishBinder;
 import org.eclipse.riena.communication.core.publisher.IServicePublisher;
-import org.eclipse.riena.communication.core.publisher.RSDPublisherProperties;
-import org.eclipse.riena.communication.core.util.CommunicationUtil;
 import org.eclipse.riena.communication.publisher.Publish;
 import org.eclipse.riena.communication.publisher.ServicePublishBinder;
 import org.eclipse.riena.core.RienaActivator;
 import org.eclipse.riena.core.injector.Inject;
-import org.eclipse.riena.core.service.ServiceInjector;
 
-import org.eclipse.equinox.log.Logger;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceEvent;
-import org.osgi.framework.ServiceListener;
-import org.osgi.framework.ServiceReference;
 
 public class Activator extends RienaActivator {
 
-	private ServicePublishEventDispatcher dispatcher;
-	private ServiceInjector publisherInjector;
-	private UpdateNotifierRemoteService updateNotifierRemoteService;
-	private Logger logger;
+	//	private Logger logger;
 
 	// The shared instance
 	private static Activator plugin;
@@ -45,62 +35,13 @@ public class Activator extends RienaActivator {
 		super.start(context);
 
 		Activator.plugin = this;
-		logger = getLogger(Activator.class.getName());
+		//		logger = getLogger(Activator.class.getName());
 
 		IServicePublishBinder binder = new ServicePublishBinder();
 		context.registerService(IServicePublishBinder.class.getName(), binder, null);
 		Inject.service(IServicePublisher.class.getName()).into(binder).andStart(context);
 
 		Publish.allServices().useFilter("(&(riena.remote=true)(riena.remote.protocol=*))").andStart(context); //$NON-NLS-1$
-
-		// dispatcher = new ServicePublishEventDispatcher(context);
-		// publisherInjector =
-		// Inject.service(IServicePublisher.class.getName()).useRanking().into(
-		// dispatcher).andStart(
-		// context);
-		//
-		// // register as OSGi service, the start will pick up the OSGi service
-		// and
-		// // publish it
-		// Dictionary<String, Object> properties =
-		// ServiceDescriptor.newDefaultServiceProperties();
-		// properties.put(RSDPublisherProperties.PROP_IS_REMOTE,
-		// Boolean.TRUE.toString());
-		// properties.put(RSDPublisherProperties.PROP_REMOTE_PROTOCOL,
-		// "hessian");
-		// properties.put(RSDPublisherProperties.PROP_REMOTE_PATH,
-		// "/ServicePublisherWS");
-		//
-		//context.registerService(IServicePublishEventDispatcher.class.getName()
-		// ,
-		// dispatcher, properties);
-
-		// dispatcher.start();
-
-		// register UpdateNotified so all services trigger the
-		// servicePublishEventDispatcher
-
-		// register a service hook which is called then for every webservice
-
-		// context.registerService(IServiceHook.class.getName(), new
-		// IServiceHook() {
-		//
-		// public void afterService(ServiceContext context) {
-		// logger.log(LogService.LOG_DEBUG, "after service (in hook)");
-		// context.getMessageContext().addResponseHeader("Set-Cookie",
-		// "x-scpserver-test-sessionid=11");
-		// }
-		//
-		// public void beforeService(ServiceContext context) {
-		// logger.log(LogService.LOG_DEBUG, "before service (in hook)");
-		// Map<String, List<String>> headers =
-		// context.getMessageContext().listRequestHeaders();
-		// for (Map.Entry<String, List<String>> entry : headers.entrySet()) {
-		// logger.log(LogService.LOG_DEBUG, "header: name:" + entry.getKey() +
-		// " value: " + entry.getValue());
-		// }
-		// }
-		// }, null);
 	}
 
 	/*
@@ -110,43 +51,8 @@ public class Activator extends RienaActivator {
 	 * org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
 	 */
 	public void stop(BundleContext context) throws Exception {
-		// context.removeServiceListener(updateNotifierRemoteService);
-		dispatcher.stop();
-		dispatcher = null;
-		publisherInjector.stop();
-		publisherInjector = null;
-		updateNotifierRemoteService = null;
-
 		Activator.plugin = null;
 		super.stop(context);
-	}
-
-	static class UpdateNotifierRemoteService implements ServiceListener {
-		ServicePublishEventDispatcher dispatcher;
-
-		UpdateNotifierRemoteService(ServicePublishEventDispatcher dispatcher) {
-			this.dispatcher = dispatcher;
-		}
-
-		public void serviceChanged(ServiceEvent event) {
-			ServiceReference serviceRef = event.getServiceReference();
-
-			// check if we are getting the event for a new remote service
-			String isRemote = CommunicationUtil.accessProperty(serviceRef
-					.getProperty(RSDPublisherProperties.PROP_IS_REMOTE), null);
-			String remoteType = CommunicationUtil.accessProperty(serviceRef
-					.getProperty(RSDPublisherProperties.PROP_REMOTE_PROTOCOL), null);
-			if (remoteType == null || remoteType.length() == 0) {
-				return;
-			}
-			if (Boolean.parseBoolean(isRemote)) {
-				if (event.getType() == ServiceEvent.UNREGISTERING) {
-					dispatcher.unpublish(event.getServiceReference());
-				} else if (event.getType() == ServiceEvent.REGISTERED) {
-					dispatcher.publish(event.getServiceReference());
-				}
-			}
-		}
 	}
 
 	/**
