1b2b53c1dcd577650e185152145817ed49dbd29e
Stefan Liebig <stefan.liebig@compeople.de>
2008-11-28 08:00:28 +0000

    asArray allows meta characters asMap and asArray allow passing of an Object type

--- org.eclipse.riena.core/src/org/eclipse/riena/core/util/PropertiesUtils.java
+++ org.eclipse.riena.core/src/org/eclipse/riena/core/util/PropertiesUtils.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
 import java.util.StringTokenizer;
@@ -52,8 +53,13 @@ public final class PropertiesUtils {
 	}
 
 	/**
-	 * Transform the string representation of a map into a map and optionally
-	 * check the existence of the specified expected keys.
+	 * Check the given data object and if it is a {@code Hashtable} as it may be
+	 * provided by the {@code IExecutableExtension.setInitializationData()}
+	 * method return the {@code Hashtable} with optional checking of the
+	 * expected keys.<br>
+	 * If the data object is a {@code String} than transform the given string
+	 * representation of a map into a map and optionally check the existence of
+	 * the expected keys.
 	 * 
 	 * <pre>
 	 * The format of the string is: 
@@ -61,58 +67,114 @@ public final class PropertiesUtils {
 	 * pair := key = value
 	 * </pre>
 	 * 
-	 * @param stringified
+	 * @param data
+	 *            this can be the data parameter of the
+	 *            IExecutableExtension.setInitializationData
+	 *            (IConfigurationElement config, String propertyName, Object
+	 *            data) method.
 	 * @param expectedKeys
-	 * @return
+	 * @return a map
+	 * @throws IllegalArgumentException
+	 *             for any errors
+	 * @see IExecutableExtension.setInitializationData()
 	 */
-	public static Map<String, String> asMap(String stringified, String... expectedKeys) {
-		if (StringUtils.isEmpty(stringified)) {
-			Assert.isLegal(expectedKeys.length == 0,
-					"Excpeted keys " + Arrays.toString(expectedKeys) + " not found in empty string."); //$NON-NLS-1$ //$NON-NLS-2$
-			return Collections.emptyMap();
+	@SuppressWarnings("unchecked")
+	public static Map<String, String> asMap(Object data, String... expectedKeys) {
+		Map<String, String> result = null;
+		if (data == null) {
+			result = Collections.emptyMap();
+		} else if (data instanceof Hashtable) {
+			result = (Hashtable<String, String>) data;
+		} else if (data instanceof String) {
+			String stringified = (String) data;
+			result = new HashMap<String, String>();
+			if (StringUtils.isEmpty(stringified)) {
+				Assert.isLegal(expectedKeys.length == 0,
+						"Excpeted keys " + Arrays.toString(expectedKeys) + " not found in empty string."); //$NON-NLS-1$ //$NON-NLS-2$
+				return Collections.emptyMap();
+			}
+			StringTokenizer parts = new StringTokenizer(stringified, ","); //$NON-NLS-1$
+			while (parts.hasMoreTokens()) {
+				String part = parts.nextToken();
+				int equal = part.indexOf('=');
+				Assert.isLegal(equal > 0, "Error within definition. Expecting a string of the form: " //$NON-NLS-1$
+						+ " [ <key> \"=\" <value> ] { [ \",\" <key> \"=\" <value> ] }"); //$NON-NLS-1$
+				result.put(part.substring(0, equal).trim(), part.substring(equal + 1).trim());
+			}
+		} else {
+			Assert.isLegal(false, "Can not deal with data type: " + data.getClass().getName()); //$NON-NLS-1$
 		}
-		Map<String, String> result = new HashMap<String, String>();
-		StringTokenizer parts = new StringTokenizer(stringified, ","); //$NON-NLS-1$
-		while (parts.hasMoreTokens()) {
-			String part = parts.nextToken();
-			int equal = part.indexOf('=');
-			Assert.isLegal(equal > 0, "Error within definition. Expecting a string of the form: " //$NON-NLS-1$
-					+ " [ <key> \"=\" <value> ] { [ \",\" <key> \"=\" <value> ] }"); //$NON-NLS-1$
-			result.put(part.substring(0, equal).trim(), part.substring(equal + 1).trim());
-		}
-
+		// validate expected keys
 		for (String expectedKey : expectedKeys) {
-			Assert.isLegal(result.containsKey(expectedKey), "Map " + stringified + "does not contain expected key " //$NON-NLS-1$ //$NON-NLS-2$
+			Assert.isLegal(result.containsKey(expectedKey), "data " + data + "does not contain expected key " //$NON-NLS-1$ //$NON-NLS-2$
 					+ expectedKey + "."); //$NON-NLS-1$
 		}
 		return Collections.unmodifiableMap(result);
 	}
 
 	private static final String[] EMPTY_STRING_ARRAY = new String[0];
+	private static final int CHAR = 0;
+	private static final int BACK_SLASH = 1;
 
 	/**
-	 * Transform the string representation of a list into a array.
+	 * Transform the string representation of a list into a array.<br>
+	 * This method supports the escaped meta characters: '\,' which is a literal
+	 * ',' and '\\' which is a '\'.
 	 * 
 	 * <pre>
 	 * The format of the string is: 
 	 *  string = [ value ] | value { , value }
 	 * </pre>
 	 * 
-	 * @param stringified
+	 * @param data
+	 *            must be of type string; otherwise a IllegalArgumentException
+	 *            will be thrown
 	 * @return
+	 * @throws IllegalArgumentException
+	 *             in case of errors
 	 */
-	public static String[] asArray(String stringified) {
+	public static String[] asArray(Object data) {
+		if (data == null) {
+			return EMPTY_STRING_ARRAY;
+		}
+		Assert.isLegal(data instanceof String, "data must be of type String."); //$NON-NLS-1$
+		String stringified = (String) data;
 		if (StringUtils.isEmpty(stringified)) {
 			return EMPTY_STRING_ARRAY;
 		}
 		List<String> result = new ArrayList<String>();
-		int comma;
-		int fromIndex = 0;
-		while ((comma = stringified.indexOf(',', fromIndex)) != -1) {
-			result.add(stringified.substring(fromIndex, comma));
-			fromIndex = comma + 1;
+		StringBuilder value = new StringBuilder();
+		char ch = 0;
+		int state = CHAR;
+		for (int i = 0; i < stringified.length(); i++) {
+			ch = stringified.charAt(i);
+			switch (state) {
+			case CHAR:
+				if (ch == '\\') {
+					state = BACK_SLASH;
+				} else if (ch == ',') {
+					result.add(value.toString());
+					value.setLength(0);
+				} else {
+					value.append(ch);
+				}
+				break;
+			case BACK_SLASH:
+				if (ch == '\\' || ch == ',') {
+					value.append(ch);
+				} else {
+					Assert.isLegal(false, "Unknown escaped character: " + ch + "."); //$NON-NLS-1$ //$NON-NLS-2$
+				}
+				state = CHAR;
+				break;
+			default:
+				break;
+			}
 		}
-		result.add(stringified.substring(fromIndex));
+		// final comma
+		if (value.length() != 0 || ch == ',') {
+			result.add(value.toString());
+		}
 		return result.toArray(new String[result.size()]);
 	}
 }
