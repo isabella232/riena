e3ed8d3a4a5a69f65e1814566c5c376f71b6b8f0
Stefan Liebig <stefan.liebig@compeople.de>
2008-12-10 10:47:18 +0000

    added helper for convenient converting to milli seconds

---
+++ org.eclipse.riena.core/src/org/eclipse/riena/core/util/Millis.java
@@ -0,0 +1,62 @@
+/*******************************************************************************
+ * Copyright (c) 2007, 2008 compeople AG and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    compeople AG - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.riena.core.util;
+
+/**
+ * {@code Millis} is a little helper class that provides methods to convert from
+ * seconds, minutes, hours and days to milli seconds.
+ */
+public final class Millis {
+
+	private Millis() {
+		// utility class
+	}
+
+	/**
+	 * Return the given seconds in milli seconds.
+	 * 
+	 * @param seconds
+	 * @return
+	 */
+	public final static long seconds(final int seconds) {
+		return 1000l * seconds;
+	}
+
+	/**
+	 * Return the given minutes in milli seconds.
+	 * 
+	 * @param minutes
+	 * @return
+	 */
+	public final static long minutes(final int minutes) {
+		return seconds(60 * minutes);
+	}
+
+	/**
+	 * Return the given hours in milli seconds.
+	 * 
+	 * @param hours
+	 * @return
+	 */
+	public final static long hours(final int hours) {
+		return minutes(60 * hours);
+	}
+
+	/**
+	 * Return the given days in milli seconds.
+	 * 
+	 * @param days
+	 * @return
+	 */
+	public final static long days(final int days) {
+		return hours(24 * days);
+	}
+}
