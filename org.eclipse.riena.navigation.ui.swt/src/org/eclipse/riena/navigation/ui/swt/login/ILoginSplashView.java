bf82264ec5fe8ff4cc21f20d89fcf90da8b1894c
Frank Schepp <frank.schepp@compeople.de>
2009-01-12 13:13:52 +0000

    Result IApplication.EXIT_RESTART not supported

--- org.eclipse.riena.navigation.ui.swt/src/org/eclipse/riena/navigation/ui/swt/login/ILoginSplashView.java
+++ org.eclipse.riena.navigation.ui.swt/src/org/eclipse/riena/navigation/ui/swt/login/ILoginSplashView.java
@@ -27,8 +27,6 @@ public interface ILoginSplashView {
 	 * to be considered:
 	 * <ol>
 	 * <li>IApplication.EXIT_OK indicates that the login was successful,</li>
-	 * <li>IApplication.EXIT_RESTART indicates that the login was not successful
-	 * (i.e. wrong authentication data was specified),</li>
 	 * <li>In case of some other result the login operation was aborted.</li>
 	 * </ol>
 	 * 
