6ec52495d13ac493bb221201c1e1cd48caf93a13
Christian Campo <christian.campo@compeople.de>
2009-01-12 09:46:58 +0000

    changes to make the NanoHTTPD actually go away

--- org.eclipse.riena.tests/src/org/eclipse/riena/tests/nanohttp/TestServer.java
+++ org.eclipse.riena.tests/src/org/eclipse/riena/tests/nanohttp/TestServer.java
@@ -12,6 +12,7 @@ package org.eclipse.riena.tests.nanohttp;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 
 /**
@@ -20,6 +21,7 @@ import java.util.Properties;
 public class TestServer extends NanoHTTPD {
 
 	private File root;
+	private int port;
 
 	/**
 	 * @param port
@@ -29,17 +31,30 @@ public class TestServer extends NanoHTTPD {
 	public TestServer(int port, File root) throws IOException {
 		super(port);
 		this.root = root;
+		this.port = port;
 	}
 
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see org.eclipse.riena.tests.nanohttp.NanoHTTPD#serveFile(java.lang.String,
-	 *      java.util.Properties, java.io.File, boolean)
+	 * @see
+	 * org.eclipse.riena.tests.nanohttp.NanoHTTPD#serveFile(java.lang.String,
+	 * java.util.Properties, java.io.File, boolean)
 	 */
 	@Override
 	public Response serveFile(String uri, Properties header, File homeDir, boolean allowDirectoryListing) {
 		return super.serveFile(uri, header, root, allowDirectoryListing);
+	}
+
+	@Override
+	public void stop() {
+		// after the NanoHTTPD has received its stop signal, we have to open any connection to actually see the thread go away
+		super.stop();
+		try {
+			new URL("http://localhost:" + port).openStream();
+		} catch (Exception e) {
+			// no action necessary
+		}
 	}
 
 }
