1b4c0ffd9ec80ddfef4c4045d15e17eccfae39d9
Stefan Liebig <stefan.liebig@compeople.de>
2009-01-12 14:40:52 +0000

    are now inhertiable

--- org.eclipse.riena.tests/src/org/eclipse/riena/tests/collect/IntegrationTestCase.java
+++ org.eclipse.riena.tests/src/org/eclipse/riena/tests/collect/IntegrationTestCase.java
@@ -11,6 +11,7 @@
 package org.eclipse.riena.tests.collect;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -20,6 +21,7 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
+@Inherited
 public @interface IntegrationTestCase {
 
 }
