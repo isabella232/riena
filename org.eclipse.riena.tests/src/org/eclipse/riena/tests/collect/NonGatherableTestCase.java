56ebd29664cd008ebee1743cde526077e656cbbe
Stefan Liebig <stefan.liebig@compeople.de>
2009-01-15 07:30:44 +0000

    this should not be inheritable, because that would make it too easy to exclude tests from collecting

--- org.eclipse.riena.tests/src/org/eclipse/riena/tests/collect/NonGatherableTestCase.java
+++ org.eclipse.riena.tests/src/org/eclipse/riena/tests/collect/NonGatherableTestCase.java
@@ -11,7 +11,6 @@
 package org.eclipse.riena.tests.collect;
 
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -22,7 +21,6 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@Inherited
 public @interface NonGatherableTestCase {
 
 	/**
