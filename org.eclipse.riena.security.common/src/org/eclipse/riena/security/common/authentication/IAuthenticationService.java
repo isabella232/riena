f29ffd0dd0be8f750ee50b08237b3a19cf000741
Christian Campo <christian.campo@compeople.de>
2009-01-21 16:09:58 +0000

    removed many commented out methods in interface that contained TODOs that were no longer relevant

--- org.eclipse.riena.security.common/src/org/eclipse/riena/security/common/authentication/IAuthenticationService.java
+++ org.eclipse.riena.security.common/src/org/eclipse/riena/security/common/authentication/IAuthenticationService.java
@@ -21,78 +21,22 @@ public interface IAuthenticationService {
 	String WS_ID = "/AuthenticationService"; //$NON-NLS-1$
 
 	/**
-	 * login operation.
-	 * 
-	 * @param principalID
-	 *            principal ID
-	 * @param credentials
-	 *            credential container
-	 * @return a security ticket
-	 * @throws AuthenticationException
-	 *             thrown in cases of application errors
-	 * @throws AuthenticationFailure
-	 *             thrown in cases of fatal error occurrences
-	 * @pre principalID != null
-	 * @pre credentials != null
-	 */
-	// ISecurityTicket login(PrincipalID principalID, Credentials credentials)
-	// throws AuthenticationException, AuthenticationFailure;
-	/**
-	 * Login as a proxy on behalf of someone else. The proxy login requires that
-	 * a user has already been logged in, to become a proxy for another user.
-	 * 
-	 * @param proxyTicket
-	 *            the security ticket returned from a previous login()
-	 * @param principalID
-	 *            principal ID of the represented user
-	 * @param principalCredentials
-	 *            credential container of the represented user
-	 * @return a security ticket
-	 * @throws AuthenticationException
-	 *             thrown in cases of application errors
-	 * @throws AuthenticationFailure
-	 *             thrown in cases of fatal error occurrences
-	 * @pre proxyTicket != null
-	 * @pre principalID != null
-	 * @pre principalCredentials != null
-	 */
-	// ISecurityTicket proxyLogin(ISecurityTicket proxyTicket, PrincipalID
-	// principalID, Credentials principalCredentials) throws
-	// AuthenticationException,
-	// AuthenticationFailure;
-	/**
-	 * change the value credential(s).
-	 * 
-	 * @param ticket
-	 *            the principal object reference
-	 * @param oldCredentials
-	 *            the old credentials container
-	 * @param newCredentials
-	 *            the new credentials container
-	 * @throws AuthenticationFailure
-	 *             thrown in cases of fatal error occurrences
-	 * @pre ticket != null
-	 * @pre ticket.getSession() != null
-	 * @pre oldCredentials != null
-	 * @pre newCredentials != null
-	 */
-	// void changeCredentials(ISecurityTicket ticket, Credentials
-	// oldCredentials, Credentials newCredentials) throws AuthenticationFailure;
-	/**
-	 * logoff operation.
+	 * Login with specified loginContext and credentials
 	 * 
 	 * @param loginContext
-	 *            TODO
-	 * @param ticket
-	 *            a security ticket
-	 * @throws AuthenticationFailure
-	 *             thrown in cases of fatal error occurrences
-	 * @pre ticket != null
-	 * @pre ticket.getSession() != null
+	 *            name of the equinox.security / JAAS security context
+	 * @param credentials
+	 *            list of credentials that need to be verified for a successful
+	 *            login
+	 * @return
 	 */
-	// void logoff(ISecurityTicket ticket) throws AuthenticationFailure;
 	AuthenticationTicket login(String loginContext, AbstractCredential[] credentials);
 
+	/**
+	 * Logoff currently logged in user and invalidate current session
+	 * 
+	 * @throws AuthenticationFailure
+	 */
 	void logout() throws AuthenticationFailure;
 
 }
