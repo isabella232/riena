7e08ce1ccd939726ce1121b35c01a97c7bc687d6
Stefan Liebig <stefan.liebig@compeople.de>
2009-01-28 13:49:24 +0000

    a no-op :-)

---
+++ org.eclipse.riena.core/src/org/eclipse/riena/internal/core/ignore/Nop.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2007, 2008 compeople AG and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    compeople AG - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.riena.internal.core.ignore;
+
+/**
+ * Provide a simple solution for documenting empty code blocks, e.g. empty
+ * try-catch clauses.
+ */
+public final class Nop {
+
+	private Nop() {
+		// utility
+	}
+
+	/**
+	 * Reason for the nop.
+	 * 
+	 * @param reason
+	 */
+	public static void reason(String reason) {
+		// nothing
+	}
+}
