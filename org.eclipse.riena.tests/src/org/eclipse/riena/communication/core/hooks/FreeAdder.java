bbff58ad93578e6905ea6e16b3fa9476fb83173f
Stefan Liebig <stefan.liebig@compeople.de>
2009-02-25 12:14:01 +0000

    nre

---
+++ org.eclipse.riena.tests/src/org/eclipse/riena/communication/core/hooks/FreeAdder.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2007, 2008 compeople AG and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    compeople AG - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.riena.communication.core.hooks;
+
+/**
+ *
+ */
+public class FreeAdder {
+
+	public String add(String s1, String s2) {
+		return s1 + s2;
+	}
+
+	public String add(Number n1, Number n2) {
+		return n1.toString() + n2.toString();
+	}
+
+	public String add(Integer i1, Integer i2) {
+		return Integer.toString(i1 + i2);
+	}
+
+}
---
+++ org.eclipse.riena.tests/src/org/eclipse/riena/communication/core/hooks/HooksProxy.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (c) 2007, 2008 compeople AG and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    compeople AG - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.riena.communication.core.hooks;
+
+import javax.security.auth.Subject;
+
+/**
+ * A concrete {@code AbstractHooksProxy} for testing purposes.
+ */
+public class HooksProxy extends AbstractHooksProxy {
+
+	private Subject subject;
+
+	/**
+	 * @param proxiedInstance
+	 */
+	public HooksProxy(Object proxiedInstance) {
+		super(proxiedInstance);
+	}
+
+	/**
+	 * @param proxiedInstance
+	 * @param subject
+	 */
+	public HooksProxy(Object proxiedInstance, Subject subject) {
+		super(proxiedInstance);
+		this.subject = subject;
+	}
+
+	@Override
+	public Subject getSubject() {
+		return subject;
+	}
+
+}
