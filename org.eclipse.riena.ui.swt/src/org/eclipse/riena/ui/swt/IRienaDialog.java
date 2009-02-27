68c0427d2a68c816b1dffbf199284b45597d6688
Stefan Liebig <stefan.liebig@compeople.de>
2009-02-27 10:56:35 +0000

    update for 2009

--- org.eclipse.riena.ui.swt/src/org/eclipse/riena/ui/swt/IRienaDialog.java
+++ org.eclipse.riena.ui.swt/src/org/eclipse/riena/ui/swt/IRienaDialog.java
@@ -1,7 +1,7 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2008 compeople AG and others.
+ * Copyright (c) 2007, 2009 compeople AG and others.
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse  License v1.0
+ * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
