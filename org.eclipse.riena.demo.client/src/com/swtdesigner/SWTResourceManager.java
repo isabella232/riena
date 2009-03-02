b18d2308a072fe1a39f3ab654a503eb5a9ed883d
Stefan Liebig <stefan.liebig@compeople.de>
2009-03-02 08:02:28 +0000

    Copyright header fixed

--- org.eclipse.riena.demo.client/src/com/swtdesigner/SWTResourceManager.java
+++ org.eclipse.riena.demo.client/src/com/swtdesigner/SWTResourceManager.java
@@ -1,13 +1,9 @@
-/*******************************************************************************
- * Copyright (c) 2007, 2009 compeople AG and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *    compeople AG - initial API and implementation
- *******************************************************************************/
+/**
+ * This class may be freely distributed as part of any application or plugin.
+ * <p>
+ * Copyright (c) 2003 - 2005, Instantiations, Inc. <br>
+ * All Rights Reserved
+ */
 package com.swtdesigner;
 
 import java.io.BufferedInputStream;
