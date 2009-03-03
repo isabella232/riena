de914f757a2fb7ae1230a0edfe5f5a9f90fc17bf
Stefan Liebig <stefan.liebig@compeople.de>
2009-03-03 13:15:56 +0000

    added tests for proxy selector support

---
+++ org.eclipse.riena.tests/src/org/eclipse/riena/communication/core/proxyselector/TestUtil.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2007, 2009 compeople AG and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    compeople AG - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.riena.communication.core.proxyselector;
+
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+
+/**
+ *
+ */
+public final class TestUtil {
+
+	private TestUtil() {
+		// Utility
+	}
+
+	public static Proxy newProxy(String host) {
+		return new Proxy(Proxy.Type.HTTP, InetSocketAddress.createUnresolved(host, 80));
+	}
+
+}
