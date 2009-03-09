a4801de86b1c7f9958c8db9a1ab82a6cc92f4cd6
Stefan Liebig <stefan.liebig@compeople.de>
2009-03-09 08:38:17 +0000

    Better static imports

--- org.eclipse.riena.core/src/org/eclipse/riena/internal/core/ignore/IgnoreFindBugs.java
+++ org.eclipse.riena.core/src/org/eclipse/riena/internal/core/ignore/IgnoreFindBugs.java
@@ -10,12 +10,7 @@
  *******************************************************************************/
 package org.eclipse.riena.internal.core.ignore;
 
-import static java.lang.annotation.ElementType.CONSTRUCTOR;
-import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.ElementType.LOCAL_VARIABLE;
-import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.ElementType.PARAMETER;
-import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.ElementType.*;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
