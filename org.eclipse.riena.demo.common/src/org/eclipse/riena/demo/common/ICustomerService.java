822fead0d4b92af15f36405750a9aea5208adddf
Stefan Liebig <stefan.liebig@compeople.de>
2009-04-08 12:14:47 +0000

    Schubser: copyright

--- org.eclipse.riena.demo.common/src/org/eclipse/riena/demo/common/ICustomerService.java
+++ org.eclipse.riena.demo.common/src/org/eclipse/riena/demo/common/ICustomerService.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2008 compeople AG and others.
+ * Copyright (c) 2007, 2009 compeople AG and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -16,16 +16,18 @@ import java.util.List;
  * service methods for the CustomerDemoService
  */
 public interface ICustomerService {
-	public static final String ID = ICustomerService.class.getName();
-	public static final String WS_ID = "/CustomerServiceWS"; //$NON-NLS-1$
+
+	String ID = ICustomerService.class.getName();
+	String WS_ID = "/CustomerServiceWS"; //$NON-NLS-1$
 
 	/**
 	 * 
-	 * @param lastName TODO
+	 * @param lastName
+	 *            TODO
 	 * @return CustomerSearchResult
 	 */
 	List<Customer> search(String lastName);
-	
+
 	boolean store(Customer customer);
 
 }
