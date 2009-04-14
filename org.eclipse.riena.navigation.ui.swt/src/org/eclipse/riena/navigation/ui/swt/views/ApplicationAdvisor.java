5233ccd2434ad053f6f2ca52300c39bafaf363bc
Elias Volanakis <elias@eclipsesource.com>
2009-04-14 00:20:19 +0000

    269855: Default eclipse key bindings shows unwanted eclipse dialogs in riena

--- org.eclipse.riena.navigation.ui.swt/src/org/eclipse/riena/navigation/ui/swt/views/ApplicationAdvisor.java
+++ org.eclipse.riena.navigation.ui.swt/src/org/eclipse/riena/navigation/ui/swt/views/ApplicationAdvisor.java
@@ -10,14 +10,23 @@
  *******************************************************************************/
 package org.eclipse.riena.navigation.ui.swt.views;
 
-import org.eclipse.riena.internal.core.exceptionmanager.ExceptionHandlerManagerAccessor;
-import org.eclipse.riena.navigation.ui.controllers.ApplicationController;
-import org.eclipse.riena.navigation.ui.swt.presentation.stack.TitlelessStackPresentationFactory;
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.ActiveShellExpression;
+import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchAdvisor;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+import org.eclipse.ui.handlers.IHandlerService;
 import org.eclipse.ui.statushandlers.AbstractStatusHandler;
 import org.eclipse.ui.statushandlers.StatusAdapter;
+
+import org.eclipse.riena.internal.core.exceptionmanager.ExceptionHandlerManagerAccessor;
+import org.eclipse.riena.navigation.ui.controllers.ApplicationController;
+import org.eclipse.riena.navigation.ui.swt.presentation.stack.TitlelessStackPresentationFactory;
 
 public class ApplicationAdvisor extends WorkbenchAdvisor {
 
@@ -57,4 +66,35 @@ public class ApplicationAdvisor extends WorkbenchAdvisor {
 		};
 	}
 
+	@Override
+	public void postStartup() {
+		overwriteStandardHandlers();
+	}
+
+	// helping methods
+	//////////////////
+
+	/**
+	 * Rebinds certain commands that are by bound to handlers in org.eclipse.ui
+	 * to a NOOP-Handler.
+	 * 
+	 * @see http://bugs.eclipse.org/269855
+	 */
+	private void overwriteStandardHandlers() {
+		IWorkbench workbench = getWorkbenchConfigurer().getWorkbench();
+		Shell shell = workbench.getActiveWorkbenchWindow().getShell();
+		IHandlerService service = (IHandlerService) workbench.getService(IHandlerService.class);
+		IHandler handler = new AbstractHandler() {
+			public Object execute(ExecutionEvent event) throws ExecutionException {
+				// Do nothing by design
+				return null;
+			}
+		};
+		String[] commandIds = { "org.eclipse.ui.newWizard", "org.eclipse.ui.window.previousView", //$NON-NLS-1$ //$NON-NLS-2$
+				"org.eclipse.ui.window.nextView", "org.eclipse.ui.window.nextPerspective", //$NON-NLS-1$ //$NON-NLS-2$
+				"org.eclipse.ui.window.previousPerspective", }; //$NON-NLS-1$
+		for (String id : commandIds) {
+			service.activateHandler(id, handler, new ActiveShellExpression(shell));
+		}
+	}
 }
