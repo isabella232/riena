3cc908a42edf254da3f9a55f76211c6b90764cee
Carsten Drossel <carsten.drossel@compeople.de>
2009-04-22 13:57:36 +0000

    extracted SWT-control independent superclasses of Ridgets to allow reuse by Ridgets for custom controls

--- org.eclipse.riena.ui.ridgets.swt/src/org/eclipse/riena/internal/ui/ridgets/swt/LabelRidget.java
+++ org.eclipse.riena.ui.ridgets.swt/src/org/eclipse/riena/internal/ui/ridgets/swt/LabelRidget.java
@@ -10,48 +10,23 @@
  *******************************************************************************/
 package org.eclipse.riena.internal.ui.ridgets.swt;
 
-import java.net.URL;
-
-import org.eclipse.core.databinding.beans.BeansObservables;
-import org.eclipse.core.databinding.observable.value.IObservableValue;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Label;
 
-import org.eclipse.riena.ui.ridgets.AbstractMarkerSupport;
-import org.eclipse.riena.ui.ridgets.ILabelRidget;
+import org.eclipse.riena.ui.ridgets.swt.AbstractLabelRidget;
 import org.eclipse.riena.ui.ridgets.swt.AbstractSWTRidget;
-import org.eclipse.riena.ui.ridgets.swt.AbstractValueRidget;
 
 /**
  * Ridget for an SWT {@link Label} widget.
  */
-public class LabelRidget extends AbstractValueRidget implements ILabelRidget {
-
-	private static final String EMPTY_STRING = ""; //$NON-NLS-1$
-	private String text;
-	private String icon;
-	private URL iconLocation;
-	private boolean textAlreadyInitialized;
-	private boolean useRidgetIcon;
+public class LabelRidget extends AbstractLabelRidget {
 
 	public LabelRidget() {
 		this(null);
 	}
 
 	public LabelRidget(Label label) {
-		textAlreadyInitialized = false;
-		useRidgetIcon = false;
 		setUIControl(label);
-	}
-
-	@Override
-	protected AbstractMarkerSupport createMarkerSupport() {
-		return new BasicMarkerSupport(this, propertyChangeSupport);
-	}
-
-	@Override
-	protected IObservableValue getRidgetObservable() {
-		return BeansObservables.observeValue(this, ILabelRidget.PROPERTY_TEXT);
 	}
 
 	@Override
@@ -60,119 +35,23 @@ public class LabelRidget extends AbstractValueRidget implements ILabelRidget {
 	}
 
 	@Override
-	protected void bindUIControl() {
-		initText();
-		updateUIText();
-		updateUIIcon();
-	}
-
-	/**
-	 * If the text of the ridget has no value, initialize it with the text of
-	 * the UI control.
-	 */
-	private void initText() {
-		if ((text == null) && (!textAlreadyInitialized)) {
-			if ((getUIControl()) != null && !(getUIControl().isDisposed())) {
-				text = getUIControl().getText();
-				if (text == null) {
-					text = EMPTY_STRING;
-				}
-				textAlreadyInitialized = true;
-			}
-		}
-	}
-
-	@Override
 	public Label getUIControl() {
 		return (Label) super.getUIControl();
 	}
 
-	public String getIcon() {
-		return icon;
-	}
-
-	public URL getIconLocation() {
-		return iconLocation;
-	}
-
-	public String getText() {
-		return text;
-	}
-
-	/**
-	 * Always returns true because mandatory markers do not make sense for this
-	 * ridget.
-	 */
 	@Override
-	public boolean isDisableMandatoryMarker() {
-		return true;
+	protected String getUIControlText() {
+		return getUIControl().getText();
 	}
 
-	public void setIcon(String icon) {
-		boolean oldUseRidgetIcon = useRidgetIcon;
-		useRidgetIcon = true;
-		String oldIcon = this.icon;
-		this.icon = icon;
-		if (hasChanged(oldIcon, icon) || !oldUseRidgetIcon) {
-			updateUIIcon();
-		}
+	@Override
+	protected void setUIControlText(String text) {
+		getUIControl().setText(text);
 	}
 
-	public void setIconLocation(URL location) {
-		useRidgetIcon = true;
-		URL oldUrl = this.iconLocation;
-		this.iconLocation = location;
-		if (hasChanged(oldUrl, location)) {
-			updateUIIcon();
-		}
-	}
-
-	public void setText(String text) {
-		String oldValue = this.text;
-		this.text = text;
-		updateUIText();
-		firePropertyChange(ILabelRidget.PROPERTY_TEXT, oldValue, this.text);
-	}
-
-	// helping methods
-	// ////////////////
-
-	private void updateUIText() {
-		Label control = getUIControl();
-		if (control != null) {
-			control.setText(text);
-		}
-	}
-
-	private void updateUIIcon() {
-		Label control = getUIControl();
-		if (control != null) {
-			Image image = null;
-			if (icon != null) {
-				image = getManagedImage(icon);
-			} else if (iconLocation != null) {
-				String key = iconLocation.toExternalForm();
-				image = getManagedImage(key);
-			}
-			if ((image != null) || useRidgetIcon) {
-				control.setImage(image);
-			}
-		}
-	}
-
-	private boolean hasChanged(URL oldValue, URL newValue) {
-
-		if (oldValue == null && newValue == null) {
-			return false;
-		}
-		if (oldValue == null || newValue == null) {
-			return true;
-		}
-
-		// avoid URL.equals(...) since it opens a network connection :(
-		String str1 = oldValue.toExternalForm();
-		String str2 = newValue.toExternalForm();
-		return !str1.equals(str2);
+	@Override
+	protected void setUIControlImage(Image image) {
+		getUIControl().setImage(image);
 	}
 
 }
