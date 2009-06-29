5f5d87f0912cf48ff91996471cf965fe91dd3d04
Elias Volanakis <elias@eclipsesource.com>
2009-06-29 21:54:11 +0000

    279179: Close module groups / submodule groups with keyboard

---
+++ org.eclipse.riena.navigation.ui.swt/src/org/eclipse/riena/internal/navigation/ui/swt/handlers/CloseModuleGroup.java
@@ -0,0 +1,58 @@
+/*******************************************************************************
+ * Copyright (c) 2007, 2009 compeople AG and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    compeople AG - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.riena.internal.navigation.ui.swt.handlers;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+import org.eclipse.riena.navigation.ApplicationNodeManager;
+import org.eclipse.riena.navigation.IApplicationNode;
+import org.eclipse.riena.navigation.IModuleGroupNode;
+import org.eclipse.riena.navigation.INavigationNode;
+
+/**
+ * Close the currently active module-group.
+ */
+public class CloseModuleGroup extends AbstractHandler {
+
+	@SuppressWarnings("unchecked")
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IApplicationNode application = ApplicationNodeManager.getApplicationNode();
+		INavigationNode<?> subApplication = findActive((List) application.getChildren());
+		if (subApplication != null) {
+			INavigationNode<?> moduleGroup = findActive((List) subApplication.getChildren());
+			if (moduleGroup instanceof IModuleGroupNode) {
+				((IModuleGroupNode) moduleGroup).dispose();
+			}
+		}
+		return null;
+	}
+
+	// helping methods
+	//////////////////
+
+	protected final INavigationNode<?> findActive(List<INavigationNode<?>> children) {
+		INavigationNode<?> result = null;
+		Iterator<INavigationNode<?>> iter = children.iterator();
+		while (result == null && iter.hasNext()) {
+			INavigationNode<?> candidate = iter.next();
+			if (candidate.isActivated()) {
+				result = candidate;
+			}
+		}
+		return result;
+	}
+
+}
