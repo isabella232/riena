3ec3e95da1e401e2f9b3df1a222b90e2cef6dac1
Christian Campo <christian.campo@compeople.de>
2009-07-02 11:06:22 +0000

    *** empty log message ***

--- org.eclipse.riena.demo.client/src/org/eclipse/riena/demo/client/views/EmailView2.java
+++ org.eclipse.riena.demo.client/src/org/eclipse/riena/demo/client/views/EmailView2.java
@@ -41,13 +41,12 @@ public class EmailView2 extends SubModuleView<AbstractEmailController> {
 
 	@Override
 	protected void basicCreatePartControl(Composite parent) {
-
 		parent.setLayout(new FillLayout(SWT.VERTICAL));
-		Composite container = new Composite(parent, SWT.NONE);
 
 		Font boldFont12 = SWTResourceManager.getFont("Arial", 12, SWT.BOLD); //$NON-NLS-1$
 		Font boldFont11 = SWTResourceManager.getFont("Arial", 11, SWT.BOLD); //$NON-NLS-1$
 		Font normalFont11 = SWTResourceManager.getFont("Arial", 11, SWT.NORMAL); //$NON-NLS-1$
+		Composite container = new Composite(parent, SWT.NONE);
 
 		table = new Table(container, SWT.SINGLE | SWT.BORDER | SWT.FULL_SELECTION);
 		table.setBackground(SWTResourceManager.getColor(255, 255, 254));
@@ -64,59 +63,59 @@ public class EmailView2 extends SubModuleView<AbstractEmailController> {
 		tableColumn_2.setWidth(143);
 
 		Label l = new Label(container, SWT.WRAP);
-		l.setLocation(31, 264);
+		l.setLocation(25, 254);
 		l.setSize(58, 18);
 		l.setFont(boldFont11);
 		l.setForeground(SWTResourceManager.getColor(1, 1, 1));
-		l.setText("Subject:"); //$NON-NLS-1$
+		l.setText("Subject"); //$NON-NLS-1$
 
 		Label subjectLabel = new Label(container, SWT.NONE);
-		subjectLabel.setLocation(109, 264);
+		subjectLabel.setLocation(103, 254);
 		subjectLabel.setSize(619, 17);
 		//		subjectLabel.setLayoutData(new GridData(GridData.FILL_BOTH));
-		subjectLabel.setFont(normalFont11);
+		subjectLabel.setFont(SWTResourceManager.getFont("Arial", 11, SWT.BOLD));
 		subjectLabel.setForeground(SWTResourceManager.getColor(1, 1, 1));
 		addUIControl(subjectLabel, "emailSubject"); //$NON-NLS-1$
 
 		l = new Label(container, SWT.WRAP);
-		l.setLocation(31, 288);
+		l.setLocation(25, 278);
 		l.setSize(40, 18);
 		l.setFont(boldFont11);
 		l.setForeground(SWTResourceManager.getColor(1, 1, 1));
-		l.setText("From:"); //$NON-NLS-1$
+		l.setText("From"); //$NON-NLS-1$
 
 		Label fromLabel = new Label(container, SWT.NONE);
-		fromLabel.setLocation(109, 287);
+		fromLabel.setLocation(103, 277);
 		fromLabel.setSize(302, 17);
 		//		fromLabel.setLayoutData(new GridData(GridData.FILL_BOTH));
-		fromLabel.setFont(SWTResourceManager.getFont("Arial", 11, SWT.ITALIC)); //$NON-NLS-1$
+		fromLabel.setFont(SWTResourceManager.getFont("Arial", 11, SWT.NORMAL)); //$NON-NLS-1$
 		fromLabel.setForeground(SWTResourceManager.getColor(1, 1, 1));
 		addUIControl(fromLabel, "emailFrom"); //$NON-NLS-1$
 
 		l = new Label(container, SWT.WRAP);
-		l.setLocation(417, 287);
+		l.setLocation(411, 277);
 		l.setSize(23, 18);
 		l.setFont(boldFont11);
 		l.setForeground(SWTResourceManager.getColor(1, 1, 1));
-		l.setText("To:"); //$NON-NLS-1$
+		l.setText("To"); //$NON-NLS-1$
 
 		Label toLabel = new Label(container, SWT.NONE);
-		toLabel.setLocation(446, 287);
+		toLabel.setLocation(440, 277);
 		toLabel.setSize(282, 17);
 		//		toLabel.setLayoutData(new GridData(GridData.FILL_BOTH));
-		toLabel.setFont(SWTResourceManager.getFont("Arial", 11, SWT.ITALIC)); //$NON-NLS-1$
+		toLabel.setFont(SWTResourceManager.getFont("Arial", 11, SWT.NORMAL)); //$NON-NLS-1$
 		toLabel.setForeground(SWTResourceManager.getColor(1, 1, 1));
 		addUIControl(toLabel, "emailTo"); //$NON-NLS-1$
 
 		l = new Label(container, SWT.WRAP);
-		l.setLocation(34, 312);
+		l.setLocation(25, 302);
 		l.setSize(37, 18);
 		l.setFont(boldFont11);
 		l.setForeground(SWTResourceManager.getColor(1, 1, 1));
-		l.setText("Date:"); //$NON-NLS-1$
+		l.setText("Date"); //$NON-NLS-1$
 
 		Label dateLabel = new Label(container, SWT.NONE);
-		dateLabel.setLocation(109, 310);
+		dateLabel.setLocation(103, 300);
 		dateLabel.setSize(142, 17);
 		//		dateLabel.setLayoutData(new GridData(GridData.FILL_BOTH));
 		dateLabel.setFont(normalFont11);
@@ -126,7 +125,7 @@ public class EmailView2 extends SubModuleView<AbstractEmailController> {
 		// message contents
 
 		Text emailBody = new Text(container, SWT.MULTI | SWT.WRAP);
-		emailBody.setLocation(31, 347);
+		emailBody.setLocation(31, 326);
 		emailBody.setSize(697, 112);
 		GridDataFactory.fillDefaults().grab(true, true).applyTo(emailBody);
 		emailBody.setFont(normalFont11);
@@ -134,15 +133,18 @@ public class EmailView2 extends SubModuleView<AbstractEmailController> {
 
 		//create button
 		Button openCustomerButton = new Button(container, SWT.NONE);
-		openCustomerButton.setBounds(25, 465, 133, 34);
-		openCustomerButton.setFont(SWTResourceManager.getFont("Arial", 11, SWT.BOLD)); //$NON-NLS-1$
+		openCustomerButton.setBounds(580, 474, 133, 38);
+		openCustomerButton.setFont(SWTResourceManager.getFont("Arial", 11, SWT.NORMAL)); //$NON-NLS-1$
 		openCustomerButton.setText("Open Customer"); //$NON-NLS-1$
 		addUIControl(openCustomerButton, "openCustomer"); //$NON-NLS-1$
-		{
-			Composite composite = new Composite(container, SWT.NONE);
-			composite.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
-			composite.setBounds(-28, 0, 948, 248);
-		}
+
+		Composite composite = new Composite(container, SWT.NONE);
+		composite.setBackground(SWTResourceManager.getColor(121, 117, 168));
+		composite.setBounds(25, 235, 1054, 2);
+
+		Composite composite2 = new Composite(container, SWT.NONE);
+		composite2.setBackground(SWTResourceManager.getColor(121, 117, 168));
+		composite2.setBounds(25, 457, 1054, 2);
 
 	}
 }
