d3ee5936a11cb4844f2b77ac0f2624c8ba2623f8
Stefan Liebig <stefan.liebig@compeople.de>
2009-07-15 09:27:17 +0000

    serializer factory test

---
+++ org.eclipse.riena.tests/src/org/eclipse/riena/internal/communication/hessian/InputStreamSerializerFactoryTest.java
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ * Copyright (c) 2007, 2009 compeople AG and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    compeople AG - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.riena.internal.communication.hessian;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.eclipse.riena.communication.core.attachment.Attachment;
+import org.eclipse.riena.internal.communication.factory.hessian.InputStreamSerializerFactory;
+import org.eclipse.riena.internal.core.ignore.Nop;
+
+/**
+ * Test the {@code InputStreamSerializerFactory} class.
+ */
+public class InputStreamSerializerFactoryTest extends AbstractSerializerFactoryTestCase {
+
+	private final static byte[] BYTES = "That will be pushed through hessian!".getBytes();
+
+	public void testInputStreamFail() throws IOException {
+		Attachment attachment = new Attachment(new ByteArrayInputStream(BYTES));
+		try {
+			inAndOut(attachment, HessianSerializerVersion.Two, null);
+			fail("Must fail!");
+		} catch (IOException e) {
+			Nop.reason("Expected");
+		}
+	}
+
+	public void testInputStream() throws IOException {
+		ByteArrayInputStream bais = new ByteArrayInputStream(BYTES);
+		Attachment attachment = new Attachment(bais);
+		Attachment attechmentOut = (Attachment) inAndOut(attachment, HessianSerializerVersion.Two, null,
+				new InputStreamSerializerFactory());
+		byte[] outBytes = new byte[BYTES.length];
+		attechmentOut.readAsStream().read(outBytes);
+		assertTrue(Arrays.equals(BYTES, outBytes));
+	}
+}
