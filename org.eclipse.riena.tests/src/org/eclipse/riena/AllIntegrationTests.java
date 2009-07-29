64102862632719b90ec55e544afeb22e28fa7597
Stefan Liebig <stefan.liebig@compeople.de>
2009-07-29 12:34:20 +0000

    added

---
+++ org.eclipse.riena.tests/src/org/eclipse/riena/AllIntegrationTests.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2007, 2009 compeople AG and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    compeople AG - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.riena;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+
+import org.eclipse.riena.internal.tests.Activator;
+import org.eclipse.riena.tests.collect.IntegrationTestCase;
+import org.eclipse.riena.tests.collect.NonGatherableTestCase;
+import org.eclipse.riena.tests.collect.TestCollector;
+
+/**
+ * Tests all ´integration´ test cases within this bundle.
+ */
+@NonGatherableTestCase("This is not a ´TestCase´!")
+public class AllIntegrationTests extends TestCase {
+
+	@SuppressWarnings("unchecked")
+	public static Test suite() {
+		return TestCollector.createTestSuiteWith(Activator.getDefault().getBundle(), null, IntegrationTestCase.class);
+	}
+
+}
