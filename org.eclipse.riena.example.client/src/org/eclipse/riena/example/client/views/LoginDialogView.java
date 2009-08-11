16fed9b64d49c470a45e34c8c0363397b5844c08
Christian Campo <christian.campo@compeople.de>
2009-08-11 11:25:28 +0000

    changes to make it easy to edit with SWT designer #286214

--- org.eclipse.riena.example.client/src/org/eclipse/riena/example/client/views/LoginDialogView.java
+++ org.eclipse.riena.example.client/src/org/eclipse/riena/example/client/views/LoginDialogView.java
@@ -13,6 +13,15 @@ package org.eclipse.riena.example.client.views;
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+
 import org.eclipse.riena.beans.common.IntegerBean;
 import org.eclipse.riena.example.client.controllers.LoginDialogController;
 import org.eclipse.riena.navigation.ui.login.ILoginDialogView;
@@ -21,22 +30,11 @@ import org.eclipse.riena.ui.ridgets.swt.views.DialogView;
 import org.eclipse.riena.ui.swt.lnf.LnfKeyConstants;
 import org.eclipse.riena.ui.swt.lnf.LnfManager;
 import org.eclipse.riena.ui.swt.utils.UIControlsFactory;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Text;
 
 /**
  * The view for the login dialog of the example.
  */
 public class LoginDialogView extends DialogView implements ILoginDialogView {
-
-	private static final GridData GD11FILL = new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1);
-	private static final GridData GD21FILL = new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1);
-	private static final GridData GD41FILL = new GridData(SWT.FILL, SWT.CENTER, true, false, 4, 1);
 
 	private IntegerBean result;
 
@@ -80,6 +78,7 @@ public class LoginDialogView extends DialogView implements ILoginDialogView {
 	private Composite createContentView(Composite parent) {
 
 		Composite content = UIControlsFactory.createComposite(parent);
+		content.setLayout(new GridLayout()); // create a simple GridLayout so SWT-Designer can parse it
 		GridLayoutFactory.fillDefaults().numColumns(4).spacing(6, 9).equalWidth(false).applyTo(content);
 
 		// dummy to occupy the cell
@@ -87,7 +86,7 @@ public class LoginDialogView extends DialogView implements ILoginDialogView {
 
 		UIControlsFactory.createLabel(content, "User"); //$NON-NLS-1$
 		Text user = UIControlsFactory.createText(content);
-		user.setLayoutData(GD21FILL);
+		user.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1));
 		addUIControl(user, LoginDialogController.RIDGET_ID_USER);
 
 		// dummy to occupy the cell
@@ -95,7 +94,7 @@ public class LoginDialogView extends DialogView implements ILoginDialogView {
 
 		UIControlsFactory.createLabel(content, "Password"); //$NON-NLS-1$
 		Text password = UIControlsFactory.createText(content);
-		password.setLayoutData(GD21FILL);
+		password.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1));
 		addUIControl(password, LoginDialogController.RIDGET_ID_PASSWORD);
 
 		// dummy to occupy the cell
@@ -105,19 +104,19 @@ public class LoginDialogView extends DialogView implements ILoginDialogView {
 
 		Button okButton = UIControlsFactory.createButton(content);
 		okButton.setText("   Login   "); //$NON-NLS-1$
-		okButton.setLayoutData(GD11FILL);
+		okButton.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 		addUIControl(okButton, LoginDialogController.RIDGET_ID_OK);
 
 		Button cancelButton = UIControlsFactory.createButton(content);
 		cancelButton.setText("   Cancel   "); //$NON-NLS-1$
-		cancelButton.setLayoutData(GD11FILL);
+		cancelButton.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 		addUIControl(cancelButton, LoginDialogController.RIDGET_ID_CANCEL);
 
 		Label info = UIControlsFactory
 				.createLabel(
 						content,
 						"\nTo authenticate with running Sample App Server\n    type: user=john, password=john.\n    To omit it just press login."); //$NON-NLS-1$
-		info.setLayoutData(GD41FILL);
+		info.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 4, 1));
 
 		addUIControl(UIControlsFactory.createMessageBox(content),
 				LoginDialogController.RIDGET_ID_MESSAGE_LOGIN_EXCEPTION);
