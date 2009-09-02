aef2e2e73241de38246a5eab3580e50c7ff5e94b
Stefan Liebig <stefan.liebig@compeople.de>
2009-09-02 09:29:16 +0000

    cleaned up

--- org.eclipse.riena.ui.swt/src/org/eclipse/riena/ui/swt/lnf/LnfManager.java
+++ org.eclipse.riena.ui.swt/src/org/eclipse/riena/ui/swt/lnf/LnfManager.java
@@ -12,31 +12,86 @@ package org.eclipse.riena.ui.swt.lnf;
 
 import org.osgi.framework.Bundle;
 
+import org.eclipse.core.runtime.Assert;
 import org.eclipse.core.runtime.Platform;
 
+import org.eclipse.riena.core.exception.Failure;
+import org.eclipse.riena.core.util.Nop;
 import org.eclipse.riena.ui.swt.lnf.rienadefault.RienaDefaultLnf;
 import org.eclipse.riena.ui.swt.utils.BundleUtil;
 
 /**
- * <code>LnfManager</code> manages the current look and feel of the riena
- * (navigation) widgets.
+ * The {@code LnfManager} manages the current look and feel of the riena
+ * (navigation) widgets.<br>
+ * The {@code LnfManager} has the term of a default look-and-feel. The default
+ * L&F is initially set by Riena to {@code RienaDefaultLnf}. But the default L&F
+ * may also be overridden by frameworks based on Riena. That allows them to
+ * define their own L&F.<br>
+ * However, applications can again override the default L&F. This can be done by
+ * either setting the system property "riena.lnf" or by specifying their L&F
+ * with the methods {@code LnfManager.setLnf()}. Setting the L&F with the system
+ * property overrules all other methods.<br>
+ * When specifying the L&F class via a string (either system property or one of
+ * the above mentioned methods) the string should conform to:
+ * 
+ * <pre>
+ * lnf := [ Bundle-Symbolic-Name &quot;:&quot; ] LnF-Class-Name
+ * </pre>
+ * 
+ * Where Bundle-Symbolic-Name allows to load the L&F class from the bundle with
+ * this symbolic name.
  */
 public final class LnfManager {
 
-	private static RienaDefaultLnf defaultLnfClass = new RienaDefaultLnf();
-	private static String lnfClassName;
-
-	private static RienaDefaultLnf lnf;
-
 	/**
-	 * @since 1.2
+	 * Allows setting of an application L&F.
 	 */
-	public static void setDefaultLnf(RienaDefaultLnf parmDefaultLnfClazz) {
-		defaultLnfClass = parmDefaultLnfClazz;
-	}
+	public static final String RIENA_LNF_SYSTEM_PROPERTY = "riena.lnf"; //$NON-NLS-1$
+
+	private static RienaDefaultLnf defaultLnf = new RienaDefaultLnf();;
+	private static RienaDefaultLnf currentLnf;
 
 	private LnfManager() {
-		// cannot instantiated, because all method are static
+		// cannot instantiated, because all methods are static
+	}
+
+	/**
+	 * Set a new default L&F.
+	 * 
+	 * @param defaultLnf
+	 *            new default L&F
+	 * @since 1.2
+	 */
+	public static void setDefaultLnf(RienaDefaultLnf defaultLnf) {
+		Assert.isNotNull(defaultLnf, "defaultLnf must not be null."); //$NON-NLS-1$
+		dispose();
+		LnfManager.defaultLnf = defaultLnf;
+	}
+
+	/**
+	 * Set the new look and feel specified by the given class name (see class
+	 * header JavaDoc). New look and feel will be initialized and the old one
+	 * will be uninitialized.
+	 * 
+	 * @param currentLnfClassName
+	 *            a string specifying the name of the class that implements the
+	 *            look and feel
+	 */
+	public static void setLnf(String currentLnfClassName) {
+		dispose();
+		setLnf(createLnf(currentLnfClassName));
+	}
+
+	/**
+	 * Sets the new look and feel. New look and feel will be initialized and the
+	 * old one will be uninitialized.
+	 * 
+	 * @param currentLnf
+	 *            new look and feel to install.
+	 */
+	public static void setLnf(RienaDefaultLnf currentLnf) {
+		dispose();
+		LnfManager.currentLnf = currentLnf;
 	}
 
 	/**
@@ -46,100 +101,96 @@ public final class LnfManager {
 	 * @return current look and feel
 	 */
 	public static RienaDefaultLnf getLnf() {
-
-		if (lnf == null) {
-			String className = getLnfClassName();
-			if (className != null) {
-				try {
-					RienaDefaultLnf myLnf = createLnf(className);
-					if (myLnf == null) {
-						throw new Error("can't load LnfClass '" + className + "' from the list of installed bundles."); //$NON-NLS-1$ //$NON-NLS-2$
-					}
-					setLnf(myLnf);
-				} catch (Exception e) {
-					e.printStackTrace();
-					throw new Error(
-							"can't load " + className + " If you use -Driena.lnf, try prefixing the classname with the bundleId i.e. org.eclipse.riena.demo.client:org.eclipse.riena.demo.client.lnf.EclipseLnf"); //$NON-NLS-1$ //$NON-NLS-2$
-				}
-			} else {
-				setLnf(defaultLnfClass);
-			}
+		final String rienaLnf = System.getProperty(RIENA_LNF_SYSTEM_PROPERTY);
+		if (currentLnf == null || rienaLnf != null) {
+			RienaDefaultLnf lnf = createLnf(rienaLnf);
+			currentLnf = lnf == null ? defaultLnf : lnf;
 		}
-		lnf.initialize();
-
-		return lnf;
-
+		currentLnf.initialize();
+		return currentLnf;
 	}
 
 	/**
-	 * Loads the look and feel specified by the given class name and passes it
-	 * to <code>setLnf(RienaDefaultLnf)</code>.
+	 * Return the current L&F class name.
 	 * 
-	 * @param lnfClassName
-	 *            - a string specifying the name of the class that implements
-	 *            the look and feel
-	 * @throws ClassNotFoundException
-	 * @throws InstantiationException
-	 * @throws IllegalAccessException
-	 */
-	public static void setLnf(String newLnfClassName) {
-		dispose();
-		lnfClassName = newLnfClassName;
-	}
-
-	private static RienaDefaultLnf createLnf(String lnfClassName) throws ClassNotFoundException,
-			InstantiationException, IllegalAccessException {
-
-		Class<?> lnfClass = null;
-		if (lnfClassName.contains(":")) { //$NON-NLS-1$
-			String[] parts = lnfClassName.split(":"); //$NON-NLS-1$
-			String pluginID = parts[0];
-			String classPath = parts[1];
-			Bundle bundle = Platform.getBundle(pluginID);
-			if (!BundleUtil.isReady(bundle)) {
-				return null;
-			}
-			lnfClass = bundle.loadClass(classPath);
-
-		} else {
-
-			ClassLoader classLoader = LnfManager.class.getClassLoader();
-			lnfClass = classLoader.loadClass(lnfClassName);
-		}
-		return (RienaDefaultLnf) lnfClass.newInstance();
-
-	}
-
-	/**
-	 * Sets the current look and feel. New look and feel will be initialized and
-	 * the old one will be uninitialized.
-	 * 
-	 * @param newLnf
-	 *            - new look and feel to install.
-	 */
-	public static void setLnf(RienaDefaultLnf newLnf) {
-		lnf = newLnf;
-	}
-
-	/**
 	 * @return the lnfClassName
 	 */
 	public static String getLnfClassName() {
-		String className = lnfClassName;
-		if (className == null) {
-			className = System.getProperty("riena.lnf"); //$NON-NLS-1$
-		}
-		return className;
+		return getLnf().getClass().getName();
 	}
 
 	/**
 	 * Disposes (uninitializes) the current look and feel.
 	 */
 	public static void dispose() {
-		if (lnf != null) {
-			lnf.uninitialize();
-			lnf = null;
+		if (currentLnf != null) {
+			currentLnf.uninitialize();
+			currentLnf = null;
 		}
 	}
 
+	private static final String BUNDLE_CLASS_NAME_SEPARATOR = ":"; //$NON-NLS-1$
+
+	private static RienaDefaultLnf createLnf(String lnfClassName) {
+		if (lnfClassName == null) {
+			return null;
+		}
+		Class<?> lnfClass = null;
+		if (lnfClassName.contains(BUNDLE_CLASS_NAME_SEPARATOR)) {
+			String[] parts = lnfClassName.split(BUNDLE_CLASS_NAME_SEPARATOR);
+			String bundleSymbolicName = parts[0];
+			String className = parts[1];
+			Bundle bundle = Platform.getBundle(bundleSymbolicName);
+			if (!BundleUtil.isReady(bundle)) {
+				throw new LnfManagerFailure(
+						"can't load LnfClass '" + className + "' from bundle " + bundleSymbolicName + " because bundle is not ready."); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+			}
+			try {
+				lnfClass = bundle.loadClass(className);
+			} catch (ClassNotFoundException e) {
+				throw new LnfManagerFailure(
+						"can't load LnfClass '" + className + "' from bundle " + bundleSymbolicName + ".", e); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+			}
+		} else {
+			lnfClass = loadClass(lnfClassName);
+		}
+		try {
+			return (RienaDefaultLnf) lnfClass.newInstance();
+		} catch (Exception e) {
+			throw new LnfManagerFailure("can't create instance for LnfClass '" + lnfClass.getName() + ".", e); //$NON-NLS-1$ //$NON-NLS-2$
+		}
+	}
+
+	private static Class<?> loadClass(String className) {
+		try {
+			return LnfManager.class.getClassLoader().loadClass(className);
+		} catch (ClassNotFoundException e) {
+			Nop.reason("try next"); //$NON-NLS-1$
+		}
+		try {
+			return Thread.currentThread().getContextClassLoader().loadClass(className);
+		} catch (Exception e) {
+			Nop.reason("try next"); //$NON-NLS-1$
+		}
+
+		throw new LnfManagerFailure("can't load LnfClass '" + className + "."); //$NON-NLS-1$ //$NON-NLS-2$
+	}
+
+	@SuppressWarnings("serial")
+	private static class LnfManagerFailure extends Failure {
+
+		/**
+		 * @param msg
+		 */
+		public LnfManagerFailure(String msg) {
+			super(msg);
+		}
+
+		/**
+		 * @param msg
+		 */
+		public LnfManagerFailure(String msg, Throwable thrown) {
+			super(msg, thrown);
+		}
+	};
 }
