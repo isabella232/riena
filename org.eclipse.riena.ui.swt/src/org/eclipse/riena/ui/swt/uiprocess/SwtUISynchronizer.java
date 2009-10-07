1a1abbf6c5d8451de657475f77fcfbbf4e9f32f4
Stefan Liebig <stefan.liebig@compeople.de>
2009-10-07 11:29:48 +0000

    a little bit more efficent

--- org.eclipse.riena.ui.swt/src/org/eclipse/riena/ui/swt/uiprocess/SwtUISynchronizer.java
+++ org.eclipse.riena.ui.swt/src/org/eclipse/riena/ui/swt/uiprocess/SwtUISynchronizer.java
@@ -16,23 +16,30 @@ import org.eclipse.riena.core.util.ReflectionUtils;
 import org.eclipse.riena.ui.core.uiprocess.IUISynchronizer;
 
 /**
- * serializes a runnable to the SWT-Thread
+ * serializes a runnable to the SWT-Thread - if possible
  */
 public class SwtUISynchronizer implements IUISynchronizer {
 
-	private Display display;
-
 	public void synchronize(Runnable r) {
-		synchronized (this) {
+		if (hasDisplay()) {
+			Display.getDefault().syncExec(r);
+		} else {
+			r.run();
+		}
+	}
+
+	private static Display display;
+
+	private boolean hasDisplay() {
+		if (display != null) {
+			return true;
+		}
+		synchronized (SwtUISynchronizer.class) {
 			if (display == null) {
 				// TODO [sli] Another hack - How can I find out whether the Display has been set by the correct thread?
 				display = ReflectionUtils.getHidden(Display.class, "Default"); //$NON-NLS-1$
 			}
 		}
-		if (display == null) {
-			r.run();
-		} else {
-			Display.getDefault().syncExec(r);
-		}
+		return display != null;
 	}
 }
