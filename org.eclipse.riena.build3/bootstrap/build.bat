4cbe6151b915b389a402f6d9bbf5b198bcef17b9
Christian Campo <christian.campo@compeople.de>
2009-11-17 09:31:16 +0000

    new build scripts and properties to enable builds for a specific tag

--- org.eclipse.riena.build3/bootstrap/build.bat
+++ org.eclipse.riena.build3/bootstrap/build.bat
@@ -3,6 +3,7 @@ set JAVA_HOME=%TOOLSROOT%\jdk1.5.0_18
 set ANT_HOME=%TOOLSROOT%\apache-ant-1.7.1
 set CVS_HOME_BIN=%TOOLSROOT%\cygwin\bin
 set PATH=%JAVA_HOME%\bin;%ANT_HOME%\bin;%CVS_HOME_BIN%
+set FETCHTAG_PARM=HEAD
 
 c:
 cd \build3
@@ -14,8 +15,15 @@ echo.
 java -version
 echo.
 
+if '%2' EQU '' GOTO :CONT
+
+set FETCHTAG_PARM=%2
+
+:CONT
+
 if '%1' EQU 'build' GOTO :BUILD
 if '%1' EQU 'runtests' GOTO :RUNTESTS
+
 
 echo Usage:
 echo build build	- Build Riena
@@ -23,7 +31,8 @@ echo build runtests	- Run tests (must build first)
 GOTO :EOF
 
 :BUILD
-ant -f build.xml clean build
+echo Building version %FETCHTAG_PARM%
+ant -f build.xml -DFETCHTAG_PARM=%FETCHTAG_PARM% clean build
 GOTO :EOF
 
 :RUNTESTS
