3e72e79c9b274b67dd8db6be92c63e00850e1fd0
Sabine Achilles <sabine.achilles@compeople.de>
2009-12-17 11:30:04 +0000

    added new test

--- org.eclipse.riena.tests/src/org/eclipse/riena/client/controller/test/ComboSubModuleControllerTest.java
+++ org.eclipse.riena.tests/src/org/eclipse/riena/client/controller/test/ComboSubModuleControllerTest.java
@@ -10,11 +10,14 @@
  *******************************************************************************/
 package org.eclipse.riena.client.controller.test;
 
+import java.util.List;
+
 import org.eclipse.riena.beans.common.Person;
 import org.eclipse.riena.beans.common.PersonFactory;
 import org.eclipse.riena.example.client.controllers.ComboSubModuleController;
 import org.eclipse.riena.internal.core.test.collect.NonUITestCase;
 import org.eclipse.riena.navigation.ISubModuleNode;
+import org.eclipse.riena.ui.ridgets.IActionRidget;
 import org.eclipse.riena.ui.ridgets.IComboRidget;
 import org.eclipse.riena.ui.ridgets.ITextRidget;
 
@@ -29,27 +32,46 @@ public class ComboSubModuleControllerTest extends AbstractSubModuleControllerTes
 		return new ComboSubModuleController(node);
 	}
 
-	public void testJohnDoe() {
-
-		IComboRidget comboOne = controller.getRidget(IComboRidget.class, "comboOne"); //$NON-NLS-1$
-		comboOne.setSelection(0);
-
-		Person expected = PersonFactory.createPersonList().get(0);
-		assertEquals(((Person) comboOne.getSelection()).getFirstname(), expected.getFirstname());
-		assertEquals(((Person) comboOne.getSelection()).getLastname(), expected.getLastname());
-
-		ITextRidget textFirst = controller.getRidget(ITextRidget.class, "textFirst"); //$NON-NLS-1$
-		ITextRidget textLast = controller.getRidget(ITextRidget.class, "textLast"); //$NON-NLS-1$
-
-		assertEquals(textFirst.getText(), expected.getFirstname());
-		assertEquals(textLast.getText(), expected.getLastname());
+	public void testComboSelection() {
+		basicTestComboSelection(0);
+		basicTestComboSelection(4);
 	}
 
 	public void testNoSelection() {
+		IComboRidget combo = controller.getRidget(IComboRidget.class, "comboOne");
+		combo.setSelection(null);
+		assertEquals(-1, combo.getSelectionIndex());
+		assertEquals(null, combo.getSelection());
+	}
 
-		IComboRidget comboOne = controller.getRidget(IComboRidget.class, "comboOne"); //$NON-NLS-1$
-		comboOne.setSelection(null);
-		assertEquals(-1, comboOne.getSelectionIndex());
-		assertEquals(null, comboOne.getSelection());
+	public void testSaveName() {
+		ITextRidget textFirst = controller.getRidget(ITextRidget.class, "textFirst");
+		ITextRidget textLast = controller.getRidget(ITextRidget.class, "textLast");
+		IComboRidget combo = controller.getRidget(IComboRidget.class, "comboOne");
+		IActionRidget saveButton = controller.getRidget(IActionRidget.class, "buttonSave");
+
+		// set new name and save
+		Person newPerson = (Person) combo.getSelection();
+		textFirst.setText("Jane");
+		textLast.setText("Fonda");
+
+		saveButton.fireAction();
+
+		assertEquals(newPerson, combo.getSelection());
+	}
+
+	private void basicTestComboSelection(int index) {
+		ITextRidget textFirst = controller.getRidget(ITextRidget.class, "textFirst");
+		ITextRidget textLast = controller.getRidget(ITextRidget.class, "textLast");
+		IComboRidget combo = controller.getRidget(IComboRidget.class, "comboOne");
+		List<Person> expected = PersonFactory.createPersonList();
+
+		combo.setSelection(index);
+
+		assertEquals(((Person) combo.getSelection()).getFirstname(), expected.get(index).getFirstname());
+		assertEquals(((Person) combo.getSelection()).getLastname(), expected.get(index).getLastname());
+
+		assertEquals(textFirst.getText(), expected.get(index).getFirstname());
+		assertEquals(textLast.getText(), expected.get(index).getLastname());
 	}
 }
