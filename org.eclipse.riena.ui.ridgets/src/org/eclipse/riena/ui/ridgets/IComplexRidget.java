afb4e4cb15a6a0b8301c425a91f4466bc2a4a902
Elias Volanakis <elias@eclipsesource.com>
2009-12-17 19:15:32 +0000

    Javadoc

--- org.eclipse.riena.ui.ridgets/src/org/eclipse/riena/ui/ridgets/IComplexRidget.java
+++ org.eclipse.riena.ui.ridgets/src/org/eclipse/riena/ui/ridgets/IComplexRidget.java
@@ -13,10 +13,21 @@ package org.eclipse.riena.ui.ridgets;
 import org.eclipse.riena.ui.common.IComplexComponent;
 
 /**
- * A Ridget that is bound to an IComplexComponent. The implementation of the
- * IComplexRidget must have a property for each UI-control of the
- * IComplexComponent. The type of the property must be a Ridget class matching
- * the type of the UI control.
+ * A Ridget that containts other ridgets.
+ * <p>
+ * Implementors are advised to subclass {@link AbstractCompositeRidget} and
+ * override the widget-specific protected methods. Implementations are often
+ * bound against an IComplexCompontent widget, but do not have to.
+ * <p>
+ * To enable API-like access to the ridgets contained herein, it is recommended
+ * to expose the IDs of the contained ridgets via constants, such as
+ * <tt>MyComplexRidget.RIDGET_BTN_DELETE</tt>. These constants can be used with
+ * {@link #getRidget(String)} or {@link #getRidget(Class, String)} to obtain a
+ * reference to the contained ridget. Example:
+ * 
+ * <pre>
+ * complexRidget.getRidget(MyComplexRidget.RIDGET_BTN_DELETE);
+ * </pre>
  * 
  * @see IComplexComponent
  */
