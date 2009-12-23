5cfeeeba0d354f5cebbff93a1c24b1b453e23dfa
Thorsten Schenkel <thorsten.schenkel@compeople.de>
2009-12-23 13:46:21 +0000

    java doc supplemented

--- org.eclipse.riena.ui.filter/src/org/eclipse/riena/ui/filter/extension/IRuleMapperExtension.java
+++ org.eclipse.riena.ui.filter/src/org/eclipse/riena/ui/filter/extension/IRuleMapperExtension.java
@@ -13,29 +13,83 @@ package org.eclipse.riena.ui.filter.extension;
 import org.eclipse.riena.core.injector.extension.ExtensionInterface;
 
 /**
- * Mappers for all rules.
+ * To avoid problems with bundle dependencies (e.g. cycles) the classes of the
+ * different rules of the UI Filters are defined with extensions.
  * <p>
  * <b>Note:</b> The "org.eclipse.riena.filter.rulemapper" is @deprecated.
  */
 @ExtensionInterface(id = "org.eclipse.riena.filter.rulemapper,ruleMapper")
 public interface IRuleMapperExtension {
 
+	/**
+	 * Returns the type that maps the rule to add a {@code HiddenMarker} to a
+	 * ridget.
+	 * 
+	 * @return mapper of {@code HiddenMarker} for ridget
+	 */
 	IRuleMarkerRidgetMapper getRidgetHiddenMarker();
 
+	/**
+	 * Returns the type that maps the rule to add a {@code DisabledMarker} to a
+	 * ridget.
+	 * 
+	 * @return mapper of {@code DisabledMarker} for ridget
+	 */
 	IRuleMarkerRidgetMapper getRidgetDisabledMarker();
 
+	/**
+	 * Returns the type that maps the rule to add a {@code OutputMarker} to a
+	 * ridget.
+	 * 
+	 * @return mapper of {@code OutputMarker} for ridget
+	 */
 	IRuleMarkerRidgetMapper getRidgetOutputMarker();
 
+	/**
+	 * Returns the type that maps the rule to add a {@code MandatoryMarker} to a
+	 * ridget.
+	 * 
+	 * @return mapper of {@code MandatoryMarker} for ridget
+	 */
 	IRuleMarkerRidgetMapper getRidgetMandatoryMarker();
 
+	/**
+	 * Returns the type that maps the rule to add a {@code HiddenMarker} to a
+	 * menu item.
+	 * 
+	 * @return mapper of {@code HiddenMarker} for menu item
+	 */
 	IRuleMarkerRidgetMapper getMenuItemHiddenMarker();
 
+	/**
+	 * Returns the type that maps the rule to add a {@code DisabledMarker} to a
+	 * menu item.
+	 * 
+	 * @return mapper of {@code DisabledMarker} for menu item
+	 */
 	IRuleMarkerRidgetMapper getMenuItemDisabledMarker();
 
+	/**
+	 * Returns the type that maps the rule to add a {@code HiddenMarker} to a
+	 * navigation node.
+	 * 
+	 * @return mapper of {@code HiddenMarker} for navigation node
+	 */
 	IRuleMarkerNavigationMapper getNavigationHiddenMarker();
 
+	/**
+	 * Returns the type that maps the rule to add a {@code DisabledMarker} to a
+	 * navigation node.
+	 * 
+	 * @return mapper of {@code DisabledMarker} for navigation node
+	 */
 	IRuleMarkerNavigationMapper getNavigationDisabledMarker();
 
+	/**
+	 * Returns the type that maps the rule to add a validator to a ridget.
+	 * 
+	 * @return mapper of validator for ridget
+	 */
 	IRuleValidatorRidgetMapper getRidgetValidator();
 
 }
