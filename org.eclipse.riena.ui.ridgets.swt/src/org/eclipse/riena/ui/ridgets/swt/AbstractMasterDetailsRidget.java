eab96e8a1fa3ae8cc12667f3c0fb43505becf460
Elias Volanakis <elias@eclipsesource.com>
2010-01-27 04:51:56 +0000

    minor fix - 2nd try

--- org.eclipse.riena.ui.ridgets.swt/src/org/eclipse/riena/ui/ridgets/swt/AbstractMasterDetailsRidget.java
+++ org.eclipse.riena.ui.ridgets.swt/src/org/eclipse/riena/ui/ridgets/swt/AbstractMasterDetailsRidget.java
@@ -435,7 +435,7 @@ public abstract class AbstractMasterDetailsRidget extends AbstractCompositeRidge
 
 	private IRidget getTableRidget() {
 		// this is not necessarily an ITableRidget, can be any IRidget
-		return getRidget(IRidget.class, MasterDetailsComposite.BIND_ID_TABLE);
+		return (IRidget) getRidget(MasterDetailsComposite.BIND_ID_TABLE);
 	}
 
 	private IActionRidget getNewButtonRidget() {
