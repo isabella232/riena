f05a1c75863aa007a70845c1046f1339dac4351f
Christian Campo <christian.campo@compeople.de>
2010-02-03 12:50:15 +0000

    added 2 more callbacks for ServiceContext

--- org.eclipse.riena.communication.core/src/org/eclipse/riena/communication/core/hooks/IServiceMessageContext.java
+++ org.eclipse.riena.communication.core/src/org/eclipse/riena/communication/core/hooks/IServiceMessageContext.java
@@ -15,14 +15,43 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+/**
+ * Protocol independant interface that gives access to lowlevel properties of
+ * the protocol implementation
+ */
 public interface IServiceMessageContext {
 
+	/**
+	 * Returns a Map of headers, typical HTTP Headers
+	 * 
+	 * @return Map of HTTP Headers
+	 */
 	Map<String, List<String>> listRequestHeaders();
 
+	/**
+	 * Returns the value for a specific HTTP header
+	 * 
+	 * @param name
+	 *            name of HTTP Header i.e. "Cookie"
+	 * @return value of the HTTP Header if found, otherwise it returns null
+	 */
 	List<String> getRequestHeaderValue(String name);
 
+	/**
+	 * Adds a HTTP header to the response
+	 * 
+	 * @param name
+	 *            name of HTTP Header to add
+	 * @param value
+	 *            value of HTTP Header to add
+	 */
 	void addResponseHeader(String name, String value);
 
+	/**
+	 * Access for the servlet request for this remote service call
+	 * 
+	 * @return ServletRequest object i.e. HttpServletRequest object
+	 */
 	HttpServletRequest getServletRequest();
 
 }
--- org.eclipse.riena.communication.core/src/org/eclipse/riena/communication/core/hooks/ServiceContext.java
+++ org.eclipse.riena.communication.core/src/org/eclipse/riena/communication/core/hooks/ServiceContext.java
@@ -10,6 +10,7 @@
  *******************************************************************************/
 package org.eclipse.riena.communication.core.hooks;
 
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -18,6 +19,10 @@ import javax.servlet.http.Cookie;
 
 import org.eclipse.riena.communication.core.RemoteServiceDescription;
 
+/**
+ * Allows a ServiceHook to access information associated with the current remote
+ * service call
+ */
 public class ServiceContext {
 
 	private RemoteServiceDescription rsd;
@@ -31,14 +36,68 @@ public class ServiceContext {
 		this.messageContext = messageContext;
 	}
 
+	/**
+	 * The name as string of the remote service interface
+	 * 
+	 * @return
+	 */
 	public String getInterfaceName() {
 		return rsd.getServiceInterfaceClassName();
 	}
 
+	/**
+	 * The name as class object of the remote service interface
+	 * 
+	 * @return
+	 */
+	public Class<?> getInterfaceClass() {
+		return rsd.getServiceInterfaceClass();
+	}
+
+	/**
+	 * The method that is called as string.
+	 * 
+	 * @return
+	 */
 	public String getMethodName() {
 		return method;
 	}
 
+	/**
+	 * The method that is called as Method object.
+	 * 
+	 * @return
+	 * @throws UnsupportedOperationException
+	 *             if more than one method with the method name are found. That
+	 *             is currently not supported.
+	 */
+	public Method getMethod() {
+		Class<?> interf = getInterfaceClass();
+		String methodName = getMethodName();
+		Method[] methods = interf.getMethods();
+		Method foundMethod = null;
+		for (Method method : methods) {
+			if (method.getName().equals(methodName)) {
+				if (foundMethod == null) {
+					foundMethod = method;
+				} else {
+					throw new UnsupportedOperationException("More than one method with the same name '" + methodName
+							+ "' found.");
+				}
+			}
+		}
+		return foundMethod;
+	}
+
+	/**
+	 * Arbitrary property that can be associated with this call. Accessible in
+	 * beforeService and afterService
+	 * 
+	 * @param name
+	 *            key
+	 * @param value
+	 *            value for that key
+	 */
 	public void setProperty(String name, Object value) {
 		if (properties == null) {
 			properties = new HashMap<String, Object>();
@@ -47,6 +106,12 @@ public class ServiceContext {
 		return;
 	}
 
+	/**
+	 * Getter access to key/value property store in setProperty
+	 * 
+	 * @param name
+	 * @return
+	 */
 	public Object getProperty(String name) {
 		if (properties == null) {
 			return null;
@@ -54,10 +119,22 @@ public class ServiceContext {
 		return properties.get(name);
 	}
 
+	/**
+	 * Access to the ServiceMessageContext that has more lowlevel access to
+	 * properties
+	 * 
+	 * @return
+	 */
 	public IServiceMessageContext getMessageContext() {
 		return messageContext;
 	}
 
+	/**
+	 * Accessor to read cookies that where transferred as part of the remote
+	 * service call. Currently used for session information.
+	 * 
+	 * @return
+	 */
 	public Cookie[] getCookies() {
 		List<String> cookieValues = messageContext.getRequestHeaderValue("Cookie"); //$NON-NLS-1$
 		if (cookieValues == null || cookieValues.size() == 0) {
@@ -70,6 +147,12 @@ public class ServiceContext {
 		return cookies.toArray(new Cookie[cookies.size()]);
 	}
 
+	/**
+	 * Adds a new Set-Cookie on the service side, which is sent back to the
+	 * client when the remote service call returns
+	 * 
+	 * @param cookie
+	 */
 	public void addCookie(Cookie cookie) {
 		messageContext.addResponseHeader("Set-Cookie", cookie.getName() + "=" + cookie.getValue()); //$NON-NLS-1$ //$NON-NLS-2$
 	}
