9244192dfdfa658b7a86802212afd72e4cbc5fab
Stefan Liebig <stefan.liebig@compeople.de>
2010-02-17 07:35:14 +0000

    removed unused method and added javadoc

--- org.eclipse.riena.communication.factory.hessian/src/org/eclipse/riena/internal/communication/factory/hessian/ISerializerFactoryExtension.java
+++ org.eclipse.riena.communication.factory.hessian/src/org/eclipse/riena/internal/communication/factory/hessian/ISerializerFactoryExtension.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2009 compeople AG and others.
+ * Copyright (c) 2007, 2010 compeople AG and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -22,8 +22,11 @@ import org.eclipse.riena.core.injector.extension.ExtensionInterface;
 @ExtensionInterface(id = "serializerFactories,org.eclipse.riena.communication.hessian.AbstractSerializerFactory")
 public interface ISerializerFactoryExtension {
 
+	/**
+	 * Create an instance of the {@code AbstractRienaSerializerFactory}.
+	 * 
+	 * @return a {@code AbstractRienaSerializerFactory}
+	 */
 	AbstractRienaSerializerFactory createImplementation();
-
-	String getImplementation();
 
 }
