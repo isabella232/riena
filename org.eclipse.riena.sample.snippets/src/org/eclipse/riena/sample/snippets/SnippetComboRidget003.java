271c84ec32df1cd00ae0ae12c5e34b837a795c25
Elias Volanakis <elias@eclipsesource.com>
2010-03-06 01:14:51 +0000

    304733: Combo/CCombo: show error marker when current selection becomes unavailable

--- org.eclipse.riena.sample.snippets/src/org/eclipse/riena/sample/snippets/SnippetComboRidget003.java
+++ org.eclipse.riena.sample.snippets/src/org/eclipse/riena/sample/snippets/SnippetComboRidget003.java
@@ -10,21 +10,26 @@
  *******************************************************************************/
 package org.eclipse.riena.sample.snippets;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.core.databinding.conversion.IConverter;
 import org.eclipse.jface.layout.GridDataFactory;
-import org.eclipse.swt.custom.CCombo;
-import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 
 import org.eclipse.riena.beans.common.ListBean;
+import org.eclipse.riena.beans.common.StringBean;
 import org.eclipse.riena.ui.ridgets.IActionListener;
 import org.eclipse.riena.ui.ridgets.IActionRidget;
 import org.eclipse.riena.ui.ridgets.IComboRidget;
+import org.eclipse.riena.ui.ridgets.ILabelRidget;
 import org.eclipse.riena.ui.ridgets.swt.SwtRidgetFactory;
 import org.eclipse.riena.ui.swt.utils.UIControlsFactory;
 
@@ -38,24 +43,54 @@ public class SnippetComboRidget003 {
 
 		try {
 			Shell shell = UIControlsFactory.createShell(display);
-			shell.setLayout(new GridLayout(2, false));
 			shell.setText(SnippetComboRidget003.class.getSimpleName());
+			GridLayoutFactory.swtDefaults().numColumns(2).spacing(5, 5).applyTo(shell);
 
 			UIControlsFactory.createLabel(shell, "Combo:"); //$NON-NLS-1$
-			CCombo combo = UIControlsFactory.createCCombo(shell);
+			Combo combo = UIControlsFactory.createCombo(shell);
 
 			UIControlsFactory.createLabel(shell, "Selection:"); //$NON-NLS-1$
-			Label label = UIControlsFactory.createLabel(shell, "Monday"); //$NON-NLS-1$
-			GridDataFactory.fillDefaults().grab(true, false).applyTo(label);
+			final Label lblSelection = UIControlsFactory.createLabel(shell, ""); //$NON-NLS-1$
+			GridDataFactory.fillDefaults().grab(true, false).applyTo(lblSelection);
 
 			Button button = UIControlsFactory.createButton(shell, "&Remove Selection"); //$NON-NLS-1$
 			GridDataFactory.fillDefaults().span(2, 1).applyTo(button);
 
-			final IComboRidget comboRidget = (IComboRidget) SwtRidgetFactory.createRidget(combo);
+			// data model
+
 			final ListBean input = createInput();
-			comboRidget.bindToModel(input, ListBean.PROPERTY_VALUES, String.class, null, label, "text"); //$NON-NLS-1$
+			final StringBean selection = new StringBean("Monday"); //$NON-NLS-1$
+
+			// ridgets
+
+			final IComboRidget comboRidget = (IComboRidget) SwtRidgetFactory.createRidget(combo);
+			comboRidget.bindToModel(input, ListBean.PROPERTY_VALUES, String.class, null, selection,
+					StringBean.PROP_VALUE);
 			comboRidget.updateFromModel();
 			comboRidget.setMandatory(true);
+
+			final ILabelRidget labelRidget = (ILabelRidget) SwtRidgetFactory.createRidget(lblSelection);
+			labelRidget.setModelToUIControlConverter(new IConverter() {
+				public Object getToType() {
+					return String.class;
+				}
+
+				public Object getFromType() {
+					return String.class;
+				}
+
+				public Object convert(Object fromObject) {
+					return String.valueOf(fromObject);
+				}
+			});
+			labelRidget.bindToModel(selection, StringBean.PROP_VALUE);
+			labelRidget.updateFromModel();
+			selection.addPropertyChangeListener(new PropertyChangeListener() {
+				public void propertyChange(PropertyChangeEvent evt) {
+					labelRidget.updateFromModel();
+					comboRidget.setErrorMarked(selection.getValue() == null);
+				}
+			});
 
 			IActionRidget actionRidget = (IActionRidget) SwtRidgetFactory.createRidget(button);
 			actionRidget.addListener(new IActionListener() {
@@ -79,7 +114,7 @@ public class SnippetComboRidget003 {
 
 	private static ListBean createInput() {
 		List<String> days = new ArrayList<String>();
-		for (String day : new String[] { "<Empty>", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$
+		for (String day : new String[] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$
 				"Sunday" }) { //$NON-NLS-1$
 			days.add(day);
 		}
