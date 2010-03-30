40dde9eedad3be1963a777f6d8829e4cddbf6cbf
Elias Volanakis <elias@eclipsesource.com>
2010-03-30 06:08:26 +0000

    Refactoring - DRY

--- org.eclipse.riena.sample.snippets/src/org/eclipse/riena/sample/snippets/SnippetTableRidget002.java
+++ org.eclipse.riena.sample.snippets/src/org/eclipse/riena/sample/snippets/SnippetTableRidget002.java
@@ -10,9 +10,6 @@
  *******************************************************************************/
 package org.eclipse.riena.sample.snippets;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.eclipse.core.databinding.observable.list.IObservableList;
 import org.eclipse.core.databinding.observable.list.WritableList;
 import org.eclipse.jface.layout.TableColumnLayout;
@@ -23,6 +20,8 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
 
+import org.eclipse.riena.beans.common.SuperHero;
+import org.eclipse.riena.beans.common.SuperHeroFactory;
 import org.eclipse.riena.beans.common.TypedComparator;
 import org.eclipse.riena.ui.ridgets.ITableRidget;
 import org.eclipse.riena.ui.ridgets.swt.SwtRidgetFactory;
@@ -50,7 +49,7 @@ public class SnippetTableRidget002 {
 		ITableRidget tableRidget = (ITableRidget) SwtRidgetFactory.createRidget(table);
 		String[] columnPropertyNames = { "pseudonym", "name", "appearance" }; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ 
 		String[] columnHeaders = { "Pseudonym", "Name", "First appearance" }; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ 
-		IObservableList input = new WritableList(createInput(), SuperHero.class);
+		IObservableList input = new WritableList(SuperHeroFactory.createInput(), SuperHero.class);
 		tableRidget.bindToModel(input, SuperHero.class, columnPropertyNames, columnHeaders);
 		tableRidget.setComparator(0, new TypedComparator<String>());
 		tableRidget.setColumnSortable(1, false);
@@ -77,46 +76,4 @@ public class SnippetTableRidget002 {
 		}
 	}
 
-	// helping methods
-	//////////////////
-
-	private List<SuperHero> createInput() {
-		List<SuperHero> nodes = new ArrayList<SuperHero>(7);
-		nodes.add(new SuperHero("Superman", "Clark Kent", 1938)); //$NON-NLS-1$ //$NON-NLS-2$
-		nodes.add(new SuperHero("Batman", "Bruce Wayne", 1939)); //$NON-NLS-1$ //$NON-NLS-2$
-		nodes.add(new SuperHero("Zorro", "Don Diego de la Vega", 1919)); //$NON-NLS-1$ //$NON-NLS-2$
-		nodes.add(new SuperHero("Flash Gordon", "Gordon Ferrao", 1934)); //$NON-NLS-1$ //$NON-NLS-2$
-		nodes.add(new SuperHero("Hulk", "Bruce Banner", 1962)); //$NON-NLS-1$ //$NON-NLS-2$
-		nodes.add(new SuperHero("Spider-Man", "Peter Parker", 1962)); //$NON-NLS-1$ //$NON-NLS-2$
-		nodes.add(new SuperHero("Silver Surfer", "Norrin Radd", 1966)); //$NON-NLS-1$ //$NON-NLS-2$
-
-		return nodes;
-	}
-
-	private static final class SuperHero {
-		private String pseudonym;
-		private String name;
-		private Integer appearance;
-
-		public SuperHero(String pseudonym, String name, int appearance) {
-			this.pseudonym = pseudonym;
-			this.name = name;
-			this.appearance = appearance;
-		}
-
-		@SuppressWarnings("unused")
-		public String getPseudonym() {
-			return pseudonym;
-		}
-
-		@SuppressWarnings("unused")
-		public String getName() {
-			return name;
-		}
-
-		@SuppressWarnings("unused")
-		public Integer getAppearance() {
-			return appearance;
-		}
-	}
 }
--- org.eclipse.riena.sample.snippets/src/org/eclipse/riena/sample/snippets/SnippetTableRidget007.java
+++ org.eclipse.riena.sample.snippets/src/org/eclipse/riena/sample/snippets/SnippetTableRidget007.java
@@ -10,7 +10,6 @@
  *******************************************************************************/
 package org.eclipse.riena.sample.snippets;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.core.databinding.observable.list.IObservableList;
@@ -24,6 +23,8 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Table;
 
+import org.eclipse.riena.beans.common.SuperHero;
+import org.eclipse.riena.beans.common.SuperHeroFactory;
 import org.eclipse.riena.core.marker.IMarker;
 import org.eclipse.riena.ui.core.marker.RowErrorMessageMarker;
 import org.eclipse.riena.ui.ridgets.IActionListener;
@@ -54,7 +55,7 @@ public class SnippetTableRidget007 {
 		final ITableRidget tableRidget = (ITableRidget) SwtRidgetFactory.createRidget(table);
 		String[] columnPropertyNames = { "pseudonym", "name", "appearance" }; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ 
 		String[] columnHeaders = { "Pseudonym", "Name", "First appearance" }; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ 
-		IObservableList input = new WritableList(createInput(), SuperHero.class);
+		IObservableList input = new WritableList(SuperHeroFactory.createInput(), SuperHero.class);
 		tableRidget.bindToModel(input, SuperHero.class, columnPropertyNames, columnHeaders);
 		tableRidget.updateFromModel();
 		tableRidget.setToolTipText("The ORIGINAL table tooltip!"); //$NON-NLS-1$
@@ -114,51 +115,4 @@ public class SnippetTableRidget007 {
 		}
 	}
 
-	// helping methods
-	//////////////////
-
-	private List<SuperHero> createInput() {
-		List<SuperHero> nodes = new ArrayList<SuperHero>(7);
-		nodes.add(new SuperHero("Superman", "Clark Kent", 1938)); //$NON-NLS-1$ //$NON-NLS-2$
-		nodes.add(new SuperHero("Batman", "Bruce Wayne", 1939)); //$NON-NLS-1$ //$NON-NLS-2$
-		nodes.add(new SuperHero("Zorro", "Don Diego de la Vega", 1919)); //$NON-NLS-1$ //$NON-NLS-2$
-		nodes.add(new SuperHero("Flash Gordon", "Gordon Ferrao", 1934)); //$NON-NLS-1$ //$NON-NLS-2$
-		nodes.add(new SuperHero("Hulk", "Bruce Banner", 1962)); //$NON-NLS-1$ //$NON-NLS-2$
-		nodes.add(new SuperHero("Spider-Man", "Peter Parker", 1962)); //$NON-NLS-1$ //$NON-NLS-2$
-		nodes.add(new SuperHero("Silver Surfer", "Norrin Radd", 1966)); //$NON-NLS-1$ //$NON-NLS-2$
-
-		return nodes;
-	}
-
-	private static final class SuperHero {
-		private String pseudonym;
-		private String name;
-		private Integer appearance;
-
-		public SuperHero(String pseudonym, String name, int appearance) {
-			this.pseudonym = pseudonym;
-			this.name = name;
-			this.appearance = appearance;
-		}
-
-		@SuppressWarnings("unused")
-		public String getPseudonym() {
-			return pseudonym;
-		}
-
-		@SuppressWarnings("unused")
-		public String getName() {
-			return name;
-		}
-
-		@SuppressWarnings("unused")
-		public Integer getAppearance() {
-			return appearance;
-		}
-
-		@Override
-		public String toString() {
-			return String.format("%s (%s)", pseudonym, name); //$NON-NLS-1$
-		}
-	}
 }
