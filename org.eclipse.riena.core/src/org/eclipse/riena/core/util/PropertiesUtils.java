ca442f3ccdc2327be532ff3cd75306770181b7c7
Stefan Liebig <stefan.liebig@compeople.de>
2010-04-13 09:28:46 +0000

    refactored: uses now the more general RichString class

--- org.eclipse.riena.core/src/org/eclipse/riena/core/util/PropertiesUtils.java
+++ org.eclipse.riena.core/src/org/eclipse/riena/core/util/PropertiesUtils.java
@@ -10,14 +10,10 @@
  *******************************************************************************/
 package org.eclipse.riena.core.util;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Hashtable;
-import java.util.List;
 import java.util.Map;
-import java.util.StringTokenizer;
 
 import org.eclipse.core.runtime.Assert;
 
@@ -26,13 +22,6 @@ import org.eclipse.core.runtime.Assert;
  * of maps and lists to their Java counter parts.
  */
 public final class PropertiesUtils {
-
-	// Delimiter of pairs in a map.
-	private static final String PAIR_DELIM = ";"; //$NON-NLS-1$
-	// Delimiter of items in a list
-	private static final char LIST_DELIM = ',';
-	// Escape character for lists
-	private static final char ESCAPE_CHAR = '\\';
 
 	private PropertiesUtils() {
 		// Utility
@@ -49,7 +38,7 @@ public final class PropertiesUtils {
 	 * @return the String value for propValue or the default value
 	 *         <code>returnIfNoString</code>
 	 */
-	public static String accessProperty(Object propValue, String returnIfNoString) {
+	public static String accessProperty(final Object propValue, final String returnIfNoString) {
 		if (propValue instanceof String) { // if api programmed we receive
 			return (String) propValue; // a String
 		} else if (propValue instanceof String[]) { // for DS we receive a
@@ -86,7 +75,7 @@ public final class PropertiesUtils {
 	 *             for any errors
 	 * @see IExecutableExtension.setInitializationData()
 	 */
-	public static Map<String, String> asMap(Object data, String... expectedKeys) {
+	public static Map<String, String> asMap(final Object data, final String... expectedKeys) {
 		return asMap(data, null, expectedKeys);
 	}
 
@@ -122,41 +111,28 @@ public final class PropertiesUtils {
 	 * @see IExecutableExtension.setInitializationData()
 	 */
 	@SuppressWarnings("unchecked")
-	public static Map<String, String> asMap(Object data, Map<String, String> defaults, String... expectedKeys) {
+	public static Map<String, String> asMap(final Object data, final Map<String, String> defaults,
+			final String... expectedKeys) {
 		Map<String, String> result = null;
 		if (data == null) {
 			result = new HashMap<String, String>();
-		} else if (data instanceof Hashtable) {
+		} else if (data instanceof Hashtable<?, ?>) {
 			result = (Hashtable<String, String>) data;
 		} else if (data instanceof String) {
-			String stringified = (String) data;
-			result = new HashMap<String, String>();
-			if (StringUtils.isEmpty(stringified)) {
-				Assert.isLegal(expectedKeys.length == 0,
-						"Excpeted keys " + Arrays.toString(expectedKeys) + " not found in empty string."); //$NON-NLS-1$ //$NON-NLS-2$
-				return new HashMap<String, String>();
-			}
-			StringTokenizer parts = new StringTokenizer(stringified, PAIR_DELIM);
-			while (parts.hasMoreTokens()) {
-				String part = parts.nextToken();
-				int equal = part.indexOf('=');
-				Assert.isLegal(equal > 0, "Error within definition. Expecting a string of the form: " //$NON-NLS-1$
-						+ " [ <key> \"=\" <value> ] { [ \"" + PAIR_DELIM + "\" <key> \"=\" <value> ] }"); //$NON-NLS-1$ //$NON-NLS-2$
-				result.put(part.substring(0, equal).trim(), part.substring(equal + 1).trim());
-			}
+			result = new RichString((String) data).toMap();
 		} else {
 			Assert.isLegal(false, "Can not deal with data type: " + data.getClass().getName()); //$NON-NLS-1$
 		}
 		// if optional defaults add them if necessary
 		if (defaults != null) {
-			for (Map.Entry<String, String> entry : defaults.entrySet()) {
+			for (final Map.Entry<String, String> entry : defaults.entrySet()) {
 				if (!result.containsKey(entry.getKey())) {
 					result.put(entry.getKey(), entry.getValue());
 				}
 			}
 		}
 		// validate optional expected keys
-		for (String expectedKey : expectedKeys) {
+		for (final String expectedKey : expectedKeys) {
 			Assert.isLegal(result.containsKey(expectedKey), "data " + data + "does not contain expected key " //$NON-NLS-1$ //$NON-NLS-2$
 					+ expectedKey + "."); //$NON-NLS-1$
 		}
@@ -164,8 +140,6 @@ public final class PropertiesUtils {
 	}
 
 	private static final String[] EMPTY_STRING_ARRAY = new String[0];
-	private static final int CHAR = 0;
-	private static final int BACK_SLASH = 1;
 
 	/**
 	 * Transform the string representation of a list into a array.<br>
@@ -184,48 +158,11 @@ public final class PropertiesUtils {
 	 * @throws IllegalArgumentException
 	 *             in case of errors
 	 */
-	public static String[] asArray(Object data) {
+	public static String[] asArray(final Object data) {
 		if (data == null) {
 			return EMPTY_STRING_ARRAY;
 		}
 		Assert.isLegal(data instanceof String, "data must be of type String."); //$NON-NLS-1$
-		String stringified = (String) data;
-		if (StringUtils.isEmpty(stringified)) {
-			return EMPTY_STRING_ARRAY;
-		}
-		List<String> result = new ArrayList<String>();
-		StringBuilder value = new StringBuilder();
-		char ch = 0;
-		int state = CHAR;
-		for (int i = 0; i < stringified.length(); i++) {
-			ch = stringified.charAt(i);
-			switch (state) {
-			case CHAR:
-				if (ch == ESCAPE_CHAR) {
-					state = BACK_SLASH;
-				} else if (ch == LIST_DELIM) {
-					result.add(value.toString());
-					value.setLength(0);
-				} else {
-					value.append(ch);
-				}
-				break;
-			case BACK_SLASH:
-				if (ch == ESCAPE_CHAR || ch == LIST_DELIM) {
-					value.append(ch);
-				} else {
-					Assert.isLegal(false, "Unknown escaped character: " + ch + "."); //$NON-NLS-1$ //$NON-NLS-2$
-				}
-				state = CHAR;
-				break;
-			default:
-				break;
-			}
-		}
-		// final comma
-		if (value.length() != 0 || ch == LIST_DELIM) {
-			result.add(value.toString());
-		}
-		return result.toArray(new String[result.size()]);
+		return new RichString((String) data).toArray();
 	}
 }
