c11608b7aa2062b976d722bb9da5a8d4b10842b2
Christian Campo <christian.campo@compeople.de>
2010-04-21 09:33:27 +0000

    added javadoc

--- org.eclipse.riena.communication.core/src/org/eclipse/riena/communication/core/progressmonitor/IRemoteProgressMonitorList.java
+++ org.eclipse.riena.communication.core/src/org/eclipse/riena/communication/core/progressmonitor/IRemoteProgressMonitorList.java
@@ -20,12 +20,38 @@ public interface IRemoteProgressMonitorList {
 
 	int BYTE_COUNT_INCR = 512;
 
+	/**
+	 * Fired when first byte is sent in a remote service call
+	 */
 	void fireStartEvent();
 
+	/**
+	 * Fired when call is finished and is about to return
+	 * 
+	 * @param totalBytes
+	 */
 	void fireEndEvent(int totalBytes);
 
+	/**
+	 * Fired for every n bytes that are sent to the server. The size of 'n' is
+	 * up to the implementation (currently 512). So typically you will receive
+	 * events for increments of 512 and a last call with the complete size when
+	 * all data is sent.
+	 * 
+	 * @param totalBytes
+	 * @param bytesSent
+	 */
 	void fireWriteEvent(int totalBytes, int bytesSent);
 
+	/**
+	 * Fire for every n bytes that are read from the server. The size of 'n' is
+	 * up to the implementation (currently 512). So typically you will receive
+	 * event for increments of 512 and a last call when all data is read from
+	 * the server.
+	 * 
+	 * @param totalBytes
+	 * @param bytesRead
+	 */
 	void fireReadEvent(int totalBytes, int bytesRead);
 
 }
