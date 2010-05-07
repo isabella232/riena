9eb40358d4d5213f834a1fdbfae1b96fc1c8f2ee
Heiko Barth <heiko.barth@compeople.de>
2010-05-07 07:06:22 +0000

    RienaLocations instance area

--- org.eclipse.riena.core/src/org/eclipse/riena/core/RienaLocations.java
+++ org.eclipse.riena.core/src/org/eclipse/riena/core/RienaLocations.java
@@ -13,9 +13,11 @@ package org.eclipse.riena.core;
 import java.io.File;
 
 import org.osgi.framework.Bundle;
+import org.osgi.service.log.LogService;
 
 import org.eclipse.core.runtime.Assert;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.equinox.log.Logger;
 import org.eclipse.osgi.service.datalocation.Location;
 
 import org.eclipse.riena.core.util.StringUtils;
@@ -27,28 +29,78 @@ public final class RienaLocations {
 
 	public static final String RIENA_NAME = "riena"; //$NON-NLS-1$
 
+	private final static Logger LOGGER = Log4r.getLogger(RienaLocations.class);
+
 	private RienaLocations() {
 		// utility class
 	}
 
 	/**
-	 * Return the data area.<br>
-	 * <b>Note:</b> This version creates a special folder within the Eclipse
-	 * install location.
+	 * Returns the area on the file system where data files shall be written to.
+	 * <p>
+	 * Uses/creates a directory within the RCP instance area, as specified by
+	 * the osgi.instance.area system property, if it is set. Otherwise, the RCP
+	 * configuration area (see system properties osgi.*configuration.*) are used
+	 * for data.
 	 * 
-	 * @param bundle
-	 * @return
+	 * @return the area on the file system where data files shall be written to.
 	 */
 	public static File getDataArea() {
-		Location installLocation = Platform.getInstallLocation();
-		Assert.isLegal(installLocation != null, "Platform.getInstallLocation() should not be null."); //$NON-NLS-1$
-		File dataArea = new File(installLocation.getURL().getFile(), RIENA_NAME);
-		if (dataArea.isDirectory()) {
-			return dataArea;
+		File dataArea;
+
+		Location instanceAreaLocation = Platform.getInstanceLocation();
+		if (instanceAreaLocation != null && instanceAreaLocation.getURL() != null) {
+			LOGGER.log(LogService.LOG_DEBUG, "Platform Instance Location: " + instanceAreaLocation.getURL().toString()); //$NON-NLS-1$
+			dataArea = new File(instanceAreaLocation.getURL().getFile(), RIENA_NAME);
+		} else {
+			throw new RuntimeException("No instance area could be found! Only set " //$NON-NLS-1$
+					+ "osgi.instance.area to @noDefault if you specify one explicitly!"); //$NON-NLS-1$
 		}
-		boolean created = dataArea.mkdirs();
-		Assert.isLegal(created, "data area is not a directory or does not exist."); //$NON-NLS-1$
-		return dataArea;
+		return existingOrNewDirectory(dataArea);
+	}
+
+	private static File existingOrNewDirectory(File location) {
+		if (location.isDirectory()) {
+			LOGGER.log(LogService.LOG_DEBUG, "Found directory at " + location.getAbsolutePath()); //$NON-NLS-1$
+			return location;
+		} else {
+			LOGGER.log(LogService.LOG_DEBUG, location.getAbsolutePath() + " is not a directory."); //$NON-NLS-1$
+			boolean canWrite = true;
+			File tempFile = location;
+			while (tempFile.getParentFile() != null && canWrite) {
+				tempFile = tempFile.getParentFile();
+				if (!tempFile.canWrite()) {
+					break;
+				}
+			}
+
+			final String fallbackLocation = System.getProperty("user.home") + "/." //$NON-NLS-1$ //$NON-NLS-2$
+					+ System.getProperty("eclipse.product"); //$NON-NLS-1$
+
+			if (!canWrite) {
+				LOGGER.log(LogService.LOG_DEBUG,
+						"No write permissions; using ${user.home}/.${eclipse.product} as fallback"); //$NON-NLS-1$
+				location = new File(fallbackLocation);
+			}
+
+			boolean created = location.mkdirs();
+
+			if (!created && canWrite) {
+				LOGGER
+						.log(LogService.LOG_DEBUG,
+								"Could not create; using ${user.home}/.${eclipse.product} as fallback"); //$NON-NLS-1$
+				location = new File(fallbackLocation);
+				created = location.mkdirs();
+				if (!created) {
+					throw new RuntimeException(
+							"Could not create fallback instance area at '" + location.getAbsolutePath() //$NON-NLS-1$
+									+ "'. Giving up."); //$NON-NLS-1$
+				}
+			}
+			Assert.isLegal(created, "Missing instance area could not be created."); //$NON-NLS-1$
+			LOGGER.log(LogService.LOG_DEBUG, "Created instance area directory at " + location.getAbsolutePath()); //$NON-NLS-1$
+			return location;
+		}
 	}
 
 	/**
@@ -64,11 +116,6 @@ public final class RienaLocations {
 		String symbolicName = bundle.getSymbolicName();
 		Assert.isLegal(StringUtils.isGiven(symbolicName), "no symbolic name for bundle."); //$NON-NLS-1$
 		File dataArea = new File(getDataArea(), symbolicName);
-		if (dataArea.isDirectory()) {
-			return dataArea;
-		}
-		boolean created = dataArea.mkdirs();
-		Assert.isLegal(created, "data area is not a directory or does not exist."); //$NON-NLS-1$
-		return dataArea;
+		return existingOrNewDirectory(dataArea);
 	}
 }
--- org.eclipse.riena.core/src/org/eclipse/riena/core/util/JavaSystemPropertiesResolver.java
+++ org.eclipse.riena.core/src/org/eclipse/riena/core/util/JavaSystemPropertiesResolver.java
@@ -10,23 +10,32 @@
  *******************************************************************************/
 package org.eclipse.riena.core.util;
 
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+import org.osgi.framework.FrameworkUtil;
+
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.core.variables.IDynamicVariable;
 import org.eclipse.core.variables.IDynamicVariableResolver;
 
+import org.eclipse.riena.core.RienaLocations;
+
 /**
- * A dynamic variable resolver for Java system properties. <br>
- * Additionally this resolver resolves currently also two <i>synthetic</i>
+ * A dynamic variable resolver for Java system properties.
+ * <p>
+ * Additionally this resolver also resolves the following <i>synthetic</i>
  * properties:
- * <ul>
- * <li>riena.host.name - the value of {@code
- * InetAddress.getLocalHost().getHostName()}</li>
- * <li>riena.host.address - the value of {@code
- * InetAddress.getLocalHost().getHostAddress()}</li>
- * </ul>
+ * <dl>
+ * <dt>riena.host.name</dt>
+ * <dd>the value of {@code InetAddress.getLocalHost().getHostName()}</dd>
+ * <dt>riena.host.address</dt>
+ * <dd>the value of {@code InetAddress.getLocalHost().getHostAddress()}</dd>
+ * <dt>riena.locations.data</dt>
+ * <dd>the Riena default directory for storing data</dd>
+ * </dl>
  * 
  * @since 2.0
  */
@@ -49,12 +58,15 @@ public class JavaSystemPropertiesResolver implements IDynamicVariableResolver {
 	/**
 	 * @param argument
 	 * @return
+	 * @throws CoreException
 	 */
-	private String lookForSyntheticProperties(final String argument) {
+	private String lookForSyntheticProperties(final String argument) throws CoreException {
 		if (argument.equals(HOST_ADDRESS)) {
 			return getHostAddress();
 		} else if (argument.equals(HOST_NAME)) {
 			return getHostName();
+		} else if (argument.equals(LOCATIONS_DATA)) {
+			return getDataLocation();
 		}
 		return null;
 	}
@@ -65,16 +77,22 @@ public class JavaSystemPropertiesResolver implements IDynamicVariableResolver {
 	private static final String RIENA = "riena."; //$NON-NLS-1$
 
 	/**
-	 * ´Synthetic´ property that retrieves the hosts ip address, i.e. {@code
+	 * "Synthetic" property that retrieves the host's IP address, i.e. {@code
 	 * InetAddress.getLocalHost().getHostAddress()}
 	 */
 	private static final String HOST_ADDRESS = RIENA + "host.address"; //$NON-NLS-1$
 
 	/**
-	 * ´Synthetic´ property that retrieves the host name, i.e. {@code
+	 * "Synthetic" property that retrieves the host name, i.e. {@code
 	 * InetAddress.getLocalHost().getHostName()}
 	 */
 	private static final String HOST_NAME = RIENA + "host.name"; //$NON-NLS-1$
+
+	/**
+	 * "Synthetic" property for the default file system directory for storing
+	 * data.
+	 */
+	private static final String LOCATIONS_DATA = RIENA + "locations.data"; //$NON-NLS-1$
 
 	private static final String UNKNOWN = "?"; //$NON-NLS-1$
 
@@ -104,4 +122,18 @@ public class JavaSystemPropertiesResolver implements IDynamicVariableResolver {
 		return hostAddress;
 	}
 
+	private String dataLocation;
+
+	private String getDataLocation() throws CoreException {
+		if (dataLocation == null) {
+			try {
+				dataLocation = RienaLocations.getDataArea().getCanonicalPath();
+			} catch (final IOException e) {
+				throw new CoreException(new Status(Status.ERROR, FrameworkUtil.getBundle(
+						JavaSystemPropertiesResolver.class).getSymbolicName(),
+						"Could not resolve Riena default data location", e)); //$NON-NLS-1$
+			}
+		}
+		return dataLocation;
+	}
 }
