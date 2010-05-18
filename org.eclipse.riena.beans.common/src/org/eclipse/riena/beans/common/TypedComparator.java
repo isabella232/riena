f275fd0178897f418a52f878669532a4b5c770f6
Frank Schepp <frank.schepp@compeople.de>
2010-05-18 13:25:28 +0000

    SCP [ruv] bug 201: TypedComparator#compare kann NullPointerException werfen

--- org.eclipse.riena.beans.common/src/org/eclipse/riena/beans/common/TypedComparator.java
+++ org.eclipse.riena.beans.common/src/org/eclipse/riena/beans/common/TypedComparator.java
@@ -30,6 +30,17 @@ public class TypedComparator<T extends Comparable<T>> implements Comparator<Obje
 	public int compare(Object o1, Object o2) {
 		// casting is necessary because we want to implement Comparator<Object>,
 		// not Comparator<T>
+		if (o1 == null) {
+			if (o2 == null) {
+				return 0;
+			} else {
+				return -1;
+			}
+		}
+		if (o2 == null) {
+			return 1;
+		}
+
 		T t1 = (T) o1;
 		T t2 = (T) o2;
 		return t1.compareTo(t2);
