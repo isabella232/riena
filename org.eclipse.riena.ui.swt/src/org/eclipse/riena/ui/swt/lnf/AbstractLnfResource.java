cea77a3ce1ec66ccf153469ed303ed6f226ba90b
Stefan Liebig <stefan.liebig@compeople.de>
2010-05-20 12:43:56 +0000

    Oop, forgot to remove dispose()

--- org.eclipse.riena.ui.swt/src/org/eclipse/riena/ui/swt/lnf/AbstractLnfResource.java
+++ org.eclipse.riena.ui.swt/src/org/eclipse/riena/ui/swt/lnf/AbstractLnfResource.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2009 compeople AG and others.
+ * Copyright (c) 2007, 2010 compeople AG and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,7 +12,6 @@ package org.eclipse.riena.ui.swt.lnf;
 
 import org.eclipse.swt.graphics.Resource;
 
-import org.eclipse.riena.core.util.Nop;
 import org.eclipse.riena.core.util.WeakRef;
 import org.eclipse.riena.ui.swt.utils.SwtUtilities;
 
@@ -22,17 +21,6 @@ import org.eclipse.riena.ui.swt.utils.SwtUtilities;
 public abstract class AbstractLnfResource<T extends Resource> implements ILnfResource<T> {
 
 	private WeakRef<T> resourceRef;
-
-	/**
-	 * @see org.eclipse.riena.ui.swt.lnf.ILnfResource#dispose()
-	 * 
-	 * @deprecated it is no longer necessary to dispose {@code Resource}s
-	 *             explicitly.
-	 */
-	@Deprecated
-	public void dispose() {
-		Nop.reason("deprecated"); //$NON-NLS-1$
-	}
 
 	/**
 	 * @see org.eclipse.riena.ui.swt.lnf.ILnfResource#getResource()
