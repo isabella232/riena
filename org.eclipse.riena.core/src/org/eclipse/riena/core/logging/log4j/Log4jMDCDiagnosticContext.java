0d7e497099919141411bcd4ffba6dcdfeeb38c2f
Stefan Liebig <stefan.liebig@compeople.de>
2010-06-07 12:06:51 +0000

    added support for variable substitution with each "push"

--- org.eclipse.riena.core/src/org/eclipse/riena/core/logging/log4j/Log4jMDCDiagnosticContext.java
+++ org.eclipse.riena.core/src/org/eclipse/riena/core/logging/log4j/Log4jMDCDiagnosticContext.java
@@ -14,12 +14,15 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import org.apache.log4j.MDC;
+
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExecutableExtension;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 
+import org.eclipse.riena.core.exception.Failure;
 import org.eclipse.riena.core.util.PropertiesUtils;
 import org.eclipse.riena.core.util.VariableManagerUtil;
 import org.eclipse.riena.internal.core.Activator;
@@ -32,32 +35,47 @@ import org.eclipse.riena.internal.core.Activator;
  * 
  * <pre>
  * &lt;extension point=&quot;org.eclipse.riena.core.log4jDiagnosticContext&quot;&gt;
- *     &lt;diagnosticContext class=&quot;org.eclipse.riena.core.logging.log4j.Log4jMDCDiagnosticContext:key=${riena.user.name}@${riena.host.name}&quot; /&gt;
+ *     &lt;diagnosticContext class=&quot;org.eclipse.riena.core.logging.log4j.Log4jMDCDiagnosticContext:user=${java.system.property:riena.user.name}@${java.system.property:riena.host.name}&quot; /&gt;
  * &lt;/extension&gt;
  * </pre>
  * 
- * The {@code Log4jMDCDiagnosticContext} is a configurable {@code
- * IExecutableExtension}. The configuration data (the data after the colon ':'
- * in the class attribute) defines the key(s) and value(s) that get pushed to
- * the {@code MDC}. If multiple keys/values should be pushed to the {@code MDC}
- * than they have to be separated by commas.
+ * The {@code Log4jMDCDiagnosticContext} is a configurable
+ * {@code IExecutableExtension}. The configuration data (the data after the
+ * colon ':' in the class attribute) defines the key(s) and value(s) that get
+ * pushed to the {@code MDC}. If multiple keys/values should be pushed to the
+ * {@code MDC} than they have to be separated by commas.
  * <p>
  * The values can contain variable substitutions which will be resolved by the
- * {@code IStringVariableManager}.
+ * {@code IStringVariableManager}.<br>
+ * If a key is preceded with an asterisk '*' the key will be evaluated each time
+ * the diagnostic context will be set; otherwise only once when this class gets
+ * instantiated.
  */
 public class Log4jMDCDiagnosticContext implements ILog4jDiagnosticContext, IExecutableExtension {
 
-	private final Map<String, String> contextInfo = new HashMap<String, String>();
+	private final Map<String, String> staticContextInfo = new HashMap<String, String>();
+	private final Map<String, String> dynamicContextInfo = new HashMap<String, String>();
 
 	public void push() {
-		for (final Entry<String, String> entry : contextInfo.entrySet()) {
-			org.apache.log4j.MDC.put(entry.getKey(), entry.getValue());
+		for (final Entry<String, String> entry : staticContextInfo.entrySet()) {
+			MDC.put(entry.getKey(), entry.getValue());
+		}
+		for (final Entry<String, String> entry : dynamicContextInfo.entrySet()) {
+			try {
+				MDC.put(entry.getKey(), VariableManagerUtil.substitute(entry.getValue()));
+			} catch (final CoreException e) {
+				throw new Log4jMDCDiagnosticContextFailure("Could not perform string substitution for: " //$NON-NLS-1$
+						+ entry.getValue(), e);
+			}
 		}
 	}
 
 	public void pop() {
-		for (final String key : contextInfo.keySet()) {
-			org.apache.log4j.MDC.remove(key);
+		for (final String key : staticContextInfo.keySet()) {
+			MDC.remove(key);
+		}
+		for (final String key : dynamicContextInfo.keySet()) {
+			MDC.remove(key);
 		}
 	}
 
@@ -65,7 +83,12 @@ public class Log4jMDCDiagnosticContext implements ILog4jDiagnosticContext, IExec
 			throws CoreException {
 		try {
 			for (final Entry<String, String> entry : PropertiesUtils.asMap(data, new String[] {}).entrySet()) {
-				contextInfo.put(entry.getKey(), VariableManagerUtil.substitute(entry.getValue()));
+				final String key = entry.getKey();
+				if (key.startsWith("*") && key.length() > 1) { //$NON-NLS-1$
+					dynamicContextInfo.put(key.substring(1), entry.getValue());
+				} else {
+					staticContextInfo.put(key, VariableManagerUtil.substitute(entry.getValue()));
+				}
 			}
 		} catch (final IllegalArgumentException e) {
 			throw configurationException("Bad configuration.", e); //$NON-NLS-1$
@@ -76,4 +99,12 @@ public class Log4jMDCDiagnosticContext implements ILog4jDiagnosticContext, IExec
 		return new CoreException(new Status(IStatus.ERROR, Activator.PLUGIN_ID, message, e));
 	}
 
+	private static class Log4jMDCDiagnosticContextFailure extends Failure {
+
+		private static final long serialVersionUID = 6386788702002693828L;
+
+		public Log4jMDCDiagnosticContextFailure(final String msg, final Throwable cause) {
+			super(msg, cause);
+		}
+	}
 }
