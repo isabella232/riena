c31fba41510713026cf14597813cdf4d7f889da4
Stefan Liebig <stefan.liebig@compeople.de>
2010-06-07 12:04:47 +0000

    added andStart() without bundle context parameter

--- org.eclipse.riena.core/src/org/eclipse/riena/core/wire/WirePuller.java
+++ org.eclipse.riena.core/src/org/eclipse/riena/core/wire/WirePuller.java
@@ -15,13 +15,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleEvent;
 import org.osgi.framework.BundleListener;
+import org.osgi.framework.FrameworkUtil;
 import org.osgi.framework.SynchronousBundleListener;
+import org.osgi.service.log.LogService;
 
 import org.eclipse.core.runtime.Assert;
+import org.eclipse.equinox.log.Logger;
 
+import org.eclipse.riena.core.Log4r;
 import org.eclipse.riena.core.injector.IStoppable;
 import org.eclipse.riena.core.injector.extension.ExtensionInjector;
 import org.eclipse.riena.core.injector.service.ServiceInjector;
@@ -45,6 +50,8 @@ public class WirePuller implements IStoppable {
 	// Only for unit testing of classes using accessors
 	private static Map<Class<?>, Class<? extends IWiring>> wiringMocks;
 
+	private static final Logger LOGGER = Log4r.getLogger(WirePuller.class);
+
 	/**
 	 * @param bean
 	 */
@@ -54,10 +61,30 @@ public class WirePuller implements IStoppable {
 	}
 
 	/**
+	 * Start the wiring.<br>
+	 * This method tries to retrieve the required {@code BundleContext} from the
+	 * {@code bean} that should be wired. This requires that the OSGi runtime is
+	 * up - which might not the case in unit tests. However, in such a case no
+	 * wiring will be done and this method returns {@code null}.
+	 * 
+	 * @return the {@code WirePuller} or null if wiring was no possible
+	 */
+	public synchronized WirePuller andStart() {
+		final Bundle bundle = FrameworkUtil.getBundle(bean.getClass());
+		if (bundle != null) {
+			return andStart(bundle.getBundleContext());
+		}
+		LOGGER.log(LogService.LOG_WARNING, "Could not wire bean '" + bean.getClass() //$NON-NLS-1$
+				+ "' because there is no bundle context."); //$NON-NLS-1$
+		return null;
+	}
+
+	/**
 	 * Start the wiring.
 	 * 
 	 * @param context
-	 * @return the bean
+	 *            the bundle context
+	 * @return the {@code WirePuller}
 	 */
 	public synchronized WirePuller andStart(final BundleContext context) {
 		Assert.isLegal(context != null, "context must be given."); //$NON-NLS-1$
@@ -79,23 +106,23 @@ public class WirePuller implements IStoppable {
 		}
 		context.removeBundleListener(bundleStoppingListener);
 		if (wirings != null) {
-			for (IWiring wiring : wirings) {
+			for (final IWiring wiring : wirings) {
 				wiring.unwire(bean, context);
 			}
 		}
 		if (injections != null) {
-			for (IStoppable stoppable : Iter.ableReverse(injections)) {
+			for (final IStoppable stoppable : Iter.ableReverse(injections)) {
 				stoppable.stop();
 			}
 		}
 		state = State.STOPPED;
 	}
 
-	private void wire(Class<?> beanClass) {
+	private void wire(final Class<?> beanClass) {
 		if (beanClass == null || beanClass == Object.class) {
 			return;
 		}
-		IWiring wiring = getWiring(getWiringClass(beanClass));
+		final IWiring wiring = getWiring(getWiringClass(beanClass));
 		add(wiring);
 		wire(beanClass.getSuperclass());
 		if (wiring != null) {
@@ -104,8 +131,8 @@ public class WirePuller implements IStoppable {
 		injectIntoAnnotatedMethods(bean, beanClass);
 	}
 
-	private void injectIntoAnnotatedMethods(Object bean, Class<?> beanClass) {
-		for (Method method : beanClass.getDeclaredMethods()) {
+	private void injectIntoAnnotatedMethods(final Object bean, final Class<?> beanClass) {
+		for (final Method method : beanClass.getDeclaredMethods()) {
 			if (method.isAnnotationPresent(InjectService.class)) {
 				injectServiceInto(bean, method);
 			} else if (method.isAnnotationPresent(InjectExtension.class)) {
@@ -114,15 +141,15 @@ public class WirePuller implements IStoppable {
 		}
 	}
 
-	private void injectServiceInto(Object bean, Method method) {
-		ServiceInjectorBuilder bob = new ServiceInjectorBuilder(bean, method);
-		ServiceInjector injector = bob.build();
+	private void injectServiceInto(final Object bean, final Method method) {
+		final ServiceInjectorBuilder bob = new ServiceInjectorBuilder(bean, method);
+		final ServiceInjector injector = bob.build();
 		add(injector.andStart(context));
 	}
 
-	private void injectExtensionInto(Object bean, Method method) {
-		ExtensionInjectorBuilder bob = new ExtensionInjectorBuilder(bean, method);
-		ExtensionInjector injector = bob.build();
+	private void injectExtensionInto(final Object bean, final Method method) {
+		final ExtensionInjectorBuilder bob = new ExtensionInjectorBuilder(bean, method);
+		final ExtensionInjector injector = bob.build();
 		add(injector.andStart(context));
 	}
 
@@ -131,7 +158,7 @@ public class WirePuller implements IStoppable {
 	 * 
 	 * @param wiring
 	 */
-	private void add(IWiring wiring) {
+	private void add(final IWiring wiring) {
 		if (wiring == null) {
 			return;
 		}
@@ -146,7 +173,7 @@ public class WirePuller implements IStoppable {
 	 * 
 	 * @param stoppable
 	 */
-	private void add(IStoppable stoppable) {
+	private void add(final IStoppable stoppable) {
 		if (injections == null) {
 			injections = new ArrayList<IStoppable>();
 		}
@@ -157,15 +184,15 @@ public class WirePuller implements IStoppable {
 	 * @param context
 	 * @return
 	 */
-	private IWiring getWiring(Class<? extends IWiring> wiringClass) {
+	private IWiring getWiring(final Class<? extends IWiring> wiringClass) {
 		if (wiringClass == null) {
 			return null;
 		}
 		try {
 			return wiringClass.newInstance();
-		} catch (InstantiationException e) {
+		} catch (final InstantiationException e) {
 			throw new IllegalStateException("Could not create instance of wiring " + wiringClass, e); //$NON-NLS-1$
-		} catch (IllegalAccessException e) {
+		} catch (final IllegalAccessException e) {
 			throw new IllegalStateException("Could not create instance of wiring " + wiringClass, e); //$NON-NLS-1$
 		}
 	}
@@ -173,12 +200,12 @@ public class WirePuller implements IStoppable {
 	/**
 	 * @return
 	 */
-	private Class<? extends IWiring> getWiringClass(Class<?> beanClass) {
+	private Class<? extends IWiring> getWiringClass(final Class<?> beanClass) {
 		// If mocks are defined, we use them instead of the regular mechanism.
 		if (wiringMocks != null) {
 			return wiringMocks.get(beanClass);
 		}
-		WireWith wiringAnnotation = beanClass.getAnnotation(WireWith.class);
+		final WireWith wiringAnnotation = beanClass.getAnnotation(WireWith.class);
 		// Does the bean have a wiring annotation? Yes, use this class for wiring.
 		if (wiringAnnotation != null) {
 			return wiringAnnotation.value();
@@ -191,7 +218,7 @@ public class WirePuller implements IStoppable {
 	 */
 	private class BundleStoppingListener implements SynchronousBundleListener {
 		@IgnoreFindBugs(value = "IS2_INCONSISTENT_SYNC", justification = "Field context of WirePuller is written only before this listener is added, field bundle of BundleContext is immutable.")
-		public void bundleChanged(BundleEvent event) {
+		public void bundleChanged(final BundleEvent event) {
 			if (event.getBundle() != context.getBundle()) {
 				return;
 			}
@@ -209,7 +236,7 @@ public class WirePuller implements IStoppable {
 	 * 
 	 * @param wiringMocks
 	 */
-	public static void injectWiringMocks(Map<Class<?>, Class<? extends IWiring>> wiringMocks) {
+	public static void injectWiringMocks(final Map<Class<?>, Class<? extends IWiring>> wiringMocks) {
 		WirePuller.wiringMocks = wiringMocks;
 	}
 
