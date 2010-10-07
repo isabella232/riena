fd8a9cc2198b32c621bf285811d4ef46bf1eeedb
Heiko Barth <heiko.barth@compeople.de>
2010-10-07 07:37:06 +0000

    Testing

--- org.eclipse.riena.tests/src/org/eclipse/riena/navigation/ui/swt/views/SubModuleViewTest.java
+++ org.eclipse.riena.tests/src/org/eclipse/riena/navigation/ui/swt/views/SubModuleViewTest.java
@@ -15,6 +15,7 @@ import java.util.List;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Cursor;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Shell;
 
@@ -23,6 +24,7 @@ import org.eclipse.riena.internal.core.test.RienaTestCase;
 import org.eclipse.riena.internal.core.test.collect.UITestCase;
 import org.eclipse.riena.navigation.IApplicationNode;
 import org.eclipse.riena.navigation.IModuleNode;
+import org.eclipse.riena.navigation.ISubModuleNode;
 import org.eclipse.riena.navigation.NavigationNodeId;
 import org.eclipse.riena.navigation.model.ApplicationNode;
 import org.eclipse.riena.navigation.model.ModuleGroupNode;
@@ -30,6 +32,7 @@ import org.eclipse.riena.navigation.model.ModuleNode;
 import org.eclipse.riena.navigation.model.SubApplicationNode;
 import org.eclipse.riena.navigation.model.SubModuleNode;
 import org.eclipse.riena.navigation.ui.controllers.ModuleController;
+import org.eclipse.riena.navigation.ui.controllers.SubModuleController;
 
 /**
  * Tests for the SubModuleNodeView.
@@ -43,6 +46,8 @@ public class SubModuleViewTest extends RienaTestCase {
 	private SubModuleNode anotherNodeSameView;
 	private List<SubModuleNode> nodesBoundToView;
 	private ApplicationNode appNode;
+	private IModuleNode moduleNode;
+	private ArrayList<SubModuleNode> nodesBoundToSharedView;
 
 	@Override
 	protected void setUp() throws Exception {
@@ -55,19 +60,20 @@ public class SubModuleViewTest extends RienaTestCase {
 		appNode.addChild(subAppNode);
 		final ModuleGroupNode mgNode = new ModuleGroupNode(null);
 		subAppNode.addChild(mgNode);
-		final IModuleNode parent = new ModuleNode(null, "TestModuleLabel");
-		mgNode.addChild(parent);
+		moduleNode = new ModuleNode(null, "TestModuleLabel");
+		mgNode.addChild(moduleNode);
 
 		anotherNode = new SubModuleNode(new NavigationNodeId("testId2", "2"), "TestSubModuleLabel2");
-		parent.addChild(anotherNode);
+		moduleNode.addChild(anotherNode);
 		anotherNodeSameView = new SubModuleNode(new NavigationNodeId("testId", "1"), "TestSubModuleLabel3");
-		parent.addChild(anotherNodeSameView);
+		moduleNode.addChild(anotherNodeSameView);
 		nodesBoundToView = new ArrayList<SubModuleNode>();
+		nodesBoundToSharedView = new ArrayList<SubModuleNode>();
 
 		subModuleNodeView = new TestView();
 		node = new SubModuleNode(new NavigationNodeId("testId", "0"), "TestSubModuleLabel");
-		parent.setNavigationNodeController(new ModuleController(parent));
-		parent.addChild(node);
+		moduleNode.setNavigationNodeController(new ModuleController(moduleNode));
+		moduleNode.addChild(node);
 		subModuleNodeView.createPartControl(new Shell());
 		node.activate();
 	}
@@ -98,6 +104,29 @@ public class SubModuleViewTest extends RienaTestCase {
 		assertEquals(node, subModuleNodeView.getController().getNavigationNode());
 	}
 
+	public void testShared() {
+		subModuleNodeView.dispose();
+		final SubModuleNode s1 = new SubModuleNode(new NavigationNodeId("s", "1"));
+		final SubModuleNode s2 = new SubModuleNode(new NavigationNodeId("s", "2"));
+		moduleNode.addChild(s1);
+		moduleNode.addChild(s2);
+		final TestSharedView subModuleNodeView = new TestSharedView();
+		subModuleNodeView.s1 = s1;
+		subModuleNodeView.s2 = s2;
+		subModuleNodeView.createPartControl(new Shell());
+		assertNotNull(s1.getNavigationNodeController());
+		s1.activate();
+		final SubModuleController s1c = (SubModuleController) s1.getNavigationNodeController();
+		assertNotNull(s1c.getRidget("button"));
+		assertEquals(nodesBoundToSharedView.get(0), s1);
+		s2.activate();
+		assertEquals(nodesBoundToSharedView.get(2), s2);
+		s1.dispose();
+		assertFalse(subModuleNodeView.unbindActiveCalled);
+		s2.dispose();
+		assertTrue(subModuleNodeView.unbindActiveCalled);
+	}
+
 	public void testBindOnActivate() throws Exception {
 		nodesBoundToView.clear();
 
@@ -112,6 +141,81 @@ public class SubModuleViewTest extends RienaTestCase {
 		node.activate();
 		assertEquals(1, nodesBoundToView.size());
 		assertSame(node, nodesBoundToView.get(0));
+	}
+
+	private final class TestSharedView extends SubModuleView {
+
+		SubModuleNode s1;
+		SubModuleNode s2;
+		Button button;
+
+		boolean unbindActiveCalled;
+
+		@Override
+		public void bind(final SubModuleNode node) {
+			if (node.getNavigationNodeController() == null) {
+				node.setNavigationNodeController(createController(node));
+			}
+			ReflectionUtils.setHidden(this, "currentController", node.getNavigationNodeController());
+			nodesBoundToSharedView.add(node);
+		}
+
+		@Override
+		public SubModuleNode getNavigationNode() {
+			return s1.isActivated() ? s1 : s2.isActivated() ? s2 : s1;
+		}
+
+		@Override
+		protected void basicCreatePartControl(final Composite parent) {
+			button = new Button(parent, SWT.NONE);
+			addUIControl(button, "button");
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see org.eclipse.riena.navigation.ui.swt.views.SubModuleView#
+		 * unbindActiveController()
+		 */
+		@Override
+		protected void unbindActiveController() {
+			unbindActiveCalled = true;
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see
+		 * org.eclipse.riena.navigation.ui.swt.views.SubModuleView#createController
+		 * (org.eclipse.riena.navigation.ISubModuleNode)
+		 */
+		@Override
+		protected SubModuleController createController(final ISubModuleNode node) {
+			return new SubModuleController(node);
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see
+		 * org.eclipse.riena.navigation.ui.swt.views.SubModuleView#getSecondaryId
+		 * ()
+		 */
+		@Override
+		protected String getSecondaryId() {
+			return SubModuleView.SHARED_ID;
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see
+		 * org.eclipse.riena.navigation.ui.swt.views.SubModuleView#getAppNode()
+		 */
+		@Override
+		protected IApplicationNode getAppNode() {
+			return appNode;
+		}
 	}
 
 	// helping classes
