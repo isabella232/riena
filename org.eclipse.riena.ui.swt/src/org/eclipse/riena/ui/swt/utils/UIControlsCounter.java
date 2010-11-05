6140649347b5446c76f3684b5a402c1fef100f82
Stefan Liebig <stefan.liebig@compeople.de>
2010-11-05 07:09:29 +0000

    refactored: keeps track of currently live widgets and counts total number of widgets + can be de/activated with the system property "riena.control.stats"

--- org.eclipse.riena.ui.swt/src/org/eclipse/riena/ui/swt/utils/UIControlsCounter.java
+++ org.eclipse.riena.ui.swt/src/org/eclipse/riena/ui/swt/utils/UIControlsCounter.java
@@ -10,19 +10,24 @@
  *******************************************************************************/
 package org.eclipse.riena.ui.swt.utils;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Widget;
 
 import org.eclipse.riena.core.singleton.SessionSingletonProvider;
 import org.eclipse.riena.core.singleton.SingletonProvider;
-import org.eclipse.riena.internal.ui.swt.console.UIControlsStatisticConsole;
+import org.eclipse.riena.core.util.ObjectCounter;
+import org.eclipse.riena.internal.ui.swt.console.UIControlsStatisticCommandProvider;
 
 /**
  * Statistic Counter of {@link Control}s created by {@link UIControlsFactory}.
- * Used by {@link UIControlsStatisticConsole} to get statistics.
+ * Used by {@link UIControlsStatisticCommandProvider} to get statistics.
  * 
  * @since 3.0
  */
@@ -35,12 +40,28 @@ public class UIControlsCounter {
 	private static final SingletonProvider<UIControlsCounter> UICC = new SessionSingletonProvider<UIControlsCounter>(
 			UIControlsCounter.class);
 
-	private final Map<Class<?>, Integer> counter = new HashMap<Class<?>, Integer>();
+	private final ObjectCounter<Class<?>> live = new ObjectCounter<Class<?>>();
+	private final ObjectCounter<Class<?>> total = new ObjectCounter<Class<?>>();
+
+	private final DisposeListener disposeListener = new LiveDisposeListener();
+
+	public static final String CONTROL_STATS_PROPERTY_NAME = "riena.control.stats"; //$NON-NLS-1$
+	private static final boolean CONTROL_STATS_REQUESTED = Boolean.getBoolean(CONTROL_STATS_PROPERTY_NAME);
+
+	/**
+	 * Asks whether control counting statistics are requested or not.
+	 * 
+	 * @return {@code true} for statistics should be activated; otherwise
+	 *         {@code false}
+	 */
+	public static boolean isRequested() {
+		return CONTROL_STATS_REQUESTED;
+	}
 
 	/**
 	 * @return - the Singleton instance of {@link UIControlsCounter}
 	 */
-	public static UIControlsCounter getUIControlsCounterSingleton() {
+	public static UIControlsCounter getInstance() {
 		return UICC.getInstance();
 	}
 
@@ -51,12 +72,21 @@ public class UIControlsCounter {
 	 *            - the control to register
 	 */
 	public void registerConstruction(final Object control) {
-		final Class<?> clazz = control.getClass();
-		if (!counter.containsKey(clazz)) {
-			counter.put(clazz, 1);
-		} else {
-			counter.put(clazz, counter.get(clazz) + 1);
+		if (isRequested()) {
+			total.incrementAndGetCount(control.getClass());
+			live.incrementAndGetCount(control.getClass());
+			if (control instanceof Widget) {
+				((Widget) control).addDisposeListener(disposeListener);
+			}
+		}
+	}
 
+	/**
+	 *
+	 */
+	private final class LiveDisposeListener implements DisposeListener {
+		public void widgetDisposed(final DisposeEvent e) {
+			live.decrementAndGetCount(e.widget.getClass());
 		}
 	}
 
@@ -71,12 +101,12 @@ public class UIControlsCounter {
 		strb.append(HEADER);
 		appendNTimes(STAR, NOS, strb);
 		newLine(strb);
-		final Set<Class<?>> classes = counter.keySet();
-		for (final Class<?> clazz : classes) {
-			strb.append("Control Type: "); //$NON-NLS-1$
-			strb.append(clazz.getName());
-			strb.append("\tInstance Count: "); //$NON-NLS-1$
-			strb.append(counter.get(clazz));
+		strb.append(" Live" + DELIMITER); //$NON-NLS-1$
+		strb.append("Total" + DELIMITER); //$NON-NLS-1$
+		strb.append("Type"); //$NON-NLS-1$
+		newLine(strb);
+		for (final String line : getSortedLines(live, total)) {
+			strb.append(line);
 			newLine(strb);
 		}
 		appendNTimes(STAR, 2 * NOS + HEADER.length(), strb);
@@ -84,6 +114,24 @@ public class UIControlsCounter {
 	}
 
 	///// Helping Methods
+
+	private static final String DELIMITER = "\t"; //$NON-NLS-1$
+	private static final String LINE_FORMAT = "% 5d" + DELIMITER + "% 5d" + DELIMITER + "%s"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+
+	private static List<String> getSortedLines(final ObjectCounter<Class<?>> live, final ObjectCounter<Class<?>> total) {
+		final List<String> result = new ArrayList<String>();
+		for (final Class<?> clazz : live) {
+			result.add(String.format(LINE_FORMAT, live.getCount(clazz), total.getCount(clazz), clazz.getName()));
+		}
+		Collections.sort(result, new Comparator<String>() {
+			public int compare(final String o1, final String o2) {
+				final Integer i1 = Integer.valueOf(o1.split(DELIMITER)[0].trim());
+				final Integer i2 = Integer.valueOf(o2.split(DELIMITER)[0].trim());
+				return i2.compareTo(i1);
+			}
+		});
+		return result;
+	}
 
 	private static void newLine(final StringBuilder strb) {
 		strb.append("\n"); //$NON-NLS-1$
