6c22add246939fb6cdeb34e942367d731510e898
Sabine Achilles <sabine.achilles@compeople.de>
2011-01-05 11:22:34 +0000

    cleanup

--- org.eclipse.riena.navigation/src/org/eclipse/riena/navigation/model/SimpleNavigationNodeAdapter.java
+++ org.eclipse.riena.navigation/src/org/eclipse/riena/navigation/model/SimpleNavigationNodeAdapter.java
@@ -26,228 +26,76 @@ import org.eclipse.riena.ui.filter.IUIFilter;
  */
 public class SimpleNavigationNodeAdapter implements ISimpleNavigationNodeListener {
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.riena.navigation.model.ISimpleNavigationNodeListener#activated
-	 * (org.eclipse.riena.navigation.INavigationNode<?>)
-	 */
 	public void activated(final INavigationNode<?> source) {
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @seeorg.eclipse.riena.navigation.model.ISimpleNavigationNodeListener#
-	 * afterActivated(org.eclipse.riena.navigation.INavigationNode<?>)
-	 */
 	public void afterActivated(final INavigationNode<?> source) {
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @seeorg.eclipse.riena.navigation.model.ISimpleNavigationNodeListener#
-	 * afterDeactivated(org.eclipse.riena.navigation.INavigationNode<?>)
-	 */
 	public void afterDeactivated(final INavigationNode<?> source) {
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @seeorg.eclipse.riena.navigation.model.ISimpleNavigationNodeListener#
-	 * afterDisposed(org.eclipse.riena.navigation.INavigationNode<?>)
-	 */
 	public void afterDisposed(final INavigationNode<?> source) {
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @seeorg.eclipse.riena.navigation.model.ISimpleNavigationNodeListener#
-	 * beforeActivated(org.eclipse.riena.navigation.INavigationNode<?>)
-	 */
 	public void beforeActivated(final INavigationNode<?> source) {
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @seeorg.eclipse.riena.navigation.model.ISimpleNavigationNodeListener#
-	 * beforeDeactivated(org.eclipse.riena.navigation.INavigationNode<?>)
-	 */
 	public void beforeDeactivated(final INavigationNode<?> source) {
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @seeorg.eclipse.riena.navigation.model.ISimpleNavigationNodeListener#
-	 * beforeDisposed(org.eclipse.riena.navigation.INavigationNode<?>)
-	 */
 	public void beforeDisposed(final INavigationNode<?> source) {
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.riena.navigation.model.ISimpleNavigationNodeListener#block
-	 * (org.eclipse.riena.navigation.INavigationNode<?>, boolean)
-	 */
 	public void block(final INavigationNode<?> source, final boolean block) {
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.riena.navigation.model.ISimpleNavigationNodeListener#childAdded
-	 * (org.eclipse.riena.navigation.INavigationNode<?>,
-	 * org.eclipse.riena.navigation.INavigationNode<?>)
-	 */
 	public void childAdded(final INavigationNode<?> source, final INavigationNode<?> childAdded) {
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.riena.navigation.model.ISimpleNavigationNodeListener#childRemoved
-	 * (org.eclipse.riena.navigation.INavigationNode<?>,
-	 * org.eclipse.riena.navigation.INavigationNode<?>)
-	 */
 	public void childRemoved(final INavigationNode<?> source, final INavigationNode<?> childRemoved) {
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.riena.navigation.model.ISimpleNavigationNodeListener#deactivated
-	 * (org.eclipse.riena.navigation.INavigationNode<?>)
-	 */
 	public void deactivated(final INavigationNode<?> source) {
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.riena.navigation.model.ISimpleNavigationNodeListener#disposed
-	 * (org.eclipse.riena.navigation.INavigationNode<?>)
-	 */
 	public void disposed(final INavigationNode<?> source) {
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @seeorg.eclipse.riena.navigation.model.ISimpleNavigationNodeListener#
-	 * expandedChanged(org.eclipse.riena.navigation.INavigationNode<?>)
-	 */
 	public void expandedChanged(final INavigationNode<?> source) {
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.riena.navigation.model.ISimpleNavigationNodeListener#iconChanged
-	 * (org.eclipse.riena.navigation.INavigationNode<?>)
-	 */
 	public void iconChanged(final INavigationNode<?> source) {
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.riena.navigation.model.ISimpleNavigationNodeListener#labelChanged
-	 * (org.eclipse.riena.navigation.INavigationNode<?>)
-	 */
 	public void labelChanged(final INavigationNode<?> source) {
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @seeorg.eclipse.riena.navigation.model.ISimpleNavigationNodeListener#
-	 * markersChanged(org.eclipse.riena.navigation.INavigationNode<?>)
-	 */
 	public void markerChanged(final INavigationNode<?> source, final IMarker marker) {
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @seeorg.eclipse.riena.navigation.model.ISimpleNavigationNodeListener#
-	 * parentChanged(org.eclipse.riena.navigation.INavigationNode<?>)
-	 */
 	public void parentChanged(final INavigationNode<?> source) {
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @seeorg.eclipse.riena.navigation.model.ISimpleNavigationNodeListener#
-	 * presentationChanged(org.eclipse.riena.navigation.INavigationNode<?>)
-	 */
 	public void presentationChanged(final INavigationNode<?> source) {
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @seeorg.eclipse.riena.navigation.model.ISimpleNavigationNodeListener#
-	 * selectedChanged(org.eclipse.riena.navigation.INavigationNode<?>)
-	 */
 	public void selectedChanged(final INavigationNode<?> source) {
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.riena.navigation.model.ISimpleNavigationNodeListener#stateChanged
-	 * (org.eclipse.riena.navigation.INavigationNode<?>,
-	 * org.eclipse.riena.navigation.INavigationNode<?>.State,
-	 * org.eclipse.riena.navigation.INavigationNode<?>.State)
-	 */
 	public void stateChanged(final INavigationNode<?> source, final State oldState, final State newState) {
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.riena.navigation.ISimpleNavigationNodeListener#filterChanged
-	 * (org.eclipse.riena.navigation.INavigationNode)
-	 */
 	public void filterAdded(final INavigationNode<?> source, final IUIFilter filter) {
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.riena.navigation.ISimpleNavigationNodeListener#filterChanged
-	 * (org.eclipse.riena.navigation.INavigationNode)
-	 */
 	public void filterRemoved(final INavigationNode<?> source, final IUIFilter filter) {
 	}
 
-	/**
-	 * {@inheritDoc}
-	 */
 	public void prepared(final INavigationNode<?> source) {
 	}
 
 	/**
-	 * {@inheritDoc}
-	 * 
 	 * @since 3.0
 	 */
 	public void nodeIdChange(final INavigationNode<?> source, final NavigationNodeId oldId, final NavigationNodeId newId) {
