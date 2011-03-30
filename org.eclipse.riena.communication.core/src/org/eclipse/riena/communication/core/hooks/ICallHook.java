a2a11a705361284b3c7e8d4f1e6f2e8a3b0043c8
Stefan Liebig <stefan.liebig@compeople.de>
2011-03-30 13:43:11 +0000

    javadoc - emphasizing that an exception will terminate the service call

--- org.eclipse.riena.communication.core/src/org/eclipse/riena/communication/core/hooks/ICallHook.java
+++ org.eclipse.riena.communication.core/src/org/eclipse/riena/communication/core/hooks/ICallHook.java
@@ -11,24 +11,26 @@
 package org.eclipse.riena.communication.core.hooks;
 
 /**
- * {@code ICallHook} is an interface that needs to be implemented by a component
+ * {@link ICallHook} is an interface that needs to be implemented by a component
  * that wants to plug into the remote service call process. A component needs to
  * register as OSGi Service to activate itself as such an hook i.e.
  * {@code context.registerService(ICallHook.class.getName(), new YouCallHook(),null);}
  * <p>
  * Then the beforeCall method is called (on the client) before EACH and EVERY
- * remote service call together with a {@code CallContext} instance. The
+ * remote service call together with a {@link CallContext} instance. The
  * afterCall method is called after the remote service calls return and before
- * control is returned to the application code with the same {@code CallContext}
+ * control is returned to the application code with the same {@link CallContext}
  * instance.
  * <p>
- * A new {@code CallContext} instance is created for every remote service call.
- * (they are never reused)
+ * A new {@link CallContext} instance is created for every remote service call -
+ * they are never reused.
  */
 public interface ICallHook {
 
 	/**
 	 * Is called before the remote service call.
+	 * <p>
+	 * <b>Note:</b>Throwing an exception will terminate the service call!
 	 * 
 	 * @param context
 	 *            CallContext instance that contains meta-information about the
@@ -39,6 +41,8 @@ public interface ICallHook {
 
 	/**
 	 * Is called after the remote service call.
+	 * <p>
+	 * <b>Note:</b>Throwing an exception will terminate the service call!
 	 * 
 	 * @param context
 	 *            CallContext instance (same as beforeCall) that contains
--- org.eclipse.riena.communication.core/src/org/eclipse/riena/communication/core/hooks/IServiceHook.java
+++ org.eclipse.riena.communication.core/src/org/eclipse/riena/communication/core/hooks/IServiceHook.java
@@ -11,30 +11,32 @@
 package org.eclipse.riena.communication.core.hooks;
 
 /**
- * IServiceHook is an interface that needs to be implemented by a component that
- * wants to plug into the remote service call process. A component needs to
- * register as OSGi Service to activate itself as such an hook i.e.
- * context.registerService(IServiceHook.class.getName(), new
- * YouServiceHook(),null);
- * 
+ * {@link IServiceHook} is an interface that needs to be implemented by a
+ * component that wants to plug into the remote service call process. A
+ * component needs to register as OSGi Service to activate itself as such an
+ * hook i.e. {@code context.registerService(IServiceHook.class.getName(), new
+ * YouServiceHook(),null);}
+ * <p>
  * Then the beforeService method is called (on the server) before EACH and EVERY
  * remote service is forwarded to the OSGi Service together with a
- * ServiceContext instance. The afterService method is called after the remote
- * service invocation is finished and before the call is returned to the server
- * with the same ServiceContext instance.
- * 
- * A new ServiceContext instance is created for every remote service call. (they
- * are never reused)
+ * {@link ServiceContext} instance. The afterService method is called after the
+ * remote service invocation is finished and before the call is returned to the
+ * server with the same {@link ServiceContext} instance.
+ * <p>
+ * A new {@link ServiceContext} instance is created for every remote service
+ * call - they are never reused.
  */
 public interface IServiceHook {
 
 	/**
 	 * Is called before the invocation on the server of the remote service
 	 * component.
+	 * <p>
+	 * <b>Note:</b>Throwing an exception will terminate the service call!
 	 * 
 	 * @param context
-	 *            ServiceContext instance that contains metainformation about
-	 *            the remote service call and a properties hashmap that can be
+	 *            ServiceContext instance that contains meta-information about
+	 *            the remote service call and a properties hash-map that can be
 	 *            used to store information for the afterService method.
 	 */
 	void beforeService(ServiceContext context);
@@ -44,8 +46,8 @@ public interface IServiceHook {
 	 * component.
 	 * 
 	 * @param context
-	 *            ServiceContext instance that contains metainformation about
-	 *            the remote service call and a properties hashmap that can be
+	 *            ServiceContext instance that contains meta-information about
+	 *            the remote service call and a properties hash-map that can be
 	 *            used to pass information from the beforeService method to the
 	 *            afterService method.
 	 */
