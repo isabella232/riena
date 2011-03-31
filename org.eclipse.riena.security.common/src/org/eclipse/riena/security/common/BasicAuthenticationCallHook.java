ace9da52fa71c72c5eb596b41b563c5ecd8097b8
Stefan Liebig <stefan.liebig@compeople.de>
2011-03-31 13:59:44 +0000

    Bug 291734 - BasicAuthenticationCallHook uses sun.misc.BASE64Encoder

--- org.eclipse.riena.security.common/src/org/eclipse/riena/security/common/BasicAuthenticationCallHook.java
+++ org.eclipse.riena.security.common/src/org/eclipse/riena/security/common/BasicAuthenticationCallHook.java
@@ -18,9 +18,8 @@ import javax.security.auth.Subject;
 import org.eclipse.riena.communication.core.hooks.CallContext;
 import org.eclipse.riena.communication.core.hooks.ICallHook;
 import org.eclipse.riena.core.injector.Inject;
+import org.eclipse.riena.core.util.Base64;
 import org.eclipse.riena.internal.security.common.Activator;
-
-import sun.misc.BASE64Encoder;
 
 /**
  * This optional hook implements authentication by
@@ -48,24 +47,10 @@ public class BasicAuthenticationCallHook implements ICallHook {
 		this.subjectHolder = null;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.riena.communication.core.hooks.ICallHook#afterCall(org.eclipse
-	 * .riena.communication.core.hooks.CallContext)
-	 */
 	public void afterCall(final CallContext context) {
 		// no action necessary
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.riena.communication.core.hooks.ICallHook#beforeCall(org.eclipse
-	 * .riena.communication.core.hooks.CallContext)
-	 */
 	public void beforeCall(final CallContext context) {
 		if (subjectHolder == null) {
 			return;
@@ -74,22 +59,16 @@ public class BasicAuthenticationCallHook implements ICallHook {
 		if (subject == null) {
 			return;
 		}
-		Object psw = null;
+		Object credentials = null;
 		final Iterator<Object> iterator = subject.getPrivateCredentials().iterator();
 		if (iterator.hasNext()) {
-			psw = iterator.next();
+			credentials = iterator.next();
 		}
-		String password;
-		if (psw != null && psw instanceof String) {
-			password = (String) psw;
-		} else {
-			password = ""; //$NON-NLS-1$
-		}
+		final String password = credentials instanceof String ? (String) credentials : ""; //$NON-NLS-1$
 		for (final Principal principal : subject.getPrincipals()) {
 			final String useridPlusPassword = principal.getName() + ":" + password; //$NON-NLS-1$
-			final String authorizationInBase64 = new BASE64Encoder().encode(useridPlusPassword.getBytes());
+			final String authorizationInBase64 = new String(Base64.encode(useridPlusPassword.getBytes()));
 			context.getMessageContext().addRequestHeader("Authorization", "Basic " + authorizationInBase64); //$NON-NLS-1$//$NON-NLS-2$
 		}
 	}
-
 }
