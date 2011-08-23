67dd4d3fb6fbf4d6ea71e0fcdac7a847646ded5a
Stefan Liebig <stefan.liebig@compeople.de>
2011-08-23 11:01:01 +0000

    https://bugs.eclipse.org/bugs/show_bug.cgi?id=320836 - Hessian Serializer Factory extension require class to implement internal class

---
+++ org.eclipse.riena.tests/src/org/eclipse/riena/internal/communication/factory/hessian/serializer/RienaSerializerFactoryTest.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2007, 2011 compeople AG and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    compeople AG - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.riena.internal.communication.factory.hessian.serializer;
+
+import org.eclipse.riena.internal.communication.factory.hessian.serializer.RienaSerializerFactory;
+import org.eclipse.riena.internal.core.test.RienaTestCase;
+import org.eclipse.riena.internal.core.test.collect.NonUITestCase;
+
+/**
+ * Test the {@code RienaSerializerFactory}.
+ */
+@NonUITestCase
+public class RienaSerializerFactoryTest extends RienaTestCase {
+
+	public void testCreationThatMightFailWhenTheHessianImplementationChanges() {
+		assertNotNull(new RienaSerializerFactory());
+	}
+
+}
