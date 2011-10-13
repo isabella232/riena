2f6e8487f71745fabe62c9941e0f96095583dccc
Stefan Liebig <stefan.liebig@compeople.de>
2011-10-13 07:12:12 +0000

    Example for Bug 278905 - please provide API or configuration at start/quit for app window positioning

---
+++ org.eclipse.riena.example.client/src/org/eclipse/riena/example/client/views/ExampleWindowNavigator.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2007, 2011 compeople AG and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    compeople AG - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.riena.example.client.views;
+
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.widgets.Monitor;
+import org.eclipse.swt.widgets.Shell;
+
+import org.eclipse.riena.internal.ui.swt.utils.ShellHelper;
+import org.eclipse.riena.navigation.ui.swt.views.IWindowNavigator;
+
+public class ExampleWindowNavigator implements IWindowNavigator {
+
+	/**
+	 * @see org.eclipse.riena.navigation.ui.swt.views.IWindowNavigator#beforeOpen(org.eclipse.swt.widgets.Shell)
+	 */
+	public void beforeOpen(final Shell shell) {
+		final Monitor monitor = ShellHelper.getClosestMonitor(shell);
+		final Rectangle screenBound = monitor.getBounds();
+		final int leftMargin = screenBound.x + (screenBound.width / 4);
+		final int topMargin = screenBound.y;
+		shell.setLocation(leftMargin, topMargin);
+	}
+
+	/**
+	 * @see org.eclipse.riena.navigation.ui.swt.views.IWindowNavigator#beforeClose(org.eclipse.swt.widgets.Shell)
+	 */
+	public void beforeClose(final Shell shell) {
+		// nothing by default		
+	}
+
+}
