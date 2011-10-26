edef9273315e4f0bab83f86d69d6acfa985ef240
Stefan Liebig <stefan.liebig@compeople.de>
2011-10-26 12:31:21 +0000

    new singleton initializer interface

---
+++ org.eclipse.riena.core/src/org/eclipse/riena/core/singleton/ISingletonInitializer.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2007, 2011 compeople AG and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    compeople AG - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.riena.core.singleton;
+
+/**
+ * A {@code ISingletonInitializer} allows to initialize a 'singleton'. This
+ * might be necessary because the RAP
+ * {@code org.eclipse.rwt.SessionSingletonBase} is only capable of creating
+ * singletons with their default constructor.
+ * 
+ * @since 4.0
+ */
+public interface ISingletonInitializer<T> {
+
+	/**
+	 * Perform any necessary initialization actions on the newly created
+	 * singleton.
+	 * 
+	 * @param instance
+	 */
+	void init(T instance);
+}
