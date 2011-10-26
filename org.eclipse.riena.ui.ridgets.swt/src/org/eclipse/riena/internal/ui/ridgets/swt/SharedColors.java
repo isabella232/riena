3034af0f212e9e9839c2cb008faed30bd70ad07d
Stefan Liebig <stefan.liebig@compeople.de>
2011-10-26 14:13:24 +0000

    better readable

--- org.eclipse.riena.ui.ridgets.swt/src/org/eclipse/riena/internal/ui/ridgets/swt/SharedColors.java
+++ org.eclipse.riena.ui.ridgets.swt/src/org/eclipse/riena/internal/ui/ridgets/swt/SharedColors.java
@@ -20,7 +20,7 @@ import org.eclipse.swt.widgets.Display;
 
 /**
  * Provides access to a set of shared colors. The colors can be accessed using
- * one of the predifined string constants.
+ * one of the predefined string constants.
  * 
  * @see Activator#getSharedColor(Display, String)
  */
@@ -38,35 +38,32 @@ public final class SharedColors {
 	public static final String COLOR_NEGATIVE = "COLOR_NEGATIVE"; //$NON-NLS-1$
 	public static final String COLOR_OUTPUT = "COLOR_OUTPUT"; //$NON-NLS-1$
 
-	private Map<String, Color> sharedColors;
+	private final Map<String, Color> sharedColors = new HashMap<String, Color>();
 
 	SharedColors(final Display display) {
-		final Object[] values = { COLOR_MANDATORY, new RGB(255, 255, 175), COLOR_OUTPUT, new RGB(231, 233, 245),
-				COLOR_MANDATORY_OUTPUT, new RGB(255, 249, 216), COLOR_FLASH_ERROR, new RGB(250, 190, 190),
-				COLOR_NEGATIVE, new RGB(255, 0, 0) };
-		sharedColors = new HashMap<String, Color>();
-		for (int i = 0; i < values.length; i = i + 2) {
-			final String key = (String) values[i];
-			final RGB rgb = (RGB) values[i + 1];
-			final Color color = new Color(display, rgb);
-			sharedColors.put(key, color);
-		}
+		put(display, COLOR_MANDATORY, new RGB(255, 255, 175));
+		put(display, COLOR_OUTPUT, new RGB(231, 233, 245));
+		put(display, COLOR_MANDATORY_OUTPUT, new RGB(255, 249, 216));
+		put(display, COLOR_FLASH_ERROR, new RGB(250, 190, 190));
+		put(display, COLOR_NEGATIVE, new RGB(255, 0, 0));
+	}
+
+	private void put(final Display display, final String name, final RGB rgb) {
+		sharedColors.put(name, new Color(display, rgb));
 	}
 
 	Color getSharedColor(final String colorKey) {
-		Assert.isNotNull(sharedColors, "Shared colors are disposed"); //$NON-NLS-1$
+		Assert.isLegal(!sharedColors.isEmpty(), "Shared colors are disposed"); //$NON-NLS-1$
 		final Color result = sharedColors.get(colorKey);
 		Assert.isNotNull(result, "Could not find color with key: " + colorKey); //$NON-NLS-1$
 		return result;
 	}
 
 	void dispose() {
-		if (sharedColors != null) {
-			for (final Color color : sharedColors.values()) {
-				color.dispose();
-			}
-			sharedColors = null;
+		for (final Color color : sharedColors.values()) {
+			color.dispose();
 		}
+		sharedColors.clear();
 	}
 
 }
