4d9fcf7005c299c46fe805d6493e7c9b497dddde
Sabine Achilles <sabine.achilles@compeople.de>
2012-01-10 11:53:36 +0100

    Fixed CompletionCombo bug -> selection was set twice on popup list. the second time with wrong index

--- org.eclipse.riena.ui.swt/src/org/eclipse/riena/ui/swt/CompletionCombo.java
+++ org.eclipse.riena.ui.swt/src/org/eclipse/riena/ui/swt/CompletionCombo.java
@@ -1418,7 +1418,6 @@ public abstract class CompletionCombo extends Composite {
 			setImage(index);
 			text.setText(getItem(list, index));
 			defaultTextSelection();
-			setSelection(list, index);
 			Event e = new Event();
 			e.time = event.time;
 			e.stateMask = event.stateMask;
