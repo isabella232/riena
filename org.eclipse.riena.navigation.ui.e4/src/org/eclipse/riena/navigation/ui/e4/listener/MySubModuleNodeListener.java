df20e15ae1f76d6f9e8cfa7f90265374798e472e
Heiko Barth <heiko.barth@compeople.de>
2012-06-01 14:23:13 +0200

    use e4 logger

--- org.eclipse.riena.navigation.ui.e4/src/org/eclipse/riena/navigation/ui/e4/listener/MySubModuleNodeListener.java
+++ org.eclipse.riena.navigation.ui.e4/src/org/eclipse/riena/navigation/ui/e4/listener/MySubModuleNodeListener.java
@@ -10,6 +10,7 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExtensionRegistry;
 import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.e4.core.services.log.Logger;
 import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.model.application.ui.MElementContainer;
 import org.eclipse.e4.ui.model.application.ui.advanced.MPerspective;
@@ -46,6 +47,9 @@ public class MySubModuleNodeListener extends SubModuleNodeListener {
 
 	@Inject
 	private IEclipseContext context;
+
+	@Inject
+	private Logger logger;
 
 	/**
 	 * {@inheritDoc}
@@ -84,14 +88,13 @@ public class MySubModuleNodeListener extends SubModuleNodeListener {
 
 				// construct the String as in Application.e4xmi
 				final String pluginId = Activator.getDefault().getBundleContext().getBundle().getSymbolicName();
-				partToActivate.setContributionURI("bundleclass://" + pluginId + "/" + PartWrapper.class.getName());
+				partToActivate.setContributionURI("bundleclass://" + pluginId + "/" + PartWrapper.class.getName()); //$NON-NLS-1$ //$NON-NLS-2$
 
 				// set parent and add as child
 				parent.getChildren().add(partToActivate);
 				partToActivate.setParent(parent);
 			} catch (final CoreException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				logger.error(e);
 			}
 		} else {
 			partToActivate = parts.get(0);
@@ -104,7 +107,7 @@ public class MySubModuleNodeListener extends SubModuleNodeListener {
 			}
 		}
 		if (partToActivate == null) {
-			throw new IllegalStateException("Part not found, partId: " + partId);
+			throw new IllegalStateException("Part not found, partId: " + partId); //$NON-NLS-1$
 		}
 		partToActivate.getParent().setSelectedElement(partToActivate);
 	}
@@ -116,18 +119,12 @@ public class MySubModuleNodeListener extends SubModuleNodeListener {
 		}
 	}
 
-	/**
-	 * @param partId
-	 * @return
-	 * @throws CoreException
-	 */
 	private MPart createFromPluginXmlContribution(final String partId) throws CoreException {
 		final IExtensionRegistry extensionRegistry = context.get(IExtensionRegistry.class);
 		for (final IConfigurationElement e : extensionRegistry.getConfigurationElementsFor(VIEWS_EXT_POINT)) {
 			if (partId.equals(e.getAttribute(ID))) {
 				final MPart part = MBasicFactory.INSTANCE.createPart();
 				part.setElementId(partId);
-				//				part.setObject(e.createExecutableExtension(CLASS));
 				return part;
 			}
 		}
@@ -142,18 +139,6 @@ public class MySubModuleNodeListener extends SubModuleNodeListener {
 	@Override
 	public void parentChanged(final ISubModuleNode source) {
 		MySubApplicationNodeListener.prepare(source);
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.riena.navigation.listener.NavigationNodeListener#disposed(org.eclipse.riena.navigation.INavigationNode)
-	 */
-	@Override
-	public void disposed(final ISubModuleNode source) {
-		System.err.println("MySubModuleNodeListener.disposed()");
-		// TODO Auto-generated method stub
-		super.disposed(source);
 	}
 
 	/**
@@ -172,10 +157,6 @@ public class MySubModuleNodeListener extends SubModuleNodeListener {
 		}
 	}
 
-	/**
-	 * @param mWindow
-	 * @return
-	 */
 	private List<MenuCoolBarComposite> getMenuCoolBarComposites() {
 		final EModelService modelService = context.get(EModelService.class);
 		final MPart headerPart = (MPart) modelService.find(HEADER_PART_ID, context.get(MApplication.class));
