987034b4476118fb764b53c3bb5ffe2308e00b38
Heiko Barth <heiko.barth@compeople.de>
2012-08-03 08:55:53 +0200

    Thread safety for message box code.

--- org.eclipse.riena.example.client/src/org/eclipse/riena/example/client/exceptionhandler/ExceptionMessageBox.java
+++ org.eclipse.riena.example.client/src/org/eclipse/riena/example/client/exceptionhandler/ExceptionMessageBox.java
@@ -19,22 +19,49 @@ import org.eclipse.riena.ui.ridgets.IMessageBoxRidget.MessageBoxOption;
 import org.eclipse.riena.ui.ridgets.IMessageBoxRidget.Type;
 import org.eclipse.riena.ui.ridgets.swt.SwtRidgetFactory;
 import org.eclipse.riena.ui.swt.MessageBox;
+import org.eclipse.riena.ui.swt.uiprocess.SwtUISynchronizer;
 
+/**
+ * {@link IExceptionHandler} implementation showing a swt message box. Note that instances of {@link IExceptionHandler} are called on the {@link Thread} causing
+ * the exception to be handled. For that reason {@link SwtUISynchronizer} is used to serialize execution of the message box code to the swt user interface
+ * {@link Thread}.
+ */
 public class ExceptionMessageBox implements IExceptionHandler {
 
+	private final static SwtUISynchronizer uiSynchronizer = new SwtUISynchronizer();
+
 	public IExceptionHandler.Action handleException(final Throwable t, final String msg, final Logger logger) {
-		final MessageBox messageBox = new MessageBox(Display.getCurrent().getActiveShell());
+		final Wrapper resultWrapper = new Wrapper();
+		// As we have no control over the calling thread we have to ensure thread safety here. Note: This method will block until the message box is applied/closed.
+		uiSynchronizer.syncExec(new Runnable() {
+
+			public void run() {
+				resultWrapper.action = showMessageBox(t);
+
+			}
+		});
+		return resultWrapper.action;
+	}
+
+	// Wrapper for the result of the MessageBox
+	static private class Wrapper {
+		IExceptionHandler.Action action;
+	}
+
+	private IExceptionHandler.Action showMessageBox(final Throwable t) {
+		final Display display = uiSynchronizer.getDisplay();
+		final MessageBox messageBox = new MessageBox(display.getActiveShell());
 		final IMessageBoxRidget messageBoxRidget = (IMessageBoxRidget) SwtRidgetFactory.createRidget(messageBox);
 
 		messageBoxRidget.setTitle("Exception at Runtime"); //$NON-NLS-1$
 		messageBoxRidget.setType(Type.ERROR);
 		if (t != null) {
-			messageBoxRidget.setText(t.getMessage());
+			// Message is not always filled. If message is null take the value of #toString()
+			messageBoxRidget.setText(t.getMessage() != null ? t.getMessage() : t.toString());
 		}
 		final IMessageBoxRidget.MessageBoxOption ok = new IMessageBoxRidget.MessageBoxOption("OK"); //$NON-NLS-1$
 		final IMessageBoxRidget.MessageBoxOption ignore = new IMessageBoxRidget.MessageBoxOption("Ignore"); //$NON-NLS-1$
-		final IMessageBoxRidget.MessageBoxOption printstack = new IMessageBoxRidget.MessageBoxOption(
-				"Print stacktrace and OK"); //$NON-NLS-1$
+		final IMessageBoxRidget.MessageBoxOption printstack = new IMessageBoxRidget.MessageBoxOption("Print stacktrace and OK"); //$NON-NLS-1$
 		messageBoxRidget.setOptions(new IMessageBoxRidget.MessageBoxOption[] { ok, ignore, printstack });
 		final MessageBoxOption show = messageBoxRidget.show();
 		if (t != null && show.equals(printstack)) {
