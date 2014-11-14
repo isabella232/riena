10bc60020f1cf60155fca8af97d42e73c931418d
Jordan Dukadinov <jordan.dukadinov@compeople.de>
2014-11-14 17:40:51 +0100

    clean up prebuild plugins

--- org.eclipse.riena.build3/prebuild/p2publisher.bat
+++
@@ -1,9 +0,0 @@
-set INPUT=C:/build/workspaces/riena2/org.eclipse.riena.build3/prebuild
-set OUTPUT=file:/C:/build/workspaces/riena2/org.eclipse.riena.build3/prebuild-out
-
-java -jar C:\build\develop\eclipse-3.7/plugins/org.eclipse.equinox.launcher_*.jar ^
--application org.eclipse.equinox.p2.publisher.FeaturesAndBundlesPublisher ^
--metadataRepository %OUTPUT% ^
--artifactRepository %OUTPUT% ^
--source %INPUT% ^
--configs win32.win32.x86 -compress -publishArtifacts
